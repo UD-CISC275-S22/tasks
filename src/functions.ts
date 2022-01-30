e8f8cc4a5f705e38c4b2826b9d498c95c8ecf36a
Austin Cory Bart <acbart@vt.edu>
2022-01-29 23:23:45 -0500

    Basic functions tests and stubs

---
+++ src/functions.ts
@@ -0,0 +1,19 @@
+export function fahrenheitToCelius(temperature: number): number {
+    return 0;
+}
+
+export function add3(first: number, second: number, third: number): number {
+    return 0;
+}
+
+export function shout(message: string): string {
+    return "";
+}
+
+export function isQuestion(message: string): boolean {
+    return true;
+}
+
+export function convertYesNo(word: string): boolean | null {
+    return true;
+}
