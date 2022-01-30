59acfb0c7a54ff4130f572671a12b873774c5e3c
Austin Cory Bart <acbart@vt.edu>
2022-01-30 00:24:38 -0500

    Some logging tests

--- src/HtmlCss.test.tsx
+++ src/HtmlCss.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 describe("Some HTML Elements are added.", () => {
     test("There is a header", () => {
@@ -18,11 +19,25 @@ describe("Some basic CSS is added.", () => {
 });
 
 describe("Some Bootstrap Elements are added", () => {
-    test("There is a bootstrap button", () => {
+    test("There is one bootstrap button with the text 'Log Hello World'", () => {
         render(<App />);
-        const button = screen.getByRole("button");
+        const button = screen.getByRole("button", { name: /Log Hello World/i });
         expect(button).toBeInTheDocument();
         expect(button).toHaveClass("btn");
         expect(button).toHaveClass("btn-primary");
+    });
+
+    test("Not clicking the bootstrap button does not logs 'Hello World!'", () => {
+        const consoleSpy = jest.spyOn(console, "log");
+        render(<App />);
+        expect(consoleSpy).not.toHaveBeenCalledWith("Hello World!");
+    });
+
+    test("Clicking the bootstrap button logs 'Hello World!'", () => {
+        const consoleSpy = jest.spyOn(console, "log");
+        render(<App />);
+        const button = screen.getByRole("button", { name: /Log Hello World/i });
+        userEvent.click(button);
+        expect(consoleSpy).toHaveBeenCalledWith("Hello World!");
     });
 });
