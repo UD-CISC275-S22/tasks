2498cfa955fe6061797cb9c4f95608da553e4ac9
Austin Cory Bart <acbart@vt.edu>
2022-02-01 16:09:25 -0500

    Fix question text

--- src/arrays.ts
+++ src/arrays.ts
@@ -18,7 +18,7 @@ export function tripleNumbers(numbers: number[]): number[] {
 
 /**
  * Consume an array of strings and convert them to integers. If
- * the number cannot be parsed as an integer, convert it to "?" instead.
+ * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
     return [];
@@ -28,7 +28,7 @@ export function stringsToIntegers(numbers: string[]): number[] {
  * Consume an array of strings and return them as numbers. Note that
  * the strings MAY have "$" symbols at the beginning, in which case
  * those should be removed. If the result cannot be parsed as an integer,
- * convert it to "?" instead.
+ * convert it to 0 instead.
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
@@ -37,7 +37,8 @@ export const removeDollars = (amounts: string[]): number[] => {
 
 /**
  * Consume an array of messages and return a new list of the messages. However, any
- * string that ends in "!" should be made uppercase.
+ * string that ends in "!" should be made uppercase. Also, remove any strings that end
+ * in question marks ("?").
  */
 export const shoutIfExclaiming = (messages: string[]): string[] => {
     return [];
@@ -65,6 +66,7 @@ export function allRGB(colors: string[]): boolean {
  * numbers being added together along with their actual sum.
  *
  * For instance, the array [1, 2, 3] would become "6=1+2+3".
+ * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
     return "";
@@ -74,10 +76,10 @@ export function makeMath(addends: number[]): string {
  * Consumes an array of numbers and produces a new array of the same numbers,
  * with one difference. After the FIRST negative number, insert the sum of all
  * previous numbers in the list. If there are no negative numbers, then append
- * 0 to the list.
+ * the sum to the list.
  *
  * For instance, the array [1, 9, -5, 7] would become [1, 9, -5, 10, 7]
- * And the array [1, 9, 7] would become [1, 9, 0]
+ * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
     return [];
