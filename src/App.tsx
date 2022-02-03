1ca6b6ecb074e86299e5a843cd81703e25a47df1
Josh Lyon <joshlyon@udel.edu>
2022-02-03 11:57:14 -0500

    Added sadber, changes for html-css task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">Josh was here - CISC275</header>
-            <p>
-                Hello World! Edit <code>src/App.tsx</code> and save. This page
-                automatically reload.
-            </p>
+            <h1>Hello World</h1>
+
+            <img alt="A picture of prof silber" src="assets/sadber.png" />
+            <ul>
+                <p>Things I love about that picture of prof. Silber</p>
+                <li>The poorly drawn tear</li>
+                <li>The way he looks so happy</li>
+                <li>How closely it is cropped</li>
+            </ul>
+
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
