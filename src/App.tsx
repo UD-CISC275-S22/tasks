2de3f9c32e0655cffa4bc8ab5f70c04fefc5abe6
andrewl15 <andrewl@udel.edu>
2022-02-07 18:45:03 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Andrew Long was here.
+                automatically reload. Andrew Long was here!
             </p>
         </div>
     );
