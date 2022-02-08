97eeb36684469aeb715a3b7de59e26b411c71d23
Isaac <isaacl@udel.edu>
2022-02-08 11:45:47 -0500

    back to basics

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Isaac Lewis <br></br> 
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
