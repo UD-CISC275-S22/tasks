e84fe5b2ececdcd7e71ec08c5bf9896f3ef0abd7
Isaac <isaacl@udel.edu>
2022-02-08 12:41:21 -0500

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 Isaac Lewis <br></br> UD CISC275 with React Hooks and TypeScript
+                <body className="App-header">Hello World</body>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
