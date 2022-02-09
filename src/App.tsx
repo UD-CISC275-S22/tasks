b730ea936b5c076c40f901dc937fab5f46f7a98c
Ocean Shen <oceanshen@ocean-shens-mbp.myfiosgateway.com>
2022-02-09 18:18:35 -0500

    Added 'Hello World' to App

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App(): JSX.Element {
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Edited by Ocean Shen on 2/8/22 at 7:41pm.
+                Hello World.
             </p>
         </div>
     );
