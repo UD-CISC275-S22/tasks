0709b19eeb79d08226ab1afef7a90f9bb56bfe3f
ZiyangJiang947520 <zyjiang@udel.edu>
2022-02-08 22:51:58 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                Ziyang Jiang UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
