512079507c1f364b6ddf8948fdca8e3ccbffca23
andrewl15 <andrewl@udel.edu>
2022-02-09 12:12:38 -0500

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Andrew Long was here! Test.
+                automatically reload. Andrew Long was here! Hello World
             </p>
         </div>
     );
