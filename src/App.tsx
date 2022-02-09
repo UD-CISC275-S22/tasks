fe835d4ccf8caeb484675ad07e7a1b4ed8c41233
elgan65536 <elgan256@gmail.com>
2022-02-09 18:34:59 -0500

    trying again

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App(): JSX.Element {
                 Connor Nagle UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                Hello World Edit <code>src/App.tsx</code> and save. This page
+                will automatically reload.
             </p>
         </div>
     );
