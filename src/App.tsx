27b746e6dfbac65215f10261a7bd864cf88ef553
Sydney Segear <sydneysegear@sydneys-mbp.myfiosgateway.com>
2022-02-08 22:57:30 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript. Sydney Segear
+                Hello World. UD CISC275 with React Hooks and TypeScript. Sydney
+                Segear.
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
