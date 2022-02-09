97d47b71117df739d0cb20cc09d14458af70f719
Rehan Amir <51897056+cuprehan@users.noreply.github.com>
2022-02-09 04:02:16 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,11 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                Rehan Amir UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Rehan Amir
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Rehan Amir
+                automatically reload.
             </p>
         </div>
     );
