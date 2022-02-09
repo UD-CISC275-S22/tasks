bbf838dcbdb54f65454139daff64cb8e100243e3
Zhiwen Zhu <zhiwenzhu@Zhiwens-MacBook-Pro.local>
2022-02-08 23:02:13 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload Edited by Zhiwen Zhu.
+                automatically reload Edited by Zhiwen Zhu. Hello World
             </p>
         </div>
     );
