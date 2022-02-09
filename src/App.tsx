1a0836e9857ba9cec07673c21a28799224ff4c13
Marvin Dang <marvindang@Marvins-MacBook-Pro.local>
2022-02-09 18:06:11 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript created by Marv Dang
+                and Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
