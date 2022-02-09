742928d34f715bc99fb7b6432be292dc47ee73a1
Christian Rullan <laptopchris01@gmail.com>
2022-02-09 18:11:12 -0500

    added name to app

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>Christian Rullan</p>
         </div>
     );
 }
