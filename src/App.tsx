7d0c774090cd4bfcc64372c2cfec441bef61d46e
Jackson Leadlove <leadlove@udel.edu>
2022-02-09 12:29:08 -0500

    Added HTML and CSS elements to satisfy Task 3 tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import image from "./images/image.png";
 
 function App(): JSX.Element {
     return (
@@ -8,10 +10,44 @@ function App(): JSX.Element {
                 Jackson Leadlove&apos;s UD CISC275 with React Hooks and
                 TypeScript.
             </header>
+
             <p>
-                Hello World! Prettier is yelling at me for having carriage
-                return characters! It&apos;s driving me insane!!
+                Hello World! Following is a set of HTML and Bootstrap elements
+                that satisfy the tests in Task 3.
             </p>
+
+            <h2> This is a header! Yay! </h2>
+
+            <img
+                src={image}
+                alt="This is an image I created in Photopea. More specifically, this is the alternative text for the image in case it fails to load."
+            />
+
+            <ul>
+                <li>This is an unordered list!</li>
+                <li>It has at least three elements.</li>
+                <li>
+                    Therefore, it should, in theory, satisfy the test for the
+                    Task.
+                </li>
+            </ul>
+
+            <div className="button">
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
+
+            <Container>
+                <Row>
+                    <div className="col">
+                        <Col md="auto">First column of two column layout.</Col>
+                    </div>
+                    <div className="col">
+                        <Col md="auto">Second column of two column layout.</Col>
+                    </div>
+                </Row>
+            </Container>
         </div>
     );
 }
