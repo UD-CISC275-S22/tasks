404ecf5c34ef03b5d73587d5324f43bb0ff7f17c
Andrew Woods <andrewwoods@andrews-mbp.myfiosgateway.com>
2022-02-09 11:58:01 -0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Andrew Woods.
+                automatically reload. Andrew Woods. Hello World
             </p>
         </div>
     );
