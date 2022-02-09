c85658e6ba86f70183fc7ea8e54f59adf0275a61
rjley <79172741+rjley@users.noreply.github.com>
2022-02-09 17:40:54 -0500

    Completed Task 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
