c4b3b8f0e34ea514ffde77cb319b8f22a1c9b913
Tommy Ashfield <tommyashfield@wifi-roaming-128-4-151-249.wifi.udel.edu>
2022-02-09 20:52:32 -0500

    adding hello world

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Tommy Ashfield, Hello World</p>
+            <p>Tommy Ashfield, Hello World </p>
         </div>
     );
 }
