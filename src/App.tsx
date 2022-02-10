1cc9d58d4ae8f1a792e3a4228f5b8ed17212801e
zyjiang@udel.edu <zyjiang@udel.edu>
2022-02-09 20:03:27 -0500

    check

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Name:Ziyang Jiang UD CISC275 with React Hooks and TypeScript
-                Hello World
+                Ziyang Jiang UD CISC275 with React Hooks and TypeScript Hello
+                World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
