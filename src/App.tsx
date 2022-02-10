71e89cdf421e0ce26ba56077a04b722145085d89
Yuchen Zhang <zyc@udel.edu>
2022-02-09 19:03:54 -0500

    hello word

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript, Yuchen Zhang.
+                UD CISC275 with React Hooks and TypeScript, Yuchen Zhang. Hello
+                World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
