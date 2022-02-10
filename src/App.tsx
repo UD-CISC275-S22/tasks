2917f77d6e1e267a7cf92a11975cabe4f78e0eaf
Sydney Segear <sydneysegear@sydneys-mbp.myfiosgateway.com>
2022-02-10 16:37:26 -0500

    html and css - task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,56 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
+                <h1> Welcome to a page about my cat</h1>
+                <img
+                    src="Users/sydneysegear/Downloads/bella.jpg"
+                    alt="A picture of my cat Bella"
+                />
                 Hello World. UD CISC275 with React Hooks and TypeScript. Sydney
                 Segear.
             </header>
+            Reasons I like my cat:
+            <ul>
+                <li>She is cute</li>
+                <li> She is small</li>
+                <li> She can sometimes be nice</li>
+            </ul>
             <p>
+                <div>
+                    <Button onClick={() => console.log("Hello World!")}>
+                        Log Hello World
+                    </Button>
+                </div>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "60px",
+                                backgroundColor: "#FF0000"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "60px",
+                                backgroundColor: "#FF0000"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
