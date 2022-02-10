58384845aceb60b4c64b086f91847d7ef1f6a1a4
Ocean Shen <oceanshen@ocean-shens-mbp.myfiosgateway.com>
2022-02-09 23:45:44 -0500

    Complete basic HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
 import React from "react";
+import { Container, Col, Row, Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Ocean Shen - UD CISC275 with React Hooks and TypeScript
+                Ocean Shen: UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Edited by Ocean Shen on 2/8/22 at 7:41pm.
-                Hello World.
-            </p>
+            <h1>Hello World!</h1>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <p></p>
+            <Container>
+                <Row>
+                    <Col>
+                        Who is Ocean Shen?
+                        <ul>
+                            <li>I am a sophomore CS and STAT double major.</li>
+                            <li>I like to code, eat, and sleep.</li>
+                            <li>
+                                I was introduced to programming through Python.
+                            </li>
+                        </ul>
+                        <div
+                            style={{
+                                height: 50,
+                                width: 200,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <p>
+                            Here is a picture of food I like to eat when coding:
+                        </p>
+                        <img
+                            src="https://media.chefdehome.com/740/0/0/salsa/homemade-salsa.jpg"
+                            alt="A picture of chips being dipped in salsa"
+                        />
+                        <p></p>
+                        <div
+                            style={{
+                                height: 50,
+                                width: 200,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        <p></p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
