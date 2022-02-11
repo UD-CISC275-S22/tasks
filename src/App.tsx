2f654735ce9f39189ff724eab8deac644c1fe79f
Sean Williams <sean475@gmail.com>
2022-02-10 20:35:27 -0500

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -11,8 +12,21 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
             <p>Hello World</p>
             <p>Sean Williams</p>
+            <h1>Task 3 Stuff:</h1>
+            <img src="src/car.jpg" alt="A picture of my old car" />
+            <Container>
+                <Row>
+                    <Col>First column.</Col>
+                    <Col>
+                        Second column. You can put whatever you want in here,
+                        and it will be on the right side. Maybe try adding an
+                        image?
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
