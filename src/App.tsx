2d179ae82ba8626b26ab89364535721770aa9d56
Sean Williams <sean475@gmail.com>
2022-02-10 21:15:40 -0500

    Finished Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,41 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
             <p>Hello World</p>
             <p>Sean Williams</p>
             <h1>Task 3 Stuff:</h1>
-            <img src="src/car.jpg" alt="A picture of my old car" />
+            <img
+                src="https://i.pinimg.com/originals/78/09/94/780994928b848b937791d382de43ef75.jpg"
+                alt="Fox jumping in snow"
+            />
+            <ol>
+                <li>I dont know what to put here</li>
+                <li>Foxes are cute</li>
+                <li>Filler Text</li>
+            </ol>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <Container>
                 <Row>
-                    <Col>First column.</Col>
                     <Col>
-                        Second column. You can put whatever you want in here,
-                        and it will be on the right side. Maybe try adding an
-                        image?
+                        <div
+                            style={{
+                                width: "500px",
+                                height: "200px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        Small Rectangle
+                        <div
+                            style={{
+                                width: "500px",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
                     </Col>
                 </Row>
             </Container>
