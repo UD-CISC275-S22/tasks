c8a1b56d5742e23276b57118fba8bcc47f18dd88
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-02-12 12:28:09 -0500

    Task 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,10 +8,45 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Meet my dogs</h1>
+                        <img
+                            src={require("./dogs.jpg")}
+                            style={{ height: "225px", width: "300px" }}
+                            alt="A cute picture of my dogs"
+                        />
+                        <div
+                            style={{
+                                width: "10px",
+                                height: "5px",
+                                backgroundColor: "red",
+                                margin: "auto"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <p>My dogs:</p>
+                        <ul>
+                            <li>Valegro od Dalajskeho potoka</li>
+                            <li>Blueberry od Dalajskeho potoka</li>
+                            <li>Both are standard black schnauzers</li>
+                        </ul>
+                        <div
+                            style={{
+                                width: "10px",
+                                height: "5px",
+                                backgroundColor: "red",
+                                margin: "auto"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <p>Jan Ahmed</p>
             <p>Hello World</p>
         </div>
