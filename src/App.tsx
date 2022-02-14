2e813fe36bc4417fc73b072563550f58b6134027
zyjiang@udel.edu <zyjiang@udel.edu>
2022-02-13 22:39:41 -0500

    task3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
 import React from "react";
+import { Button, Container, Row } from "react-bootstrap";
 import "./App.css";
+import { Col } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                Ziyang Jiang UD CISC275 with React Hooks and TypeScript Hello
-                World
-            </header>
+            <div
+                style={{
+                    border: "red",
+                    height: "500",
+                    width: "500",
+                    backgroundColor: "red"
+                }}
+            >
+                <Container>
+                    <Row>
+                        <Col>
+                            <header className="App-header">
+                                Ziyang Jiang UD CISC275 with React Hooks and
+                                TypeScript Hello World
+                            </header>
+                        </Col>
+                        <Col>
+                            <header>I love you. Dr.Bart</header>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <img
+                src="/Users/ziyangjiang/Desktop/iron-man_marvel_silo.png"
+                alt="A picture of Iron Man"
+            />
+            <ul>
+                <li>one</li>
+                <li>two</li>
+                <li>three</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World")}>
+                Log Hello World
+            </Button>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
