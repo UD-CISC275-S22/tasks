d584c0a6454d83191288a33b47a8a161d36930bd
Isaac <isaacl@udel.edu>
2022-02-13 23:34:21 -0500

    task 3 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
+            <Container>
+                <Row>
+                    <Col>
+                        Left Column
+                        <div className="rectangle"></div>
+                    </Col>
+                    <Col>
+                        Right Column
+                        <div className="rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
+            <h1 className="csn">Crosby, Stills and Nash</h1>
+            <img
+                src="https://i.imgur.com/6xrGsGi.jpg"
+                alt="Crosby, Stills and Nash"
+            />
             <header className="App-header">
                 Isaac Lewis <br></br> UD CISC275 with React Hooks and TypeScript
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
                 <body className="App-header">Hello World</body>
+                <ol>
+                    <li>Crosby</li>
+                    <li>Stills</li>
+                    <li>Nash</li>
+                </ol>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
