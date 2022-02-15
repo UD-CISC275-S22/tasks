5de1a2d5208c6038801e4cca54c4532f33948528
dehua123 <mheider@udel.edu>
2022-02-15 14:46:47 -0500

    made website pretty with html and css
    made the stuff pretty task3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,59 @@
 import React from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with hello world React Hooks and TypeScript mathias
-                heider
+                <h1>
+                    {" "}
+                    UD CISC275 with hello world React Hooks and TypeScript
+                    mathias heider
+                </h1>
+                <Container>
+                    <Row>
+                        <Col>
+                            <ul>
+                                <div
+                                    style={{
+                                        width: "500px",
+                                        height: "500px",
+                                        backgroundColor: "#FF0000"
+                                    }}
+                                >
+                                    {" "}
+                                </div>
+                                <li>one fish</li>
+                                <li>two fish</li>
+                                <li>red fish</li>
+                                <li>blue fish</li>
+                            </ul>
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "500px",
+                                    height: "500px",
+                                    backgroundColor: "#FF0000"
+                                }}
+                            >
+                                {" "}
+                            </div>
+                            <Button onClick={() => console.log("Hello World!")}>
+                                Log Hello World
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG"
+                    alt="big panda"
+                />
             </p>
         </div>
     );
