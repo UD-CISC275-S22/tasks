c5bc0fce76a9630ac13918c9023edd45eb917c4e
jjneilson <neilson.john38781@gmail.com>
2022-02-15 18:40:09 -0500

    Completed add3 function

--- src/functions.ts
+++ src/functions.ts
@@ -12,7 +12,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum = 0;
+    if (first > 0) {
+        sum += first;
+    }
+    if (second > 0) {
+        sum += second;
+    }
+    if (third > 0) {
+        sum += third;
+    }
+    return sum;
 }
 
 /**
