306d8184a457084d7dbc638af39a246aa484678b
Sydney Hester <sydneyph@udel.edu>
2022-02-15 19:45:41 -0500

    added features for the website defined in task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
-
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Hello World
+            <header className="App-header" style={{ backgroundColor: "blue" }}>
+                Cool Locations around the World by CISC275
             </header>
             <p>
                 Edit <code>Sydney</code> and save. This page will automatically
                 reload.
             </p>
+            <img
+                src="https://trendsinusa.com/wp-content/uploads/2018/01/9061Ujo.jpg"
+                alt="A photo of Nature"
+            />
+            <div className="container">
+                <div className="row">
+                    <div
+                        className="col"
+                        style={{ border: "3px solid red", padding: "6px" }}
+                    >
+                        <p>
+                            Nature is a very important part of the modern world
+                            but many are unable to enjoy its splendor. So this
+                            website will have photos of nature sites.
+                        </p>
+                        <button
+                            onClick={() => console.log("Travel the World!")}
+                        >
+                            Travel the World
+                        </button>
+                    </div>
+                    <div
+                        className="col"
+                        style={{ border: "3px solid red", padding: "6px" }}
+                    >
+                        <h3> Reasons you should travel here </h3>
+                        <ul>
+                            <li>It is beautiful</li>
+                            <li>It is serene here</li>
+                            <li>It is an unspoiled piece of Nature</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
