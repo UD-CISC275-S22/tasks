6e79504148eeef71dab0e202f787a7f47ef5ffbd
Abigail Walters <awrose@udel.edu>
2022-02-15 22:54:25 -0500

    isQuestion

--- src/functions.ts
+++ src/functions.ts
@@ -51,7 +51,8 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    let last: string = message[message.length - 1];
+    return last === "?" ? true : false;
 }
 
 /**
