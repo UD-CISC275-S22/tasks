940fae302de9afa507a74a13cd0d1e8506458b7a
jjneilson <neilson.john38781@gmail.com>
2022-02-17 11:07:51 -0500

    Completed bookEndList function

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,11 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (numbers.length > 0) {
+        return [numbers[0], numbers[numbers.length - 1]];
+    } else {
+        return [];
+    }
 }
 
 /**
