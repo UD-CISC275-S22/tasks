01e696e3a3d8cb61ac3bdb8735f4a6c3fdd08a47
cuprehan <cuprehan@udel.edu>
2022-02-17 00:22:26 -0500

    completed covertYesNo function

--- src/functions.ts
+++ src/functions.ts
@@ -47,5 +47,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toLowerCase() == "yes") {
+        return true;
+    } else if (word.toLowerCase() == "no") {
+        return false;
+    } else {
+        return null;
+    }
 }
