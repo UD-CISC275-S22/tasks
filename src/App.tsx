c412c01ee84c2fedc7e9763f8dea8456fe9bc6fb
Eric Merritt <ericmero@udel.edu>
2022-02-18 18:09:26 -0500

    Modify for Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Hello World Eric Merritt</p>
+            <p>Hello World, Eric Merritt</p>
         </div>
     );
 }
