cf6614b65604e2e5d11465daff6694a8441ca8d3
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-02-19 17:22:21 -0500

    tripleNumbers implemented

--- src/arrays.ts
+++ src/arrays.ts
@@ -20,7 +20,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const rarr = numbers.map((n: number): number => n * 3);
+    return rarr;
 }
 
 /**
