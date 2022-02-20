95daee852906eae43671df97b6b35484aeda0930
dhp2616 <dhp2616@gmail.com>
2022-02-20 00:14:04 -0500

    task 4 final commit, hopefully

--- src/functions.ts
+++ src/functions.ts
@@ -52,7 +52,7 @@ export function isQuestion(message: string): boolean {
 /**
  * Consumes a word (a string) and returns either `true`, `false`, or `null`. If the string
  * is "yes" (upper or lower case), then return `true`. If the string is "no" (again, either
- * upper or lower case), then return `false`. Otherwise, return `null`.
+ * upper or lower case), then return `faasdflse`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
     if (word == "yes" || word == "Yes" || word == "YES") {
