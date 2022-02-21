ee2c726d3b386c2d7fbd26050272f4d63f69c21e
Marvin Dang <marvindang@Marvins-MacBook-Pro.local>
2022-02-20 19:03:18 -0500

    1 left

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * (5 / 9);
 }
 
 /**
@@ -12,7 +12,22 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum = 0;
+    if (first < 0) {
+        sum = second + third;
+    } else if (second < 0) {
+        sum = first + third;
+    } else if (third < 0) {
+        sum = first + second;
+    } else {
+        sum = first + second + third;
+    }
+
+    if (sum <= 0) {
+        return 0;
+    } else {
+        return sum;
+    }
 }
 
 /**
@@ -20,7 +35,11 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    let new_string = "";
+    for (let i = 0; i < message.length; i++) {
+        new_string += message[i].toUpperCase();
+    }
+    return new_string + "!";
 }
 
 /**
@@ -28,7 +47,9 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    let bool = true;
+    message[message.length - 1] === "?" ? (bool = true) : (bool = false);
+    return bool;
 }
 
 /**
@@ -37,5 +58,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "yes" || word === "YES" || word == "yes") {
+        return true;
+    } else if (word === "no" || word === "NO" || word == "No") {
+        return false;
+    } else {
+        return null;
+    }
 }
