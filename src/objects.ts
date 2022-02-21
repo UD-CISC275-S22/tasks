47acc79db50b370fde981b3cb30523b4c0844c73
awrose <79182069+awrose@users.noreply.github.com>
2022-02-21 09:52:13 -0500

    addOption

--- src/objects.ts
+++ src/objects.ts
@@ -175,7 +175,8 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const newOpt = { ...question, options: [...question.options, newOption] };
+    return newOpt;
 }
 
 /**
