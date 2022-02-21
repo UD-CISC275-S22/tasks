da31cda7bf54498b7daaeffe44bd828ea67a9680
awrose <79182069+awrose@users.noreply.github.com>
2022-02-21 09:42:04 -0500

    publishQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -131,7 +131,18 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const newQues = {
+        id: question.id,
+        name: question.name,
+        type: question.type,
+        body: question.body,
+        expected: question.expected,
+        options: question.options,
+        points: question.points,
+        published: !question.published
+    };
+
+    return newQues;
 }
 
 /**
