ddec24437af04e752fcdd0fbb6338dc9c4f6d748
aidant <aidant@udel.edu>
2022-02-21 11:04:28 -0500

    got some work done

--- src/objects.ts
+++ src/objects.ts
@@ -10,7 +10,16 @@ export function makeBlankQuestion(
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false
+    };
 }
 
 /**
@@ -21,7 +30,10 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    const first = answer.trim().toLowerCase();
+    const second = question.expected.trim().toLowerCase();
+
+    return first === second;
 }
 
 /**
@@ -31,7 +43,12 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "multiple_choice_question") {
+        return question.options.some(
+            (value: string): boolean => answer === value
+        );
+    }
+    return true;
 }
 
 /**
@@ -41,7 +58,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return question.id.toString() + ": " + question.name.slice(0, 10);
 }
 
 /**
@@ -62,7 +79,16 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let mkDn = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        mkDn = mkDn + "\n- ";
+        const options = question.options.reduce(
+            (prev: string, curr: string): string => prev + "\n- " + curr
+        );
+        mkDn = mkDn + options;
+    }
+    console.log(mkDn);
+    return mkDn;
 }
 
 /**
