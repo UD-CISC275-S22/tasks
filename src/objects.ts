32f083f109fc83680287c14a08c90fd4180cc35d
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-02-22 11:39:48 -0500

    toMarkdown implemented

--- src/objects.ts
+++ src/objects.ts
@@ -87,7 +87,17 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    if (question.type === "multiple_choice_question") {
+        return (
+            "# " +
+            question.name +
+            "\n" +
+            question.body +
+            "\n- " +
+            question.options.join("\n- ")
+        );
+    }
+    return "# " + question.name + "\n" + question.body;
 }
 
 /**
