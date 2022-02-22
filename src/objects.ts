fc442ed929fc22e91683c8e08655a38a4c0db099
jjneilson <neilson.john38781@gmail.com>
2022-02-22 15:55:18 -0500

    Finished toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -58,7 +58,14 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return (
+        question.id.toString() +
+        ": " +
+        question.name.substring(
+            0,
+            question.name.length > 10 ? 10 : question.name.length
+        )
+    );
 }
 
 /**
