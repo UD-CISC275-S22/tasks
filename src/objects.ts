91560582816239b6d72062458b1d5786eafd2d96
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-02-22 11:42:02 -0500

    renameQuestion implemented

--- src/objects.ts
+++ src/objects.ts
@@ -105,7 +105,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return { ...question, name: newName };
 }
 
 /**
