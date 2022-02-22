893ac8d4a0adf8c8e8749057a794a13d23d84962
Sean Williams <sean475@gmail.com>
2022-02-21 20:41:24 -0500

    Finished mergeQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -135,5 +135,11 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return contentQuestion;
+    return {
+        ...contentQuestion,
+        id: id,
+        name: name,
+        points: points,
+        published: false
+    };
 }
