8be88ac3d17c75113757235b6f1a33b3c8549f2f
jjneilson <neilson.john38781@gmail.com>
2022-02-22 16:12:36 -0500

    renameQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -102,7 +102,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return { ...question, name: newName };
 }
 
 /**
