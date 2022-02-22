f16053e95e441db5eab78bdbba6e7d5b35f3c543
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-02-21 20:50:03 -0500

    makeBlankQuestion implemented

--- src/objects.ts
+++ src/objects.ts
@@ -10,7 +10,16 @@ export function makeBlankQuestion(
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false
+    };
 }
 
 /**
