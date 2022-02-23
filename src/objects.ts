b46cee0568f16317560a2a08b83f873b375ebb98
Sydney Hester <sydneyph@udel.edu>
2022-02-23 11:52:23 -0500

    Solved addOption

--- src/objects.ts
+++ src/objects.ts
@@ -140,7 +140,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const addedOption = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    return addedOption;
 }
 
 /**
