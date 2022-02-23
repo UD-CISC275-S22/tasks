aa1a4ba7a549bc55a0f2c3d3a2a5f44ee52621f0
Sydney Hester <sydneyph@udel.edu>
2022-02-23 11:34:41 -0500

    Solved toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -83,7 +83,14 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    const string = "# " + question.name + "\n" + question.body;
+    if (question.type == "multiple_choice_question") {
+        let listOfOptions = question.options.join("\n- ");
+        listOfOptions = "\n- " + listOfOptions;
+        return string + listOfOptions;
+    } else {
+        return string;
+    }
 }
 
 /**
