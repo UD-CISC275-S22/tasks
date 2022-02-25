98e64926c11a585458bece13e7fcc1a04f41b2c3
jjneilson <neilson.john38781@gmail.com>
2022-02-25 14:41:55 -0500

    finished sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -76,7 +76,11 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return [...questions].reduce(
+        (total: number, questions: Question): number =>
+            questions.published ? total + questions.points : total + 0,
+        0
+    );
 }
 
 /***
