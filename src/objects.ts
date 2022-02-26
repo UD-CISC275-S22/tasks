b7a728893ceb9e39cc29985452888be3e6378259
cuprehan <cuprehan@udel.edu>
2022-02-25 21:06:20 -0500

    completed isCorrect function

--- src/objects.ts
+++ src/objects.ts
@@ -30,7 +30,9 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    return (
+        answer.toLowerCase().trim() === question.expected.toLowerCase().trim()
+    );
 }
 
 /**
