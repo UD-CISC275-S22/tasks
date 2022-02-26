ad15c003a0642c8ddc1d09713117072961ea0bf4
cuprehan <cuprehan@udel.edu>
2022-02-25 21:32:34 -0500

    completed toShortForm function

--- src/objects.ts
+++ src/objects.ts
@@ -56,7 +56,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return question.id + ": " + question.name.slice(0, 10);
 }
 
 /**
