cb4275d1e1c900219b00441e92ede3be86a5ed36
Christian Rullan <laptopchris01@gmail.com>
2022-02-26 01:25:37 -0500

    fixed ESlint error

--- src/arrays.ts
+++ src/arrays.ts
@@ -1,5 +1,3 @@
-import { workerData } from "worker_threads";
-
 /**
  * Consume an array of numbers, and return a new array containing
  * JUST the first and last number. If there are no elements, return
