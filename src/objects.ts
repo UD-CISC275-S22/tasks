054ad58e716a79662e1a21277f99fc1e729b8978
cuprehan <cuprehan@udel.edu>
2022-02-25 23:13:05 -0500

    completed addOption function

--- src/objects.ts
+++ src/objects.ts
@@ -133,7 +133,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const updatedQues = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    return updatedQues;
 }
 
 /**
