7a06d6f0d28db2081b8f39df0e60472bbba855be
Sydney Hester <sydneyph@udel.edu>
2022-02-27 15:01:37 -0500

    Solved SumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -83,7 +83,15 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const publishedQuestions = questions.filter(
+        (question: Question): boolean => question.published
+    );
+    const sum = publishedQuestions.reduce(
+        (currentTotal: number, question: Question): number =>
+            currentTotal + question.points,
+        0
+    );
+    return sum;
 }
 
 /***
