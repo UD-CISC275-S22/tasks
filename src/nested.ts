dcb73b9f849792c44d6be7c0dfed039ef6e0a52f
Sydney Hester <sydneyph@udel.edu>
2022-02-27 15:10:36 -0500

    Solved toCSV

--- src/nested.ts
+++ src/nested.ts
@@ -112,7 +112,21 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const header = "id,name,options,points,published\n";
+    const ContentsStrings = questions.map(
+        (question: Question): string =>
+            question.id +
+            "," +
+            question.name +
+            "," +
+            question.options.length +
+            "," +
+            question.points +
+            "," +
+            question.published
+    );
+    const joinedStrings = ContentsStrings.join("\n");
+    return header + joinedStrings;
 }
 
 /**
