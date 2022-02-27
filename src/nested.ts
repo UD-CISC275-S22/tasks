7ca5a4c9c8ca2446065f332aca57fb10314bd3a4
Sydney Hester <sydneyph@udel.edu>
2022-02-27 14:56:34 -0500

    Solved SumPoints

--- src/nested.ts
+++ src/nested.ts
@@ -71,7 +71,12 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const sum = questions.reduce(
+        (currentTotal: number, question: Question): number =>
+            currentTotal + question.points,
+        0
+    );
+    return sum;
 }
 
 /***
