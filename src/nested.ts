eec26de0c10ee34ae5c205690a343ddf70f68d59
jjneilson <neilson.john38781@gmail.com>
2022-02-28 10:32:14 -0500

    completed publishAll

--- src/nested.ts
+++ src/nested.ts
@@ -134,7 +134,9 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    return questions.map(
+        (question: Question): Question => ({ ...question, published: true })
+    );
 }
 
 /***
