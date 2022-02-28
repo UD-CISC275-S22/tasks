47a8828da713e8e4fbf6db4918c5a470539af09d
Sean Williams <sean475@gmail.com>
2022-02-28 09:45:44 -0500

    Finished RevealAnswer and ChangeType

--- src/App.tsx
+++ src/App.tsx
@@ -18,44 +18,6 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <p>Hello World</p>
-            <p>Sean Williams</p>
-            <h1>Task 3 Stuff:</h1>
-            <img
-                src="https://i.pinimg.com/originals/78/09/94/780994928b848b937791d382de43ef75.jpg"
-                alt="Fox jumping in snow"
-            />
-            <ol>
-                <li>I dont know what to put here</li>
-                <li>Foxes are cute</li>
-                <li>Filler Text</li>
-            </ol>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <Container>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                width: "500px",
-                                height: "200px",
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                    </Col>
-                    <Col>
-                        Small Rectangle
-                        <div
-                            style={{
-                                width: "500px",
-                                height: "100px",
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                    </Col>
-                </Row>
-            </Container>
             <hr></hr>
             <Counter></Counter>
             <hr />
--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,20 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+
+    function flipType(): void {
+        setType(
+            type === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+    return (
+        <div>
+            <button onClick={flipType}>Change Type</button>
+            {type === "short_answer_question" && <div>Short Answer</div>}
+            {type === "multiple_choice_question" && <div>Multiple Choice</div>}
+        </div>
+    );
 }
