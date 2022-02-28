c2177857a742a85942b3845bf002a171725aa589
jleadlove <leadlove@udel.edu>
2022-02-28 15:16:05 -0500

    Completed sumPoints function

--- src/nested.ts
+++ src/nested.ts
@@ -67,7 +67,12 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const totalPoints = questions.reduce(
+        (currentSum: number, question: Question) =>
+            currentSum + question.points,
+        0
+    );
+    return totalPoints;
 }
 
 /***
