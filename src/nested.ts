d0696b428ea519dded87f3d2f2242a668d341678
jleadlove <leadlove@udel.edu>
2022-02-28 15:13:32 -0500

    Completed getNames function

--- src/nested.ts
+++ src/nested.ts
@@ -57,7 +57,10 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    const questionNames = questions.map(
+        (question: Question): string => question.name
+    );
+    return questionNames;
 }
 
 /***
