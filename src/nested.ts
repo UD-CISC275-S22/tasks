cc85d97d78d73a058c8ac113f43f9e60601a0bc0
cuprehan <cuprehan@udel.edu>
2022-03-01 00:42:58 -0500

    completed toCSV function

--- src/nested.ts
+++ src/nested.ts
@@ -110,7 +110,13 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const str = questions
+        .map(
+            (question: Question): string =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`
+        )
+        .join("\n");
+    return "id,name,options,points,published\n" + str;
 }
 
 /**
