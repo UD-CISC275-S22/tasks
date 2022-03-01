d3ff7f008261b8e55c9be3d884414a6a3d58e1bf
Sean Williams <sean475@gmail.com>
2022-03-01 15:13:17 -0500

    Started on CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,29 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holidays =
+    | "Chinese New Year"
+    | "New Years Eve"
+    | "Pi Day"
+    | "Whale's Day"
+    | "World Sleep Day";
+
 export function CycleHoliday(): JSX.Element {
+    const [holiday, setHoliday] = useState<Holidays>("Pi Day");
+    const alph: string[] = [
+        "Chinese New Year🐲",
+        "New Years Eve🎄",
+        "Pi Day🥧",
+        "Whale's Day🐳",
+        "World Sleep Day😴"
+    ];
+    //Need to sort and remove stuff, change to record
+    const year: string[] = [
+        "Chinese New Year🐲",
+        "New Years Eve🎄",
+        "Pi Day🥧",
+        "Whale's Day🐳",
+        "World Sleep Day😴"
+    ];
     return <div>Cycle Holiday</div>;
 }
