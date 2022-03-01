9ce42b564cd5b0c653944b776e5cc29948162bf3
Christian Rullan <laptopchris01@gmail.com>
2022-02-28 19:54:52 -0500

    completed tomarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -88,7 +88,11 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    if (question.type === "multiple_choice_question") {
+        return `# ${question.name}\n${question.body}\n- ${question.options[0]}\n- ${question.options[1]}\n- ${question.options[2]}`;
+    } else {
+        return `# ${question.name}\n${question.body}`;
+    }
 }
 
 /**
