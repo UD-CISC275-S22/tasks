56ab4dbe7e69ba63485cb2eca4ca8f8587a83251
jjneilson <neilson.john38781@gmail.com>
2022-03-02 19:45:13 -0500

    Completed StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,35 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [started, setStarted] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
+
+    function quizStartStop(): void {
+        setStarted(!started);
+        if (!started) {
+            removeAttempt();
+        }
+    }
+
+    function addAttempt(): void {
+        setAttempts(attempts + 1);
+    }
+
+    function removeAttempt(): void {
+        setAttempts(attempts - 1);
+    }
+    return (
+        <div>
+            <Button onClick={quizStartStop} disabled={started || attempts <= 0}>
+                Start Quiz
+            </Button>
+            <Button onClick={quizStartStop} disabled={!started}>
+                Stop Quiz
+            </Button>
+            <Button onClick={addAttempt} disabled={started}>
+                Mulligan
+            </Button>
+            <div>Attempts Remaining: {attempts} </div>
+        </div>
+    );
 }
