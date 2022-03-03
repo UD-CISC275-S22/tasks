9322c2662f95b76dce3a93e1904400dc74194f1d
jjneilson <neilson.john38781@gmail.com>
2022-03-02 19:02:34 -0500

    Completed RevealAnswer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,15 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(false);
+
+    function invertVisibility(): void {
+        setVisible(!visible);
+    }
+    return (
+        <div>
+            <Button onClick={invertVisibility}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
