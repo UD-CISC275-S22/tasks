cce420344b6c44e4dbe2170122d3b172a882304f
jjneilson <neilson.john38781@gmail.com>
2022-03-03 14:21:58 -0500

    Completed CylcleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,57 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("Easter");
+
+    function advanceYear(): void {
+        if (holiday === "Easter") {
+            setHoliday("Fourth of July");
+        } else if (holiday === "Fourth of July") {
+            setHoliday("Halloween");
+        } else if (holiday === "Halloween") {
+            setHoliday("Christmas");
+        } else if (holiday === "Christmas") {
+            setHoliday("New Years Eve");
+        } else {
+            setHoliday("Easter");
+        }
+    }
+
+    function advanceAlpha(): void {
+        if (holiday === "Easter") {
+            setHoliday("Fourth of July");
+        } else if (holiday === "Fourth of July") {
+            setHoliday("Halloween");
+        } else if (holiday === "Halloween") {
+            setHoliday("New Years Eve");
+        } else if (holiday === "Christmas") {
+            setHoliday("Easter");
+        } else {
+            setHoliday("Christmas");
+        }
+    }
+
+    function emoji(): string {
+        if (holiday === "Easter") {
+            return "🐇";
+        } else if (holiday === "Fourth of July") {
+            return "🎆";
+        } else if (holiday === "Halloween") {
+            return "🎃";
+        } else if (holiday === "Christmas") {
+            return "🎁";
+        } else {
+            return "🥳";
+        }
+    }
+
+    return (
+        <div>
+            <Button onClick={advanceYear}>Advance by Year</Button>
+            <Button onClick={advanceAlpha}>Advance by Alphabet</Button>
+            <div>
+                Holiday: {emoji()} {holiday}
+            </div>
+        </div>
+    );
 }
