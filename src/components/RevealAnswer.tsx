df06865201b06288896ee395803b414386f929fb
suryanuj <suryanuj@udel.edu>
2022-03-03 13:09:03 -0500

    finished

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,18 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(true);
+
+    function flipVisibility(): void {
+        // Set visible to be the logical opposite of its previous value
+        setVisible(!visible);
+    }
+
+    // Only includes <div>Hello!</div> if `visible` is true
+    return (
+        <div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {!visible && <div>42</div>}
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+import { resourceLimits } from "worker_threads";
 
 /**
  * Here is a helper function you *must* use to "roll" your die.
@@ -12,5 +13,33 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftdie, setLeftDie] = useState<number>(1);
+    const [rightdie, setRightDie] = useState<number>(3);
+
+    function result(): string {
+        if (leftdie === 1 && rightdie === 1) {
+            return "lose";
+        } else if (leftdie === rightdie) {
+            return "win";
+        } else {
+            return "";
+        }
+    }
+
+    return (
+        <div>
+            Two Dice
+            <div>
+                <Button onClick={() => setLeftDie(d6)}>Roll Left</Button>
+            </div>
+            <div>
+                <Button onClick={() => setRightDie(d6)}>Roll Right</Button>
+            </div>
+            <div>
+                <span data-testid="left-die"> Left: {leftdie}</span>
+                <span data-testid="right-die"> Right: {rightdie}</span>
+            </div>
+            <span> result: {result()} </span>
+        </div>
+    );
 }
