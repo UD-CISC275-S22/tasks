1e3b210b7a388874fef60cf53284c844064dad8b
Sydney <sydneyph@udel.edu>
2022-03-03 14:13:56 -0500

    Attempting to fix changeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,18 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    // My functions for Reveal Answer 👺
+    const [visible, setVisible] = useState<boolean>(false);
+
+    function flipVisibility(): void {
+        // Set visible to be the logical opposite of its previous value
+        setVisible(!visible);
+    }
+    return (
+        <div>
+            <div>Change Type</div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
