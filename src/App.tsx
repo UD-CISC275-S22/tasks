e61eb309cf33df096117b95f9ee22d26e97fd4b7
Sean Williams <sean475@gmail.com>
2022-03-03 18:43:34 -0500

    Finished DoubleHalf

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App(): JSX.Element {
                 automatically reload.
             </p>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
