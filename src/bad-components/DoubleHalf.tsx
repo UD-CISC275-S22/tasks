bd25ecf9421e01e71e89f498384ea56ee4611f2b
andrewl15 <andrewl@udel.edu>
2022-03-03 19:01:09 -0500

    got DoubleHalf done

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+//import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
+interface helper {
+    dhValue: number;
+    setDhValue: (x: number) => void;
+}
+function Doubler({ dhValue, setDhValue }: helper): JSX.Element {
     return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
 }
 
-function Halver(): JSX.Element {
+function Halver({ dhValue, setDhValue }: helper): JSX.Element {
     return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
 }
 
 export function DoubleHalf(): JSX.Element {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [dhValue, setDhValue] = useState<number>(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler dhValue={dhValue} setDhValue={setDhValue} />
+            <Halver dhValue={dhValue} setDhValue={setDhValue} />
         </div>
     );
 }
