4a5a6aab952a9183fdf623442fa39df5d7b161c2
Isaac <isaacl@udel.edu>
2022-03-06 15:15:20 -0500

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -10,42 +10,15 @@ import { Counter } from "./components/Counter";
 
 function App(): JSX.Element {
     return (
-        <>
-            <>
-                <h1 className="csn">Crosby, Stills and Nash</h1>
-                <img
-                    src="https://i.imgur.com/6xrGsGi.jpg"
-                    alt="Crosby, Stills and Nash"
-                />
-                <header className="App-header"> </header>
-                Hello World <br></br> Isaac Lewis <br></br> UD CISC275 with
-                React Hooks and TypeScript <br></br>
-                <Button onClick={() => console.log("Hello World!")}>
-                    Log Hello World
-                </Button>
-                <ol>
-                    <li>Crosby</li>
-                    <li>Stills</li>
-                    <li>Nash</li>
-                </ol>
-                <Container>
-                    <Row>
-                        <Col>
-                            Left Column
-                            <div className="rectangle"></div>
-                        </Col>
-                        <Col>
-                            Right Column
-                            <div className="rectangle"></div>
-                        </Col>
-                    </Row>
-                </Container>
-                <p>
-                    Edit <code>src/App.tsx</code> and save. This page will
-                    automatically reload.
-                </p>
-            </>
-        </>
+        <div className="App">
+            <header className="App-header">
+                UD CISC275 with React Hooks and Typescript
+            </header>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload.
+            </p>
+        </div>
     );
 }
 
