d0e9d947f3bf656dc29221c500322805489c2c37
Zhiwen Zhu <zhiwenzhu@wifi-roaming-128-4-98-226.wifi.udel.edu>
2022-03-07 09:29:59 -0500

    remove non-used code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./SurveyCorps.jpeg";
-import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
@@ -16,49 +14,6 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
                 <h1>This is also a header</h1>
             </header>
-<<<<<<< HEAD
-            <ul>
-                <li>Maria Wall</li>
-                <li>Rose Wall</li>
-                <li>Sina Wall</li>
-            </ul>
-            <div>
-                <Container>
-                    <Row>
-                        <Col className="box">Attack titan</Col>
-                        <Col className="box1">Armored titan</Col>
-                        <Col className="box2">Beast titan</Col>
-                    </Row>
-                </Container>
-            </div>
-            <div>
-                <Container>
-                    <Row>
-                        <Col className="box">Female titan</Col>
-                        <Col className="box1">Jaw titan</Col>
-                        <Col className="box2">Founding titan</Col>
-                    </Row>
-                </Container>
-            </div>
-            <div>
-                <Container>
-                    <Row>
-                        <Col className="box">Colossal titan</Col>
-                        <Col className="box1">Warhammer titan</Col>
-                        <Col className="box2">Cart titan</Col>
-                    </Row>
-                </Container>
-            </div>
-            <p>
-                <img src={logo} alt="Survey Corps" width="60" height="60" />
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload Edited by Zhiwen Zhu. Hello World
-            </p>
-
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -71,7 +26,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
