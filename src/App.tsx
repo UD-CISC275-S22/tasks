09d3a8ac7b48f3a94266cedeba59c31769e4022e
oceanshen <oshen@udel.edu>
2022-03-07 12:37:42 -0500

    initial commit to save any loose changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,6 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): JSX.Element {
     return (
@@ -64,14 +60,7 @@ function App(): JSX.Element {
                     </Col>
                 </Row>
             </Container>
-            <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
+
             <hr></hr>
             <Counter></Counter>
             <hr />
