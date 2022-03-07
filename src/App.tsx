71a231243dac66ce6f47950781948361ae499c7b
oceanshen <oshen@udel.edu>
2022-03-07 11:38:45 -0500

    task 8 completed

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,53 @@ function App(): JSX.Element {
             <header className="App-header">
                 Ocean Shen: UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>Hello World!</h1>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <p></p>
+            <Container>
+                <Row>
+                    <Col>
+                        Who is Ocean Shen?
+                        <ul>
+                            <li>I am a sophomore CS and STAT double major.</li>
+                            <li>I like to code, eat, and sleep.</li>
+                            <li>
+                                I was introduced to programming through Python.
+                            </li>
+                        </ul>
+                        <div
+                            style={{
+                                height: 50,
+                                width: 200,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <p>
+                            Here is a picture of food I like to eat when coding:
+                        </p>
+                        <img
+                            height="70%"
+                            width="50%"
+                            src="https://media.chefdehome.com/740/0/0/salsa/homemade-salsa.jpg"
+                            alt="A picture of chips being dipped in salsa"
+                        />
+                        <p></p>
+                        <div
+                            style={{
+                                height: 50,
+                                width: 200,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        <p></p>
+                    </Col>
+                </Row>
+            </Container>
+
             <hr></hr>
             <Counter></Counter>
             <hr />
--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,28 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function changeQuestionType(): void {
+        if (questionType === "short_answer_question") {
+            setQuestionType("multiple_choice_question");
+        } else {
+            setQuestionType("short_answer_question");
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <Button onClick={changeQuestionType}>Change Type</Button>
+                {questionType === "multiple_choice_question" && (
+                    <div>Multiple Choice</div>
+                )}
+                {questionType === "short_answer_question" && (
+                    <div>Short Answer</div>
+                )}
+            </div>
+        </div>
+    );
 }
