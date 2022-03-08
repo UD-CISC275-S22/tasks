968c8078a9cf0ff883491a9d1a2b3a43afa3cdb8
jjneilson <neilson.john38781@gmail.com>
2022-03-08 15:42:14 -0500

    Completed CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,33 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [input, setInput] = useState<string>("");
+
+    function updateInput(event: ChangeEvent): void {
+        setInput(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group>
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={input}
+                    onChange={updateInput}
+                />
+            </Form.Group>
+            {expectedAnswer === input && <div>✔️</div>}
+            {expectedAnswer !== input && <div>❌</div>}
         </div>
     );
 }
