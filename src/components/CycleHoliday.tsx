2e80989812bf315103e69bf3eff45051ddd2244a
cuprehan <cuprehan@udel.edu>
2022-03-09 00:25:14 -0500

    completed CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,43 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("💘");
+
+    const holidaysByDate: Record<string, string> = {
+        "💘": "☘️",
+        "☘️": "🤱",
+        "🤱": "🎂",
+        "🎂": "🎃",
+        "🎃": "💘"
+    };
+
+    const holidaysByAlpha: Record<string, string> = {
+        "🎂": "🎃",
+        "🎃": "🤱",
+        "🤱": "☘️",
+        "☘️": "💘",
+        "💘": "🎂"
+    };
+
+    function changeByDate(): void {
+        const newHoliday = holidaysByDate[holiday];
+        setHoliday(newHoliday);
+    }
+
+    function changeByAlpha(): void {
+        const newHoliday = holidaysByAlpha[holiday];
+        setHoliday(newHoliday);
+    }
+
+    return (
+        <>
+            <div>
+                <Button onClick={changeByDate}>Advance by Year</Button>
+            </div>
+            <div>
+                <Button onClick={changeByAlpha}>Advance by Alphabet</Button>
+            </div>
+            <div>Holiday: {holiday}</div>
+        </>
+    );
 }
