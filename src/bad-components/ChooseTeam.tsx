8bb8c48d6d3eb2296b92a779807c33384763d735
awrose <79182069+awrose@users.noreply.github.com>
2022-03-09 19:38:15 -0500

    chooseTeam

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, Row, Col } from "react-bootstrap";
+//import { setTokenSourceMapRange } from "typescript";
 
 const PEOPLE = [
     "Alan Turing",
@@ -11,17 +12,17 @@ const PEOPLE = [
 ];
 
 export function ChooseTeam(): JSX.Element {
-    const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
+    const [allOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-       // if (!team.includes(newMember)) {
-       //     team.push(newMember);
-       // }
+    function chooseMember(newMember: string) {
+        if (!team.includes(newMember)) {
+            setTeam([...team, newMember]);
+        }
     }
 
     function clearTeam() {
-        //team = [];
+        setTeam([]);
     }
 
     return (
@@ -32,7 +33,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
