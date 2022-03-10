975b32d3cb03dd73ea40f56d9de0eec5e0c5ca3e
Yuchen Zhang <zyc@udel.edu>
2022-03-09 23:02:32 -0500

    commited

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,14 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(false);
+    function flipVisibility(): void {
+        setVisible(!visible);
+    }
+    return (
+        <div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
