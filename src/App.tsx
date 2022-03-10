5ce71e8099054654a823afcab990331cbd6d603d
Yuchen Zhang <zyc@udel.edu>
2022-03-09 23:14:13 -0500

    com

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+
 import "./App.css";
+
 import { ChangeType } from "./components/ChangeType";
+
 import { RevealAnswer } from "./components/RevealAnswer";
+
 import { StartAttempt } from "./components/StartAttempt";
+
 import { TwoDice } from "./components/TwoDice";
+
 import { CycleHoliday } from "./components/CycleHoliday";
+
 import { Counter } from "./components/Counter";
 
 function App(): JSX.Element {
