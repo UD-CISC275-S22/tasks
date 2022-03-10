aa3e15d0c77cbff6d81ce7a15fccbcb79981a75b
Yuchen Zhang <zyc@udel.edu>
2022-03-09 23:02:49 -0500

    commited

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,44 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [progress, setprogress] = useState<boolean>(false);
+    const [attempts, setattempts] = useState<number>(4);
+
+    function changeProcess(): void {
+        setprogress(!progress);
+    }
+    function reduceProgress(): void {
+        setattempts(attempts - 1);
+    }
+    function clickStartQuiz(): void {
+        changeProcess();
+        reduceProgress();
+    }
+    function addProgress(): void {
+        setattempts(attempts + 1);
+    }
+
+    return (
+        <div>
+            <span>{attempts}</span>
+            <div>
+                <Button
+                    onClick={clickStartQuiz}
+                    disabled={progress || attempts === 0}
+                >
+                    Start Quiz
+                </Button>
+            </div>
+            <div>
+                <Button onClick={changeProcess} disabled={!progress}>
+                    Stop Quiz
+                </Button>
+            </div>
+            <div>
+                <Button onClick={addProgress} disabled={progress}>
+                    Mulligan
+                </Button>
+            </div>
+        </div>
+    );
 }
