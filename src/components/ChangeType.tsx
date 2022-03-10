d2151e0039e1cb62c9b9bb3178cc568e85508395
Yuchen Zhang <zyc@udel.edu>
2022-03-09 23:01:50 -0500

    commited

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,22 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    function setChangeType(): void {
+        setType(
+            type === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+    return (
+        <div>
+            <Button onClick={setChangeType}>Change Type</Button>
+            {type === "short_answer_question" ? (
+                <div>Short Answer</div>
+            ) : (
+                <div>Multiple Choice</div>
+            )}
+        </div>
+    );
 }
