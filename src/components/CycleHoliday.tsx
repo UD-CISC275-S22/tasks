e22a353cd9f6fbfb1cae16701a8ec089cb9316ea
Yuchen Zhang <zyc@udel.edu>
2022-03-09 23:02:14 -0500

    commited

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,48 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type Holiday =
+        | "Holiday: 🎃"
+        | "Holiday: 🎏"
+        | "Holiday: 🪔"
+        | "Holiday: 🎁"
+        | "Holiday: 🏮";
+
+    const byAlphabet: Record<Holiday, Holiday> = {
+        "Holiday: 🎏": "Holiday: 🪔",
+        "Holiday: 🪔": "Holiday: 🎃",
+        "Holiday: 🎃": "Holiday: 🏮",
+        "Holiday: 🏮": "Holiday: 🎁",
+        "Holiday: 🎁": "Holiday: 🎏"
+    };
+
+    const byyear: Record<Holiday, Holiday> = {
+        "Holiday: 🏮": "Holiday: 🎏",
+        "Holiday: 🎏": "Holiday: 🪔",
+        "Holiday: 🪔": "Holiday: 🎃",
+        "Holiday: 🎃": "Holiday: 🎁",
+        "Holiday: 🎁": "Holiday: 🏮"
+    };
+
+    const [holiday, setholiday] = useState<Holiday>("Holiday: 🎃");
+    function changebyAlphabet(): void {
+        const newbyAlphabet = byAlphabet[holiday];
+        setholiday(newbyAlphabet);
+    }
+    function changebyYear(): void {
+        const newbyYear = byyear[holiday];
+        setholiday(newbyYear);
+    }
+
+    return (
+        <div>
+            <div>
+                <Button onClick={changebyAlphabet}>Advance by Alphabet</Button>
+            </div>
+            <div>
+                <Button onClick={changebyYear}>Advance by Year</Button>
+            </div>
+            <div>{holiday}</div>
+        </div>
+    );
 }
