32ce747fc01d9dc4df4f435d8dfefadca41db2ea
awrose <79182069+awrose@users.noreply.github.com>
2022-03-11 17:58:35 -0500

    finally finished giveattempts

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,46 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    //has 2 states controlled by a switch; default state: text indicating the user's name and whether they are a student
+    //alternate state: editable form for editing the user's name and whether they are a student
+
+    //state: handle the component being in "edit mode", the user's name, and whether or not the user is a student
+    //INITIALLY: the component is NOT in edit moode, the user's name is literally the string "Your name" and the user IS a student
+
+    //state
+    const [editMode, setEditMode] = useState<boolean>(true);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
+    //control
+    function updateEdit(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(event.target.checked);
+    }
+
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+
+    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="is-in-edit-mode"
+                label="edit?"
+                checked={editMode}
+                onChange={updateEdit}
+            />
+            <div>
+                {isStudent
+                    ? "{name} is a student."
+                    : "{name} is not a student."}
+                .
+            </div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,55 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    //need: state to represent # of attempts the user has left + another state to represent the # of attempts they are requesting-
+    //numeric input box where the user can specify their requested number of attempts-
+    //two buttons
+    //button 1: use - decreases the attempts by one
+    //button 2: gain - increase the attempts by the amount in the input box
+
+    //if the user attempts to request an invalid amount that can't be parsed as an int, don't change their number of attempts
+
+    //simplify type definition of the changeEvent
+    type ChangeEvent = React.ChangeEvent<
+        HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+    >;
+
+    //state
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsRequested, setAttemptsRequested] = useState<number>(0);
+
+    function loseAttempt() {
+        const attempts = attemptsLeft - 1 || 0;
+        setAttemptsLeft(attempts);
+    }
+
+    function gainAttempt() {
+        const attempts = attemptsLeft + attemptsRequested || 0;
+        setAttemptsLeft(attempts);
+    }
+
     return (
+        //<div>
+        //<h3>Give Attempts</h3>
+        //</div>
         <div>
-            <h3>Give Attempts</h3>
+            <Form.Group controlId="Attempts">
+                <Form.Label>Attempts: {attemptsLeft}</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attemptsRequested}
+                    onChange={(event: ChangeEvent) =>
+                        setAttemptsRequested(parseInt(event.target.value))
+                    }
+                />
+            </Form.Group>
+            <div>
+                <Button onClick={loseAttempt} disabled={attemptsLeft === 0}>
+                    Use
+                </Button>
+                <Button onClick={gainAttempt}>Gain</Button>
+            </div>
         </div>
     );
 }
