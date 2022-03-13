9b849e1717b40e01b0b41996d78bc6611ab9435e
Zhiwen Zhu <zhiwenzhu@Zhiwens-MacBook-Pro.local>
2022-03-12 23:07:35 -0500

    task10

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,51 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
+type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
 export function EditMode(): JSX.Element {
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, SetisStudent] = useState<boolean>(true);
+    const [isEdit, setisEdit] = useState<boolean>(false);
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <div>
+                <Form.Check
+                    type="switch"
+                    id="is-edit-mode"
+                    label="Editing"
+                    checked={isEdit}
+                    onChange={() => setisEdit(!isEdit)}
+                />
+            </div>
+            <div>
+                {isEdit ? (
+                    <Form.Group controlId="FormPersonName">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            value={name}
+                            onChange={(event: ChangeEvent) =>
+                                setName(event.target.value)
+                            }
+                        />
+                        <Form.Check
+                            type="switch"
+                            id="isStudent"
+                            label="is Student?"
+                            checked={isStudent}
+                            onChange={(event: ChangeEvent) =>
+                                SetisStudent(event.target.checked)
+                            }
+                        />
+                    </Form.Group>
+                ) : (
+                    <div>
+                        {name} is
+                        {isStudent ? " a student." : " not a student."}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setanswer] = useState<string>(options[0]);
+    function updatechoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setanswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="userChoice">
+                <Form.Label>What is ypur choice?</Form.Label>
+                <Form.Select value={answer} onChange={updatechoice}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {options}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {answer === expectedAnswer ? <span>✔️</span> : <span>❌</span>}
         </div>
     );
 }
