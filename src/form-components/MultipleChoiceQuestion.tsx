ada8baf306e70849ddabce39632d619952da9e3c
oceanshen <oshen@udel.edu>
2022-03-12 23:21:31 -0500

    initial mc commit

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +12,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+
+    function updateAnswer(event: ChangeEvent) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Label>Make a selection below:</Form.Label>
+            <Form.Select value={answer} onChange={updateAnswer}>
+                {options.map((option: string) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </Form.Select>
+            {answer === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
