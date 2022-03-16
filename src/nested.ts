2da9255591a8efeb3f9e9a27ab1b4a307cbfd704
marvindang <67122144+marvindang@users.noreply.github.com>
2022-03-15 22:22:40 -0400

    task 7 all but one

--- src/nested.ts
+++ src/nested.ts
@@ -1,6 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
-import { duplicateQuestion } from "./objects";
+import { duplicateQuestion, makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
