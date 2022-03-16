62c602a505bf7924140fc9004ba2cfc0791d8df2
jjneilson <neilson.john38781@gmail.com>
2022-03-16 16:23:05 -0400

    Setup image and list in quizzer

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import appSketch from "../Assets/sketch.jpg";
 
 export function Quizzer(): JSX.Element {
-    return <h3>Quizzer</h3>;
+    return (
+        <div>
+            <h3>Quizzer</h3>
+            <img src={appSketch} alt="App Sketch" />
+            <ul>
+                <li>Added Sketch</li>
+            </ul>
+        </div>
+    );
 }
