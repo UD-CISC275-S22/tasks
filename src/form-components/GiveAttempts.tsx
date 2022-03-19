e4158bd9a677c770b177e25e3d63e100859feed9
oceanshen <oshen@udel.edu>
2022-03-18 20:59:16 -0400

    initial give attempts commit

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -6,9 +6,32 @@ type ChangeEvent = React.ChangeEvent<
 >;
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [requests, setRequests] = useState<number>(0);
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Group controlId="requestedAttempts">
+                <Form.Label>Number of requested attempts:</Form.Label>
+                <Form.Control
+                    value={requests}
+                    type="number"
+                    onChange={(event: ChangeEvent) =>
+                        setRequests(parseInt(event.target.value))
+                    }
+                />
+            </Form.Group>
+            <Button
+                onClick={() => setAttempts(attempts - 1)}
+                disabled={attempts === 0}
+            >
+                Use
+            </Button>
+            <Button onClick={() => setAttempts(attempts + requests)}>
+                Gain
+            </Button>
+            Number of attempts left: {attempts}
         </div>
     );
 }
