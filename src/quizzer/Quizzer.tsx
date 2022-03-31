a2d3d047ab5aa0a5b21da7bab8379ee681722058
awrose <79182069+awrose@users.noreply.github.com>
2022-03-31 11:57:22 -0400

    trying to push

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,11 +1,12 @@
 //have everything displayed here, similar to the "App.tsx" in the example
 
-import React, { useState } from "react";
-import quizzes from "./Data/quizes.json";
-import { Quiz } from "./Interfaces/quiz";
+//import React, { useState } from "react";
+//import quizzes from "./Data/quizes.json";
+//import { Quiz } from "./Interfaces/quiz";
+import React from "react";
 
-const QUIZZES = quizzes.map((quiz): Quiz => ({ ...quiz }));
+//const QUIZZES = quizzes.map((quiz): Quiz => ({ ...quiz }));
 export function Quizzer(): JSX.Element {
-    const [quizzes, setQuizzes] = useState<Quiz[]>(QUIZZES);
+//    const [quizzes, setQuizzes] = useState<Quiz[]>(QUIZZES);
     return <h3>Quizzer</h3>;
 }
