3a7ad2bcfc59e32be7b1b1dbba467b289675b989
imjackson <development@jacksongpack.com>
2022-08-31 17:23:21 -0400

    task [2]: add "Hello World"

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
-                <p>My name is Jackson Gunn Pack</p>
+                <p>Hello World, my name is Jackson Gunn Pack</p>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
