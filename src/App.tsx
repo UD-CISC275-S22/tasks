a120b31b6a059b9d43476ca834f6281dc42ae383
Blade Tyrrell <bladetyr@udel.edu>
2022-09-01 14:38:55 -0400

    hello world tests added

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Blade was here B]
+                automatically reload. Blade was here B] and Hello World
             </p>
         </div>
     );
