e1712127f8202ebd410b01fc20677f9ed17e6a42
Blair <blairf@udel.edu>
2022-09-01 12:10:10 -0400

    Hello World added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from "react";
 import "./App.css";
 
@@ -5,12 +6,13 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Blair Felker
+                UD CISC275 with React Hooks and TypeScript by Blair Felker
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>Hello World!</p>
         </div>
     );
 }
