bf841fb51140dec60fbc78abd88992dee31e8328
Patrick Harris <pdharris@udel.edu>
2022-09-02 17:30:47 -0400

    Added Hello World to the Body

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript by Patrick Harris
-                Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>Hello World</p>
         </div>
     );
 }
