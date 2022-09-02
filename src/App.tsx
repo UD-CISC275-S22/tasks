51d657ff220dcff399bfe2207c7c8707664c7807
Nora668 <norapan668@gmail.com>
2022-09-02 18:07:58 -0400

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Editor: Yuqing Pan
+                automatically reload. Editor: Yuqing Pan Hello World
             </p>
         </div>
     );
