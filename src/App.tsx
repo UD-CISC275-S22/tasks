6ffa63c6fea2bf6ebfe50549c0960edefd5ceed8
Patrick Harris <pdharris@udel.edu>
2022-09-02 17:08:52 -0400

    Deleted Hello World for a test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript by Patrick Harris
-                Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
