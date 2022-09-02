c382cfb0556d7a8951471b3e8e9858088b45600a
emba00 <emba@udel.edu>
2022-09-02 14:13:44 -0400

    Hello World :)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript and Emmanuel Mbah
+                UD CISC275 featuring Emmanuel Mbah and Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
