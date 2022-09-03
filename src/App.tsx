7c4b5ab55b8073db9302dac2c3478c889d5f5c48
Ryan Gilmartin <ryan22gil@gmail.com>
2022-09-02 21:58:09 -0400

    back to original

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                This is Ryan Gilmartins Site
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload.
             </p>
         </div>
     );
