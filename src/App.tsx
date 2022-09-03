1283e48b3270b528fb31c118c3b5770d18c2aca7
Xu <dxu@udel.edu>
2022-09-02 22:55:46 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with Reaction Hooks and TypeScript
             </header>
             <p>
                 Daniel Xu has edited <code>src/App.tsx</code> and saved. This
