a6be2f84c55934662d0772ef8855cec88a25aeb0
Ryan Gilmartin <ryan22gil@gmail.com>
2022-09-02 22:11:46 -0400

    passes tests

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Ryan Gilmartins website</p>
+            <p>Hello World, Ryan Gilmartins website</p>
         </div>
     );
 }
