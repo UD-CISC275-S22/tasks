c7abf04c52e514a854487ba8486380846701cfd5
AlejSilva <112499536+AlejSilva@users.noreply.github.com>
2022-09-06 17:06:47 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Task 3.
+                automatically reload. Task 2.
             </p>
         </div>
     );
