8f36c6599d94413fdc363795a2e350ab03c88cc2
Ryan Gilmartin <ryan22gil@gmail.com>
2022-09-08 14:23:17 -0400

    solved, passing tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,7 +8,38 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Hello World, Ryan Gilmartins website</p>
+            <Container>
+                <Row>
+                    <Col>
+                        <img
+                            src="https://www.animalspot.net/wp-content/uploads/2020/01/Types-of-Beetles.jpg"
+                            alt="different types of beetles"
+                            style={{ height: "400px" }}
+                        />
+                    </Col>
+                    <Col>
+                        <h3>Types of beetles</h3>
+                        <ul>
+                            <li>Cool ones</li>
+                            <li>Small ones</li>
+                            <li>Big ones</li>
+                            <li>Shiny ones</li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+
+            <div
+                style={{
+                    height: "200px",
+                    width: "250px",
+                    backgroundColor: "red"
+                }}
+            ></div>
         </div>
     );
 }
