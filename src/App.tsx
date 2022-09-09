e1c1df84c8cc1e89fdd5b4bd477807fa79e75d7a
Joshua Artis <joshartis0090@gmail.com>
2022-09-09 19:33:56 -0400

    Task 3 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
+        <div
+            className="App"
+            style={{ border: "1px solid blue", padding: "4px" }}
+        >
             <header className="App-header">
-                Joshua Artis in UD CISC275 with React Hooks and TypeScript
+                Hello World, Joshua Artis in UD CISC275 with React Hooks and
+                TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <h1>This is header text</h1>;
+            <img src="/src/land.jpg" alt="A picture of land" />
+            Ordered List:
+            <ol>
+                <li>First thing</li>
+                <li>Another thing</li>
+                <li>A third item</li>
+            </ol>
+            <Button onClick={() => console.log("Hello World!")}>
+                {" "}
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <div
+                        className="ColStyle"
+                        style={{ border: "1px solid red", padding: "4px" }}
+                    >
+                        <Col xs={6}>First column.</Col>
+                    </div>
+                    <Col>Second column.</Col>
+                </Row>
+            </Container>
         </div>
     );
 }
