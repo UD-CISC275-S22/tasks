058ce23aeae83435367ee9e0464d1cc2e3d3045d
Sean <sosull@udel.edu>
2022-09-09 11:52:14 -0400

    add stuff for task 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Name: Sean OSullivan. Hello World!!
-            </p>
+        <div>
+            <div className="App">
+                <Button onClick={() => console.log("Hello World")}>
+                    Log Hello World
+                </Button>
+                Unordered List:
+                <ul>
+                    <li>Thing1</li>
+                    <li>Thing2</li>
+                    <li>Thing3</li>
+                </ul>
+                <header className="App-header">
+                    UD CISC275 with React Hooks and TypeScript
+                </header>
+                <h1> New Header Text Goes here</h1>
+                <img
+                    src="../assets/randcake.png"
+                    alt="A cursed picture of cake"
+                />
+                <p>
+                    Edit <code>src/App.tsx</code> and save. This page will
+                    automatically reload. Name: Sean OSullivan. Hello World!!
+                </p>
+            </div>
+            <div className="newdiv">
+                <Container>
+                    <Row>
+                        <Col className="col1">c1</Col>
+                        <Col className="col2">c2</Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
