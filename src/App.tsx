e031248fe0e59b68307e74c06acac7a9458d6a38
Sean <sosull@udel.edu>
2022-09-09 12:35:00 -0400

    some small change in h1

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App(): JSX.Element {
                 <header className="App-header">
                     UD CISC275 with React Hooks and TypeScript
                 </header>
-                <h1> New Header Text Goes here</h1>
+                <h1 className="App-header"> New Header Text Goes here</h1>
                 <img
                     src={require("./assets/randcake.png")}
                     alt="A cursed picture of cake"
