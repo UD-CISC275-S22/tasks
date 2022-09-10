e7a5f80a5323a6fa76ecfd03c24cd81f310fb05b
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2022-09-10 13:33:47 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
+        <div className="App / Zachariah Barraza">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript (Zachariah Barraza /
+                SamuraiZach)
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
