9c17f060097ec168c6dbf7462b366d27825fbc5c
nafisama <nafisama@udel.com>
2022-09-09 22:40:22 -0400

    Task 3: passed all the tests, and added the rectangles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 React Hooks TypeScript Hello World, Nafisa Maryam
+                UD CISC275 React Hooks TypeScript , Nafisa Maryam
             </header>
+            <h1>This is another header added for testing, Hello World</h1>
+            <img
+                src="C:\Users\NM\Documents\cute_Cat_for_275.png"
+                alt="A picture of a cute ginger cat"
+            />
+            Things I love about cats:
+            <ul>
+                <li>They are cute </li>
+                <li>They are soo lovable </li>
+                <li>They are majestically superior ;| </li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <Container>
+                <Row>
+                    <Col>
+                        First column. <div id="rectangle"></div>
+                    </Col>
+                    <Col>
+                        Second column. <div id="rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
