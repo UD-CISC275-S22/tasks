b75dc5be2b337f64139950c22e44dd37dc0a81ca
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2022-09-10 14:26:24 -0400

    commit

--- src/text.test.tsx
+++ src/text.test.tsx
@@ -6,5 +6,4 @@ test("renders the text 'Hello World' somewhere", () => {
     render(<App />);
     const texts = screen.getAllByText(/Hello World/);
     expect(texts.length).toBeGreaterThanOrEqual(1);
-    ("Hello World");
 });
