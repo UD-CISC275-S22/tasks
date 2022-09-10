a4ead9eb50c101f8b1aad9719d0cd0943a55dcc3
Umut Kurt <kizildoktor@gmail.com>
2022-09-09 21:53:38 -0400

    Completed TASK 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,7 +8,44 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Umut Kurt</p>
+            <h1>Umut Kurt</h1>
+            <p>Hello World</p>
+            <img
+                src="https://i.imgur.com/WGovLth.png"
+                width="200"
+                height="200"
+                alt="I like this image."
+            />
+            <ol>
+                <li>Item number one.</li>
+                <li>Item number two.</li>
+                <li>Item number three</li>
+            </ol>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "200px",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "200px",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
