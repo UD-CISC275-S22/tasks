299c33faa43d5ab5180a6f61fd5fcb6ca6b50b23
Binghong Liu <binghong@udel.edu>
2022-09-09 23:03:51 -0400

    Added HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 import "./App.css";
+import Oreo from "../src/assets/images/Oreo-burger.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <h1>UD CISC275 with React Hooks and TypeScript</h1>
+                <img src={Oreo} width="200" height="200" alt="Oreo Burger" />
             </header>
-            <p>Binghong Liu Hello World</p>
+            <p>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div className="rectangle"></div>
+                            <ul>
+                                <li>Name is Bing</li>
+                                <li>unordered list I trying</li>
+                                <li>totally not rhyming</li>
+                            </ul>
+                        </Col>
+                        <Col>
+                            <div className="rectangle"></div>
+                            <Button onClick={() => console.log("Hello World!")}>
+                                Log Hello World
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </p>
         </div>
     );
 }
