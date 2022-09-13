35f5b7c614c25ec44a204b87cfdcdecdc9c3040b
Frumple <frumplequeef@gmail.com>
2022-09-12 20:24:08 -0400

    Finished shout, isQuestion, and convertYesNo functions

--- src/functions.ts
+++ src/functions.ts
@@ -1,4 +1,5 @@
 import { totalmem } from "os";
+import { stringify } from "querystring";
 
 /**
  * Consumes a single temperature in Fahrenheit (a number) and converts to Celsius
@@ -32,7 +33,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -40,7 +41,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.charAt(message.length - 1) === "?";
 }
 
 /**
@@ -49,5 +50,10 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toUpperCase() === "YES") {
+        return true;
+    } else if (word.toUpperCase() === "NO") {
+        return false;
+    }
+    return null;
 }
