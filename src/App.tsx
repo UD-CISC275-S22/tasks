7ecf4a5cb45d1c4f5f76f3bd67c68635296d2bb2
BinghongLiu <112732580+BinghongLiu@users.noreply.github.com>
2022-09-13 12:00:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 
             </header>
             <p>Binghong Liu</p>
         </div>
