5d955f18f88e707c88c4c1edab97624868f3aaeb
Madeline Pearce <pearcemaddie@gmail.com>
2022-09-21 19:35:29 -0400

    solved arrays for the millionth time

--- src/arrays.ts
+++ src/arrays.ts
@@ -1,6 +1,3 @@
-import { privateDecrypt } from "crypto";
-import { arrayBuffer } from "stream/consumers";
-
 /**
  * Consume an array of numbers, and return a new array containing
  * JUST the first and last number. If there are no elements, return
