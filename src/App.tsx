71a098399637674360c06833a57e3367b0f40662
Dina Dawood <ddawood@udel.edu>
2022-09-20 22:38:34 -0400

    Added "Hello World!"

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Name: Dina Dawood
+                Edit <code>src/App.tsx</code> and save. Hello World! This page
+                will automatically reload. Name: Dina Dawood
             </p>
         </div>
     );
