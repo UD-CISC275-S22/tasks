62f7037fb4a1f7e65134921ea8b2f716f36ec723
Dina Dawood <ddawood@udel.edu>
2022-09-20 23:04:23 -0400

    Edited previous changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript By: Dina Dawood
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. Hello World! This page
-                will automatically reload. Name: Dina Dawood
+                Hello World! Edit <code>src/App.tsx</code> and save. This page
+                will automatically reload.
             </p>
         </div>
     );
