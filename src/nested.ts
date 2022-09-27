f2f9b7c02de665e49bdf45be9751e5e53d044daf
jasonhensley <jasonh@udel.edu>
2022-09-27 15:05:30 -0400

    remove idText

--- src/nested.ts
+++ src/nested.ts
@@ -1,4 +1,3 @@
-import { idText } from "typescript";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
 import { duplicateQuestion, makeBlankQuestion } from "./objects";
