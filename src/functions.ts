aea41645e7a22c20fe313a97bdad6db72f571172
Binghong Liu <binghong@udel.edu>
2022-09-30 12:06:31 -0400

    solved functions

--- src/functions.ts
+++ src/functions.ts
@@ -3,8 +3,8 @@
  * using this formula:
  *      C = (F - 32) * 5/9
  */
-export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+ export function fahrenheitToCelius(temperature: number): number {
+    return ((temperature - 32) * 5) / 9;
 }
 
 /**
@@ -12,7 +12,23 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    if (first > 0 && second > 0 && third > 0) {
+        return first + second + third;
+    } else if (first > 0 && second > 0) {
+        return first + second;
+    } else if (first > 0 && third > 0) {
+        return first + third;
+    } else if (second > 0 && third > 0) {
+        return second + third;
+    } else if (first > 0) {
+        return first;
+    } else if (second > 0) {
+        return second;
+    } else if (third > 0) {
+        return third;
+    } else {
+        return 0;
+    }
 }
 
 /**
@@ -20,7 +36,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +44,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
@@ -37,5 +53,12 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    word = word.toUpperCase();
+    if (word === "YES") {
+        return true;
+    } else if (word === "NO") {
+        return false;
+    } else {
+        return null;
+    }
 }
