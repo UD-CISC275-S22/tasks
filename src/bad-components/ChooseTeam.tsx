a6a1947011a8d292a07596f3c1572beca2f19735
Patrick Harris <pdharris@udel.edu>
2022-10-07 21:56:54 -0400

    Completed task #9

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -14,18 +14,23 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
+    function chooseMember(newMember: string) {
         /*
         if (!team.includes(newMember)) {
             team.push(newMember);
         }
         */
+        if (!team.includes(newMember)) {
+            setTeam([...team, newMember]);
+        }
     }
 
     function clearTeam() {
         /*
         team = [];
         */
+        setTeam([]);
+        setAllOptions([...PEOPLE]);
     }
 
     return (
@@ -36,7 +41,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
