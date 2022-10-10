d8ee704aa45c95969d8a1e4503bbf1a7894863e5
Gavinyw <gaviny@udel.edu>
2022-10-10 17:14:41 -0400

    commiting missing functions

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App(): JSX.Element {
                 </Container>
             </div>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
