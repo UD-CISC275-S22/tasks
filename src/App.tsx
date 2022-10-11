bb5219acf7fb223dee478604385ad88ca486b310
jessicahowley1 <112496856+jessicahowley1@users.noreply.github.com>
2022-10-11 11:03:25 -0400

    fixed merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -11,69 +11,69 @@ import { Counter } from "./components/Counter";
 function App(): JSX.Element {
     return (
         <>
-            <h1>Jessica-Howleys-Header</h1>
+            <>
+                <h1>Jessica-Howleys-Header</h1>
+                <div className="App">
+                    <header className="App-header">
+                        UD CISC275 with React Hooks and TypeScript- Jess Howley
+                        Hello World
+                    </header>
+                    <img
+                        alt="DOG"
+                        src="https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg"
+                    ></img>
+                    <ul id="list 1">
+                        <li>Red</li>
+                        <li>Green</li>
+                        <li>Blue</li>
+                    </ul>
+                    <div>
+                        <Button onClick={() => console.log("Hello World!")}>
+                            Log Hello World
+                        </Button>
+                    </div>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save. This page will
+                        automatically reload Hello World
+                    </p>
+                </div>
+                <div>
+                    <Container>
+                        <Row>
+                            <Col>
+                                First column.
+                                <div>
+                                    width:4px; height:2px; backgroundColor:red;
+                                </div>
+                            </Col>
+                            <Col>
+                                Second column.
+                                <div>
+                                    width:4px; height:2px; backgroundColor:red;
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </>
             <div className="App">
                 <header className="App-header">
-                    UD CISC275 with React Hooks and TypeScript- Jess Howley
-                    Hello World
+                    UD CISC275 with React Hooks and TypeScript
                 </header>
-                <img
-                    alt="DOG"
-                    src="https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg"
-                ></img>
-                <ul id="list 1">
-                    <li>Red</li>
-                    <li>Green</li>
-                    <li>Blue</li>
-                </ul>
-                <div>
-                    <Button onClick={() => console.log("Hello World!")}>
-                        Log Hello World
-                    </Button>
-                </div>
-                <p>
-                    Edit <code>src/App.tsx</code> and save. This page will
-                    automatically reload Hello World
-                </p>
-            </div>
-            <div>
-                <Container>
-                    <Row>
-                        <Col>
-                            First column.
-                            <div>
-                                width:4px; height:2px; backgroundColor:red;
-                            </div>
-                        </Col>
-                        <Col>
-                            Second column.
-                            <div>
-                                width:4px; height:2px; backgroundColor:red;
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
+                <hr></hr>
+                <Counter></Counter>
+                <hr />
+                <RevealAnswer></RevealAnswer>
+                <hr />
+                <StartAttempt></StartAttempt>
+                <hr />
+                <TwoDice></TwoDice>
+                <hr />
+                <ChangeType></ChangeType>
+                <hr />
+                <CycleHoliday></CycleHoliday>
             </div>
         </>
-=======
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
-        </div>
-    upstream/task-state
     );
 }
 export default App;
