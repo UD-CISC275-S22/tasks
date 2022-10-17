92a5ddd234031e04c50860f47bd19d772e537aad
Madeline Pearce <pearcemaddie@gmail.com>
2022-10-17 17:32:37 -0400

    finsihed form-components

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [ans, setans] = useState<string>("");
+    function updateans(event: React.ChangeEvent<HTMLInputElement>) {
+        setans(event.target.value);
+    }
+
     return (
-        <div>
-            <h3>Check Answer</h3>
-        </div>
+        <span>
+            <Form.Group controlId="formCheckAns">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control value={ans} onChange={updateans} />
+            </Form.Group>
+            {ans === expectedAnswer ? "✔️" : "❌"}
+        </span>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [edit, setit] = useState<boolean>(false);
+    const [name, setname] = useState<string>("Your Name");
+    const [student, setstud] = useState<boolean>(true);
+    function updatename(event: React.ChangeEvent<HTMLInputElement>) {
+        setname(event.target.value);
+    }
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="editModeSwitch"
+                label="Edit Mode"
+                checked={edit}
+                onChange={() => setit(!edit)}
+            />
+            {edit ? (
+                <div>
+                    <Form.Group controlId="formName">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control value={name} onChange={updatename} />
+                    </Form.Group>
+                    <Form.Check
+                        type="checkbox"
+                        id="StudentCheck"
+                        label="is a student?"
+                        checked={student}
+                        onChange={() => setstud(!student)}
+                    />
+                </div>
+            ) : (
+                <span>
+                    {name} is {student ? "" : "not"} a student
+                </span>
+            )}
         </div>
     );
 }
