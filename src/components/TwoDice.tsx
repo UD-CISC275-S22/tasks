efcc13d87e0361f4b34f6cd6e62850948fbd7a83
Joshua Artis <joshartis0090@gmail.com>
2022-10-18 11:46:43 -0400

    starting forms

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,4 +1,4 @@
-import userEvent from "@testing-library/user-event";
+//import userEvent from "@testing-library/user-event";
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
