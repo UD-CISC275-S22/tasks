0a2614c475d4405e482e3bda7ae03217ccd3e474
junanBai <jnbai@udel.edu>
2022-10-24 14:51:53 -0400

    change task 8

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,49 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-
+// import { QuestionType } from "../interfaces/question";
+export type Holiday =
+    | "Dragon Boat Festival🎏"
+    | "Halloween🎃"
+    | "Diiwali earlier🪔"
+    | "Chinese New Year🏮"
+    | "Thanksgiving🎉";
+const year_order = new Map<Holiday, Holiday>([
+    ["Chinese New Year🏮", "Dragon Boat Festival🎏"],
+    ["Dragon Boat Festival🎏", "Diiwali earlier🪔"],
+    ["Diiwali earlier🪔", "Halloween🎃"],
+    ["Halloween🎃", "Thanksgiving🎉"],
+    ["Thanksgiving🎉", "Chinese New Year🏮"]
+]);
+const alphbet_order = new Map<Holiday, Holiday>([
+    ["Chinese New Year🏮", "Thanksgiving🎉"],
+    ["Thanksgiving🎉", "Diiwali earlier🪔"],
+    ["Diiwali earlier🪔", "Dragon Boat Festival🎏"],
+    ["Dragon Boat Festival🎏", "Halloween🎃"],
+    ["Halloween🎃", "Chinese New Year🏮"]
+]);
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [curr_Holiday, setHoliday] = useState<Holiday>("Chinese New Year🏮");
+    function nextYearH(): void {
+        const nextHoliday = year_order.get(curr_Holiday);
+        if (nextHoliday != undefined) {
+            setHoliday(nextHoliday);
+        }
+    }
+    function nextAlphabetH(): void {
+        const nextHoliday = alphbet_order.get(curr_Holiday);
+        if (nextHoliday != undefined) {
+            setHoliday(nextHoliday);
+        }
+    }
+    return (
+        <div>
+            <p>Holiday: {curr_Holiday}</p>
+            <div>
+                <Button onClick={nextYearH}>Advance by Year</Button>
+            </div>
+            <div>
+                <Button onClick={nextAlphabetH}>Advance by Alphabet</Button>
+            </div>
+        </div>
+    );
 }
