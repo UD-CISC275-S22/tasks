c80c5472ec8701a4dd987f6c9c2d842fd7182cd4
madelinepearce <pearcemaddie@gmail.com>
2022-10-25 10:59:49 -0400

    added columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
 import "./App.css";
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
@@ -9,39 +9,49 @@ import pic from "./assets/audrey.png";
 
 function App(): JSX.Element {
     return (
-        <><div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript- Madeline Pearce
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
-            </p>
-        </div>
-        <div>
-            <h1>This is header text.</h1>
-            <img src={pic}/>
-        </div>
-        <div>
-            <ul>
-                <li>First thing</li>
-                <li>Another thing</li>
-                <li>A third item</li>
-            </ul>
-        </div>
-        <div>
-            <Button onClick={ () => console.log("Hello World!") }>Log Hello World</Button>
-        </div>
-        <div>
-            <Container>
-                <Row>
-                    <Col>First column.</Col>
-                    <Col>
-                        Second column.
-                    </Col>
-                </Row>
-            </Container>
-        </div></>
+        <>
+            <div className="App">
+                <header className="App-header">
+                    UD CISC275 with React Hooks and TypeScript- Madeline Pearce
+                </header>
+                <p>
+                    Edit <code>src/App.tsx</code> and save. This page will
+                    automatically reload. Hello World
+                </p>
+            </div>
+            <div>
+                <h1>This is header text.</h1>
+                <img src={pic} alt="Breakfast at Tiffany's" />
+            </div>
+            <div>
+                <ul>
+                    <li>First thing</li>
+                    <li>Another thing</li>
+                    <li>A third item</li>
+                </ul>
+            </div>
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>First column.</Col>
+                        <Col>Second column.</Col>
+                    </Row>
+                    <Row>
+                        <Col className="col1">
+                            <h1 className="redbox"></h1>
+                        </Col>
+                        <Col className="col2">
+                            <h1 className="redbox"></h1>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </>
     );
 }
 
