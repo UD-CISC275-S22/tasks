2e04024e73438c726c3999be84506e862ee04fd6
Ethan Hogan <ethanwh@udel.edu>
2022-10-26 01:13:16 -0400

    finished ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,29 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [question, changeType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function changeQuestionType(): void {
+        changeType(
+            question === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+    const toUpperCase = (string: string) => {
+        return string
+            .toLowerCase()
+            .split(" ")
+            .map(function (word) {
+                return word.replace(word[0], word[0].toUpperCase());
+            })
+            .join(" ");
+    };
+    return (
+        <div>
+            <Button onClick={changeQuestionType}>Change Type</Button>
+            {toUpperCase(question.replace(/_+/g, " "))}
+        </div>
+    );
 }
