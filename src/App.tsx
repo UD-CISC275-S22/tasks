987014d9637c2768b41d06e6ad3191af1ef75564
Meghana Yarlagadda <meghanayarlagadda@wifi-roaming-128-4-161-131.wifi.udel.edu>
2022-10-27 15:18:11 -0400

    Finished CheckAnswer and GiveAttempts.

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App(): JSX.Element {
                 expectedAnswer="b"
             ></MultipleChoiceQuestion>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [requestedAttempts, setRequestedAttempts] = useState<number>(0);
+
+    function updateBox(event: React.ChangeEvent<HTMLInputElement>) {
+        setRequestedAttempts(parseInt(event.target.value) || 0);
+    }
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <Form.Group controlId="form answer text">
+                <Form.Label>Check Answer: </Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requestedAttempts}
+                    onChange={updateBox}
+                ></Form.Control>
+            </Form.Group>
+            <Button
+                //Button to use attempts
+                onClick={() => setAttemptsLeft(attemptsLeft - 1)}
+                disabled={attemptsLeft === 0}
+            >
+                Use
+            </Button>
+            <Button
+                // Button to gain attempts
+                onClick={() =>
+                    setAttemptsLeft(attemptsLeft + parseInt(requestedAttempts))
+                }
+            >
+                Gain
+            </Button>
+            Attempts Left: {attemptsLeft}
         </div>
     );
 }
