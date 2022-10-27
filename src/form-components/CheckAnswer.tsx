09aba886ef4467e43a0ea068a2fd3c26651ac08e
Ethan Hogan <ethanwh@udel.edu>
2022-10-27 01:11:04 -0400

    finished CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [name, setName] = useState<string>("");
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <div>
+                <Form.Group controlId="formMovieName">
+                    <Form.Control value={name} onChange={updateName} />
+                </Form.Group>
+                <div> {name === expectedAnswer ? "✔️" : "❌"}.</div>
+            </div>
         </div>
     );
 }
