7f7ff066e275653dc3041a8758fe31b084d6f797
Grace O'Leary <graceoleary@wifi-roaming-128-4-127-186.wifi.udel.edu>
2022-10-27 16:28:40 -0400

    completed task 8

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,18 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(false);
+    function changeVisibility(): void {
+        if (visible == true) {
+            setVisible(false);
+        } else {
+            setVisible(true);
+        }
+    }
+    return (
+        <div>
+            <Button onClick={changeVisibility}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
