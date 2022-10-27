5cec820db08f87f162b22d4f9d414e8e47662237
Ethan Hogan <ethanwh@udel.edu>
2022-10-27 01:29:31 -0400

    redid CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
-    const [name, setName] = useState<string>("");
-    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
-        setName(event.target.value);
+    type ChangeEvent = React.ChangeEvent<
+        HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+    >;
+    const [answer, setAnswer] = useState<string>("");
+    function updateAnswer(event: ChangeEvent) {
+        setAnswer(event.target.value);
     }
     return (
         <div>
             <h3>Check Answer</h3>
-            <div>
-                <Form.Group controlId="formMovieName">
-                    <Form.Control value={name} onChange={updateName} />
-                </Form.Group>
-                <div> {name === expectedAnswer ? "✔️" : "❌"}.</div>
-            </div>
+            <Form.Group controlId="">
+                <Form.Label>Answer</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    rows={3}
+                    value={answer}
+                    onChange={updateAnswer}
+                />
+            </Form.Group>
+            {answer === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
