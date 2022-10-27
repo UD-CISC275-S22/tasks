0ab6dbbc5ba102fd2f2c46f90c8296c1621e4f18
Meghana Yarlagadda <meghanayarlagadda@wifi-roaming-128-4-161-131.wifi.udel.edu>
2022-10-27 05:16:25 -0400

    Updated ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function changeQuestionType(): void {
+        if (questionType === "short_answer_question") {
+            setType("multiple_choice_question");
+        } else {
+            setType("short_answer_question");
+        }
+    }
+    return (
+        <div>
+            <Button onClick={changeQuestionType}>Change Type</Button>
+            {questionType == "multiple_choice_question" ? (
+                <span>Multiple Choice</span>
+            ) : (
+                <span>Short Answer</span>
+            )}
+        </div>
+    );
 }
