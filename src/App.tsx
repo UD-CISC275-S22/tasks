677393ae51db267479261d9ad2f997672a588a6e
craftablescience <me@craftablescience.info>
2023-02-07 14:30:50 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript. Edited by Brendan
+                Lewis.
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
