7aa624c51b75ff62dc3342df3b5fdfe487424343
Christian Munley <chrismun@udel.edu>
2023-02-07 20:18:36 -0700

    added new test with first branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Christian Munley
+                Hello World UD CISC275 with React Hooks and TypeScript Christian
+                Munley
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
