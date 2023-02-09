486152a14614d9c16788c5cfdc84db692419c33a
sam <sjglover@udel.edu>
2023-02-08 21:16:35 -0500

    Added Hello World to App

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <p>Hello World</p>
             <p>Samantha Glover</p>
         </div>
     );
