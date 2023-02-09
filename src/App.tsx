13fe1930a6a942e422bb45d15f4ad042cda54718
Quinten Bettin <122491208+qbettin@users.noreply.github.com>
2023-02-09 13:00:55 -0500

    Solved first branch (#1)
    merge for hello world changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript by Quinten Bettin
             </header>
+            <p>Hello World</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
