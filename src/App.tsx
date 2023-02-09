8a198d4ea34b1d08907d35fab166a46e5defe234
Solenn Gacon <solgacon@udel.edu>
2023-02-09 14:45:57 -0500

    Commit with Git Bash for 2nd task

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Hello World! UD CISC275 & Solenn with React Hooks and TypeScript
+                UD CISC275 & Solenn with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. And Hello World!
             </p>
         </div>
     );
