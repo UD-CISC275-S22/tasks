3d3a85397970af5d40c62b8cdcfb055178d17583
Alex <apeluso@udel.edu>
2023-02-09 11:00:12 -0500

    Started Task 3 (using as save state)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import photo from "./site.png";
 
 function App(): JSX.Element {
     return (
@@ -7,10 +9,31 @@ function App(): JSX.Element {
             <header className="App-header">
                 Alex Peluso: UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
-            </p>
+            <h1>Header</h1>
+            <p>Hello World! Testing HTML</p>
+            <img src={photo} />
+            <ul>
+                <li>Hello</li>
+                <li>My Name Is Alex Peluso</li>
+                <li>I Am A Student At UD</li>
+            </ul>
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Click Me
+                </Button>
+            </div>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>First column.</Col>
+                        <Col>
+                            Second column. You can put whatever you want in
+                            here, and it will be on the right side. Maybe try
+                            adding an image?
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
