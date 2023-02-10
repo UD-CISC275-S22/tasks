09cba19360ee3c528d77223c67ddc1f7f8bee0d0
JH COOPER <jhcooper31@gmail.com>
2023-02-09 22:07:26 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. - John Henry Cooper
             </p>
         </div>
     );
