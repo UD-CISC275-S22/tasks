12db9b6e564f37295402159d422ac54627c94b57
gavincaul <gavcaul@udel.edu>
2023-02-09 21:08:47 -0500

    added 'Hello World'

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Website belongs to Gavin Caulfield!</p>
+            <p>Website belongs to Gavin Caulfield! Hello World!</p>
         </div>
     );
 }
