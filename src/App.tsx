fafe926028b08ed0f5d956538575700364c2f824
Heni <heni@udel.edu>
2023-02-10 14:12:50 -0500

    change hello

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Heni Patel</p>
-            <p>Hello World</p>
+            <p>Heni Patel - Hello World</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
