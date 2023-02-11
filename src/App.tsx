dcc537081cb30ec4c27beadd5b3d38a85eb76bf4
Paul Kearney <paulke@udel.edu>
2023-02-11 13:39:25 -0500

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "react-bootstrap";
 import React from "react";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
+            <Button>Click Me</Button>
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
