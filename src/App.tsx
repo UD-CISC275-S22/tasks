6eb8ec0a3e1181fc7668b0ed1467dd316382db44
Will Sharp <sharpie@udel.edu>
2023-02-10 20:13:39 -0500

    Changed to my name

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                Hello World! I have arrived, and I am William Sharp UD CISC275
+                with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
