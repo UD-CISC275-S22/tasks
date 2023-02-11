7ee40055a7782ad7a26bb984ee8fdca378883c4e
Doomsuccs <Doomsucc@udel.edu>
2023-02-11 14:53:25 -0500

    Added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Ray Yang
             </p>
+            <body>Hello World</body>
         </div>
     );
 }
