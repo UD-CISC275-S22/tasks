1b493e33224eca28e26e162a32d7f4b1e6e3988b
Shawn <sdempsey@udel.edu>
2023-02-13 13:14:12 -0500

    final hello wrld add

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Shawn Dempsey! Hello World
+                automatically reload. Shawn Dempsey! Hello World.
             </p>
         </div>
     );
