8b8fdc5039b8a2261c14be2eeb12142f8667c3d9
James Krienen <jameskri@udel.edu>
2023-02-14 15:14:11 -0500

    2/14 classwork for task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
-
+import logo from "./golconda.jpeg";
 function App(): JSX.Element {
     return (
-        <div className="App">
+        <div
+            className="App"
+            style={{ border: "10px solid green", padding: "4px" }}
+        >
+            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
             <header className="App-header">
-                Kriendog UD CISC275 with React Hooks and TypeScript
+                <span style={{ color: "red" }}>
+                    Hello World UD CISC275 with React Hooks and TypeScript{" "}
+                </span>
             </header>
+            <img src={logo} />
+
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
+                Edit <code>src/App.tsx</code>3 and save. This page will
                 automatically reload.
             </p>
         </div>
