4cade32d9528954c2aeb4051bc7c9f0ebdb2786d
Mikaylla <mikahas@udel.edu>
2023-02-14 20:27:52 -0500

    Added HEADER

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Mikaylla Hello World Edit <code>src/App.tsx</code> and save.
-                This page will automatically reload.
-            </p>
-        </div>
+        <h1>
+            This is header text
+            {/* // <div className="App">
+        //     <header className="App-header">
+        //         UD CISC275 with React Hooks and TypeScript
+        //     </header>
+        //     <p>
+        //         Mikaylla Hello World Edit <code>src/App.tsx</code> and save.
+        //         This page will automatically reload. Hello World
+        //     </p>
+        // </div> */}
+        </h1>
     );
 }
 
