83bef83a69f89b1ce2c8c7ea39ef2b448f8924e6
Mikaylla <mikahas@udel.edu>
2023-02-15 14:56:11 -0500

    change header

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App(): JSX.Element {
                     UD CISC275 with React Hooks and TypeScriptS
                 </header>
                 <div>
-                    <h1>Hello World</h1>
                     <img
                         src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/22935cd4-b5b4-4716-98f8-bb6d00156232/de3qhpq-ac4700a1-0404-46d5-9f6f-ad55eda89f6d.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzIyOTM1Y2Q0LWI1YjQtNDcxNi05OGY4LWJiNmQwMDE1NjIzMlwvZGUzcWhwcS1hYzQ3MDBhMS0wNDA0LTQ2ZDUtOWY2Zi1hZDU1ZWRhODlmNmQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.nm2goVv_zI3gqXErAWbJPbVK6-KLhr-6HsvU2IzvQ7M"
                         alt="Baby"
@@ -24,17 +23,11 @@ function App(): JSX.Element {
                 </p>
             </div>
             <div>
-                Unordered List:
-                <ul>
-                    <li>3 cookies</li>
-                    <li>1 cookie</li>
-                    <li>2 cookies</li>
-                </ul>
                 Ordered List:
                 <ol>
-                    <li>1 cookie</li>
-                    <li>2 cookies</li>
-                    <li>3 cookies</li>
+                    <li>cookie</li>
+                    <li>cookies</li>
+                    <li>cookies3</li>
                 </ol>
             </div>
             ;
