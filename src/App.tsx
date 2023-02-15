e7300d7af9c969baf96312b47b9e8e464361baad
rachel-sison <RSISON@UDEL.EDU>
2023-02-15 17:32:07 -0500

    task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                Rachel Sison UD CISC275 with React Hooks and TypeScript
+            <header className="App-header" background-color="pink">
+                <h1>Rachel Sison UD CISC275 with React Hooks and TypeScript</h1>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+            <img
+                src="/Users/rachelsison/Downloads/cavapoo.jpg"
+                alt="A picture of a cavapoo"
+            />
+            List of colors:
+            <ul>
+                <li>red</li>
+                <li>orange</li>
+                <li>yellow</li>
+            </ul>
+            <Container>
+                <Row>
+                    <Col>
+                        <div>
+                            height: 200px; width: 50%; background-color:red;
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            height: 200px; width: 50%; background-color:red;
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
