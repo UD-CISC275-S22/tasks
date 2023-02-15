b28b61574d2ea2bcc9e9239a8d9237885dcfe47a
stephenwiafe <stephwiafe18@gmail.com>
2023-02-14 21:39:46 -0500

    Add header,img,red box and list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-
+import img1 from "./images/mountain.jpg";
+import { Button, Container, Row, Col } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
@@ -13,6 +14,51 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <h1 style={{ background: "#ADD8E6" }}>Welcome to my page</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <p>Column 1</p>
+                        <img
+                            style={{
+                                width: "500px",
+                                height: "300px"
+                            }}
+                            src={img1}
+                            alt="An image of a mountain"
+                        />
+                        <div>
+                            <div
+                                className="redBox"
+                                style={{
+                                    backgroundColor: "red",
+                                    width: "200px",
+                                    height: "50px"
+                                }}
+                            ></div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <ul>
+                            <p>Column 2</p>
+                            <li>basketball</li>
+                            <li>soccer</li>
+                            <li>tennis</li>
+                        </ul>
+                        <Button onClick={() => console.log("Hello World!")}>
+                            Log Hello World
+                        </Button>
+                        <div
+                            className="redBox"
+                            style={{
+                                backgroundColor: "red",
+                                width: "200px",
+                                height: "50px"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
