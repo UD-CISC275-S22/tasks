818748afab110bb2c749a44a676459c57cf9f184
Mikaylla <mikahas@udel.edu>
2023-02-14 22:08:18 -0500

    web header

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <>
-            <h1>This is header text</h1>
-            {/* <div className="App">
+            <h1>Mikaylla Website</h1>
+            <div className="App">
                 <header className="App-header">
                     UD CISC275 with React Hooks and TypeScriptS
                 </header>
                 <p>
                     Hello World Edit <code>src/App.tsx</code> and save. This
                     page will automatically reload. Hello World
-                </p> */}
-            {/* </div> */}
+                </p>
+            </div>
         </>
     );
 }
