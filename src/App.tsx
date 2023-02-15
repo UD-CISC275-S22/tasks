3d8b9a84a233ec02c770f760d62cb41a8fe51f1a
gavincaul <gavcaul@udel.edu>
2023-02-15 14:40:33 -0500

    Added and fixed image allocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import pickle from "/Users/gavincaulfield/tasks/src/pickle.jpeg";
+import pickle from "./pickle.jpeg";
 
 function App(): JSX.Element {
     return (
