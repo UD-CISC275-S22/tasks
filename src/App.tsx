918e326ebe199f6adc0a46833252082220f94409
Mikaylla <mikahas@udel.edu>
2023-02-14 21:51:51 -0500

    ch

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScriptS
             </header>
             <p>
-                Mikaylla0 Hello World Edit <code>src/App.tsx</code> and save.
-                This page will automatically reload. Hello World
+                Hello World Edit <code>src/App.tsx</code> and save. This page
+                will automatically reload. Hello World
             </p>
         </div>
     );
