bd44e6e513e045ca9dc25099ecc7151af5cba85f
alexmarshall11 <alexmar@udel.edu>
2023-02-15 22:16:56 -0500

    passed all tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import dog from "./Dog_Breeds.jpg";
 
 function App(): JSX.Element {
     return (
@@ -12,6 +14,40 @@ function App(): JSX.Element {
                 automatically reload.
             </p>
             <p>Hello World!</p>
+            <h1>
+                <span style={{ backgroundColor: "red" }}>Task 3</span>
+            </h1>
+            <img
+                src={dog}
+                width="100"
+                height="100"
+                alt="A picture of a cute dog"
+            />
+            <div>
+                Animals:
+                <ul>
+                    <li>dogs</li>
+                    <li>cats</li>
+                    <li>lizards</li>
+                </ul>
+            </div>
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        First Column.
+                        <div className="rectangle"></div>
+                    </Col>
+                    <Col>
+                        Second column.
+                        <div className="rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
