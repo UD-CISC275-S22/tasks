de8d31ebbf2aeddb8bbd28222539ad4e1437ef87
Will Sharp <sharpie@udel.edu>
2023-02-15 23:51:22 -0500

    commiting the task 3 updates in total  Please enter the commit message for your changes. Lines starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-
+import img from "./assets/F-350 Front Right.jpg";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                Hello World! I have arrived, and I am William Sharp UD CISC275
-                with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <h1 className="App-header" style={{ backgroundColor: "#7B3702" }}>
+                Hello People! I have arrived, and I am William Sharp UD CISC275
+                I hope your day is wonderful!
+            </h1>
+            <img src={img} alt="A picture of a big ol truck" />
+            <ul>
+                <li>Make the bread</li>
+                <li>Spend the bread</li>
+                <li>Make the bread again</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="rectangle"></div>
+                    </Col>
+                    <Col>
+                        <div className="rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
