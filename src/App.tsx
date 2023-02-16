789ca86762752a60fb6ff49876d521c40c168b75
Michael Lorang <mlorang@udel.edu>
2023-02-15 22:07:05 -0500

    Task 3 Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import logo from "./assets/Moon River copy.png";
 
 function App(): JSX.Element {
     return (
@@ -7,9 +9,42 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>Welcome To My Website!</h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. I am Michael Lorang. Hello World
+            </p>
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div className="App-box"></div>
+                            Some fun facts about me
+                            <ul>
+                                <li>I am the one who knocks</li>
+                                <li>I love exercising</li>
+                                <li>
+                                    I have some prior expereince to software
+                                    development
+                                </li>
+                            </ul>
+                        </Col>
+                        <Col>
+                            A column of red boxes. How exciting!!!
+                            <div className="App-box"></div>
+                            <div className="App-box"></div>
+                            <div className="App-box"></div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <p>
+                <img src={logo} alt="Moon RIver" />
             </p>
         </div>
     );
