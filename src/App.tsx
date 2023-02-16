7517a0edd5dbc845dbe793ab4f015e4f6d0410c8
MikeMcCully <michaelmccully00@gmail.com>
2023-02-16 02:08:05 -0500

    solved-html-css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import React from "react";
 import "./App.css";
-
+import { Button, Col, Container, Row } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275*React Hooks*TypeScript*Michael McCully
-            </header>
-            <i>Hello World!</i>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page reload.
-            </p>
+            <Container>
+                <header className="App-header">
+                    UD CISC275*React Hooks*TypeScript*Michael McCully
+                </header>
+                <i>Hello World!</i>
+                <p>
+                    Edit <code>src/App.tsx</code> and save. This page reload.
+                </p>
+                <header className="App-header">Current GOAT Rankings:</header>
+                <Row>
+                    <div className="Red-box"> </div>
+                    <Col>
+                        <ol>
+                            <li>Islam Makhachev</li>
+                            <li>Devonta Smith</li>
+                            <li>Dwayne Michael Carter Jr.</li>
+                            <li>Joel Embiid</li>
+                            <li>Myself(Michael McCully)</li>
+                        </ol>
+                    </Col>
+                    <Col>
+                        <img
+                            src="https://www.inquirer.com/resizer/bOio8DaUDgEMlOLzEjYZW50dUMs=/760x507/smart/filters:format(webp)/cloudfront-us-east-1.images.arcpublishing.com/pmn/SOF7A64MKJAMDPJG67V42RJ6CE.jpg"
+                            alt="Devonta Smith"
+                        />
+                        <div className="Red-box"> </div>
+                    </Col>
+                </Row>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </Container>
         </div>
     );
 }
