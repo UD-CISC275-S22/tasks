e323a4bd4068e4ca257bc2fb2c6731302e173834
diyashah <diyashah@udel.edu>
2023-02-16 12:42:12 -0500

    completed all modifications to the website for task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,56 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { alignPropType } from "react-bootstrap/esm/types";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Diya Shah - UD CISC275 with React Hooks and TypeScript
+                <h1>New Header for Task 3!</h1>
+                Diya Shah - UD CISC275 with React Hooks and TypeScript - Hello
+                World!
             </header>
+            <Container>
+                <Row>
+                    <Col>
+                        {" "}
+                        To do list:
+                        <ul>
+                            <li>Clean Room </li>
+                            <li>Do Homework</li>
+                            <li>Go to grocery store</li>
+                            <li>Fill gas</li>
+                        </ul>
+                        <div
+                            style={{
+                                width: "175px",
+                                height: "50px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <img
+                            src="https://free-images.com/tn/366a/beachy_head_lighthouse_east_2.jpg"
+                            alt="A picture of a lighthouse"
+                        />
+                        <div
+                            style={{
+                                width: "175px",
+                                height: "50px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <p>
-                Hello World ! Edit <code>src/App.tsx</code> and save. This page
-                will automatically reload.
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload. Diya Shah.
             </p>
         </div>
     );
