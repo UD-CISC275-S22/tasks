4156bd3e1813b3a38058a452ea15f73a056c2eff
Ian <ianjamessteele@gmail.com>
2023-02-15 22:34:49 -0500

    Task 3 frfr

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,30 @@ function App(): JSX.Element {
             <header className="App-header">
                 Ian Steele UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>Hejjj</h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
-            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <Container>
                 <Row>
-                    <Col>Hej</Col>
-                    <Col>How r u </Col>
+                    <Col className="col">Hej</Col>
+                    <Col className="col">
+                        <img
+                            src="https://i.kym-cdn.com/photos/images/masonry/002/363/364/447.png"
+                            alt="It's over"
+                        />
+                    </Col>
                 </Row>
             </Container>
+            <ul>
+                <li>I</li>
+                <li>Hate</li>
+                <li>School</li>
+            </ul>
         </div>
     );
 }
