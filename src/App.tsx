ea1c95ddf1151086cd11f9c4b0973c1be2ca6c18
James Krienen <jameskri@udel.edu>
2023-02-17 13:20:15 -0500

    change to iamge

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import logo from "./golconda.jpeg";
+import logo from "./images/golconda.jpeg";
 function App(): JSX.Element {
     return (
         <div
             className="App"
             style={{ border: "10px solid green", padding: "4px" }}
         >
-            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
+            <h1>This is my header for tests</h1>
+            <Container>
+                <Row>
+                    <Col>First column.</Col>
+                    <Col>
+                        Second column. You can put whatever you want in here,
+                        and it will be on the right side. Maybe try adding an
+                        image?
+                    </Col>
+                </Row>
+            </Container>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <header className="App-header">
                 <span style={{ color: "red" }}>
                     Hello World UD CISC275 with React Hooks and TypeScript{" "}
                 </span>
             </header>
-            <img src={logo} />
-
+            <img src={logo} alt="Golconda, a picture by Rene Magritte" />
+            Cars
+            <ul>
+                <li>ferrari</li>
+                <li>honda</li>
+                <li>koenigsegg</li>
+            </ul>
             <p>
                 Edit <code>src/App.tsx</code>3 and save. This page will
                 automatically reload.
