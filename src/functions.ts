dbd36893831952f221668134a6971e99a48eec1f
stephenwiafe <stephwiafe18@gmail.com>
2023-02-19 23:49:17 -0500

    Update function add3

--- src/functions.ts
+++ src/functions.ts
@@ -13,6 +13,9 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
+    if (first > 0 && second > 0 && third > 0) {
+        return first + second + third;
+    }
     return 0;
 }
 
