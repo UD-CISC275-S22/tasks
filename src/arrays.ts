bd6d54f6804b35d47d3f68cc497e4e34818d56cd
Quinten <qbettin@udel.edu>
2023-02-21 19:51:41 -0500

    thought i had an any type but I didnt

--- src/arrays.ts
+++ src/arrays.ts
@@ -20,7 +20,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    let tripled = [...numbers];
+    let tripled: number[] = [...numbers];
     tripled = numbers.map((tripled: number): number => tripled * 3);
     return tripled;
 }
