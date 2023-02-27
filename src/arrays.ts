6af3d07351f35ee6082129c918f600a29fac2782
Alexander Marshall <alexmar@udel.edu>
2023-02-26 23:22:24 -0500

    stringsToIntegers function

--- src/arrays.ts
+++ src/arrays.ts
@@ -24,7 +24,7 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map((n: string): number => (!isNaN(parseInt(n)) ? parseInt(n) : 0));
 }
 
 /**
