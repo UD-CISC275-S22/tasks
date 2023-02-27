39d3b4eef0e7fd5898ee7260cd616bf8d1489606
Alexander Marshall <alexmar@udel.edu>
2023-02-27 12:32:47 -0500

    allGRB function

--- src/arrays.ts
+++ src/arrays.ts
@@ -78,7 +78,13 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    if (colors.length > 0) {
+        return colors.every(
+            (color: string): boolean =>
+                color === "red" || color === "blue" || color === "green"
+        );
+    }
+    return true;
 }
 
 /**
