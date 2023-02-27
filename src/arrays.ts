34e73b55557af48e767d0b5b396c39eb5d574b3e
Alexander Marshall <alexmar@udel.edu>
2023-02-26 23:06:29 -0500

    tripleNumbers function

--- src/arrays.ts
+++ src/arrays.ts
@@ -16,7 +16,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((n: number): number => n * 3);
 }
 
 /**
