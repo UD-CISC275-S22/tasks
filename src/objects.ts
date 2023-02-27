d8836bf2c21e7cd186d8e6b2045ec0a231b81b7f
pkstoy13 <pkstoyanov1@gmail.com>
2023-02-27 11:44:02 -0500

    all ten object functions done

--- src/objects.ts
+++ src/objects.ts
@@ -142,7 +142,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const newOpt = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    return newOpt;
 }
 
 /**
@@ -159,5 +163,12 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return contentQuestion;
+    const mergedQuestion = {
+        ...contentQuestion,
+        id: id,
+        name: name,
+        points: points,
+        published: false
+    };
+    return mergedQuestion;
 }
