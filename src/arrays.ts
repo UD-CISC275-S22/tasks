80fe17b6076da57c5efe7a74c97d139187217c74
Will Sharp <sharpie@udel.edu>
2023-02-27 22:54:02 -0500

    Finished countShortWords function

--- src/arrays.ts
+++ src/arrays.ts
@@ -74,7 +74,9 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const shortWords = words.filter((word: string): boolean => word.length < 4);
+    const numShortWords = shortWords.length;
+    return numShortWords;
 }
 
 /**
