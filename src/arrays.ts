d0988daf62539a9a194d99707475553d2c908350
ryansexton <rjsexton1210@gmail.com>
2023-03-01 21:00:38 -0500

    array

--- src/arrays.ts
+++ src/arrays.ts
@@ -102,7 +102,16 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length == 0) {
+        return "0=0";
+    } else {
+        const sum = addends.reduce(
+            (currentTotal: number, num: number) => currentTotal + num,
+            0
+        );
+        const makeMath = addends.join("+");
+        return sum.toString() + "=" + makeMath;
+    }
 }
 
 /**
