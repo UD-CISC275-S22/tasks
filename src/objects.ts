267ebfe1e2393136f4e876cd105eef0540263ad0
Will Sharp <sharpie@udel.edu>
2023-03-02 13:49:28 -0500

    Finished renameQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -92,7 +92,8 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    const copyQuestion = { ...question, name: newName };
+    return copyQuestion;
 }
 
 /**
