3bd22c29b6ea42c4ddddf3e3a1e53948390a9afa
Will Sharp <sharpie@udel.edu>
2023-03-02 14:05:11 -0500

    Finished addOption

--- src/objects.ts
+++ src/objects.ts
@@ -135,7 +135,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const newQuestion = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    return newQuestion;
 }
 
 /**
