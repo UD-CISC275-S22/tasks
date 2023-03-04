eb1535c6fed1fc46e2ba56ed6a36391f354e95b2
Alexander Marshall <alexmar@udel.edu>
2023-03-04 01:22:27 -0500

    addOption fxn

--- src/objects.ts
+++ src/objects.ts
@@ -128,7 +128,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const updatedQuestion = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    return updatedQuestion;
 }
 
 /**
