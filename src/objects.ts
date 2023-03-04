6381d65a3bf5b2a8df368c5680c01fd468c715be
Alexander Marshall <alexmar@udel.edu>
2023-03-03 23:42:47 -0500

    toShortForm fxn

--- src/objects.ts
+++ src/objects.ts
@@ -58,7 +58,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return question.id.toString() + ": " + question.name.substring(0, 10);
 }
 
 /**
