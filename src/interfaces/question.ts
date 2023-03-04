3d47d11a5cffe2805aef11206cc9aefa10530815
Heni <heni@udel.edu>
2023-03-04 12:08:30 -0500

    some mistake

--- src/interfaces/question.ts
+++ src/interfaces/question.ts
@@ -2,6 +2,7 @@
 export type QuestionType = "multiple_choice_question" | "short_answer_question";
 
 export interface Question {
+    newOption: any;
     /** A unique identifier for the question */
     id: number;
     /** The human-friendly title of the question */
