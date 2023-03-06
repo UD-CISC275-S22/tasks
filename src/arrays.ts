f8280a62c73f8813b75e06eb3be8d0b391f2b723
James Krienen <jameskri@udel.edu>
2023-03-06 10:21:01 -0500

    finished task 5

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,13 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (numbers.length === 0) {
+        return [];
+    } else if (numbers.length === 1) {
+        return [numbers[0], numbers[0]];
+    } else {
+        return [numbers[0], numbers[numbers.length - 1]];
+    }
 }
 
 /**
@@ -13,7 +19,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((price: number): number => price * 3);
 }
 
 /**
@@ -21,7 +27,9 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map((num: string): number =>
+        Number.isNaN(parseInt(num)) ? 0 : parseInt(num)
+    );
 }
 
 /**
@@ -32,7 +40,12 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    return [];
+    const amounts2 = amounts.map((val: string): string =>
+        val.charAt(0) === "$" ? (val = val.slice(1, val.length)) : val
+    );
+    return amounts2.map((num: string): number =>
+        Number.isNaN(parseInt(num)) ? 0 : parseInt(num)
+    );
 };
 
 /**
@@ -41,7 +54,12 @@ export const removeDollars = (amounts: string[]): number[] => {
  * in question marks ("?").
  */
 export const shoutIfExclaiming = (messages: string[]): string[] => {
-    return [];
+    const noqs = messages.filter(
+        (mess: string): boolean => !(mess.charAt(mess.length - 1) === "?")
+    );
+    return noqs.map((upper: string): string =>
+        upper.charAt(upper.length - 1) === "!" ? upper.toUpperCase() : upper
+    );
 };
 
 /**
@@ -49,7 +67,9 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const ltfour = words.filter((word: string): boolean => word.length < 4);
+
+    return ltfour.length;
 }
 
 /**
@@ -58,7 +78,13 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    if (colors.length === 0) {
+        return true;
+    }
+    return colors.every(
+        (color: string): boolean =>
+            color === "red" || color === "blue" || color === "green"
+    );
 }
 
 /**
@@ -69,7 +95,15 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const sum: number = addends.reduce(
+        (total: number, cur: number) => total + cur,
+        0
+    );
+
+    return sum + "=" + addends.join("+"); //why can i do this. im concatenating a number to a string
 }
 
 /**
@@ -82,5 +116,26 @@ export function makeMath(addends: number[]): string {
  * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
-    return [];
+    const copy = [...values];
+
+    const neg = values.findIndex((val: number): boolean => val < 0);
+
+    if (neg === -1) {
+        const endVal = values.reduce(
+            (currTot: number, num: number) => currTot + num,
+            0
+        );
+        copy.splice(values.length, 0, endVal);
+        return copy;
+    } else if (neg === 0) {
+        copy.splice(neg + 1, 0, 0);
+        return copy;
+    } else {
+        const negVal = values.reduceRight(
+            (curr: number, currVal: number) => curr + currVal,
+            values[neg]
+        );
+        copy.splice(neg + 1, 0, negVal);
+        return copy;
+    }
 }
