dfcdd3f0ffb9ffe3bb963eb4454fc9548b3dfd05
Alexander Marshall <alexmar@udel.edu>
2023-03-06 12:42:46 -0500

    getPublishedQuestions fxn

--- src/nested.ts
+++ src/nested.ts
@@ -6,7 +6,10 @@ import { Question, QuestionType } from "./interfaces/question";
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-    return [];
+    const publishedQuestions = questions.filter(
+        (question: Question): boolean => question.published
+    );
+    return publishedQuestions;
 }
 
 /**
