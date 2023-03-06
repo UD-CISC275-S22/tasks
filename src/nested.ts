7bbfe1b77b88053924f6eae54749bffefdbaf517
Heni <heni@udel.edu>
2023-03-06 17:00:05 -0500

    completed functions sameType

--- src/nested.ts
+++ src/nested.ts
@@ -135,7 +135,13 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    const checkOne = questions.every(
+        (q: Question): boolean => q.type === "multiple_choice_question"
+    );
+    const checkTwo = questions.every(
+        (q: Question): boolean => q.type === "short_answer_question"
+    );
+    return checkOne || checkTwo;
 }
 
 /***
