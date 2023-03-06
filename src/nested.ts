565602c658bd00a1bf6768b4c78322dc3e796d36
Will Sharp <sharpie@udel.edu>
2023-03-06 14:07:19 -0500

    finished getNames and sumPoints functions

--- src/nested.ts
+++ src/nested.ts
@@ -69,14 +69,22 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    const questionNames = questions.map(
+        (question: Question): string => question.name
+    );
+    return questionNames;
 }
 
 /***
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const totalPoints = questions.reduce(
+        (currentPoints: number, question: Question) =>
+            (currentPoints += question.points),
+        0
+    );
+    return totalPoints;
 }
 
 /***
