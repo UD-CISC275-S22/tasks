ccdce7655e7905a02949cbba98fff66c59a120e9
Alexander Marshall <alexmar@udel.edu>
2023-03-07 01:25:34 -0500

    sumPublishedPoints fxn

--- src/nested.ts
+++ src/nested.ts
@@ -74,7 +74,8 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const publishedQuestions = getPublishedQuestions(questions);
+    return sumPoints(publishedQuestions);
 }
 
 /***
