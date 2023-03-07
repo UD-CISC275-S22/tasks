98df0cc472778525bec88cbc4a132d0e91c6e22b
stephenwiafe <stephwiafe18@gmail.com>
2023-03-07 13:31:52 -0500

    Complete function getNonEmptyQuestions

--- src/nested.ts
+++ src/nested.ts
@@ -18,7 +18,13 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    const newArray: Question[] = questions.filter(
+        (question: Question): boolean =>
+            question.body !== "" ||
+            question.expected !== "" ||
+            question.options.length !== 0
+    );
+    return newArray;
 }
 
 /***
