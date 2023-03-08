c0f9df8a52bcd49e3d12340a4a6ac1c2ed6ba264
Alexander Marshall <alexmar@udel.edu>
2023-03-07 22:48:47 -0500

    toCSV fxn

--- src/nested.ts
+++ src/nested.ts
@@ -18,11 +18,11 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    console.log(questions);
+
     return questions.filter(
         (question: Question): boolean =>
-            question.body !== "" &&
-            question.expected !== "" &&
+            question.body.trim() !== "" &&
+            question.expected.trim() !== "" &&
             question.options.length !== 0
     );
 }
@@ -96,7 +96,13 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const questionsCSV = questions
+        .map(
+            (question: Question): string =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`
+        )
+        .join("\n");
+    return "id,name,options,points,published\n" + questionsCSV;
 }
 
 /**
