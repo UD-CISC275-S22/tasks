d8bd70e3ab6852fc1c8f529ed0e868cc139a306a
Alexander Marshall <alexmar@udel.edu>
2023-03-07 23:14:41 -0500

    sameType fxn

--- src/nested.ts
+++ src/nested.ts
@@ -142,7 +142,15 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    const isShortAns = questions.every(
+        (question: Question): boolean =>
+            question.type === "short_answer_question"
+    );
+    const isMultChoice = questions.every(
+        (question: Question): boolean =>
+            question.type === "multiple_choice_question"
+    );
+    return isShortAns || isMultChoice;
 }
 
 /***
