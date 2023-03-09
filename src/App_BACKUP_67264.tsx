eee5573b0a1d148a9788a1c869a7c3ae3f1d7da9
Alexander Marshall <alexmar@udel.edu>
2023-03-09 16:35:12 -0500

    resolved merge conflict

--- src/App_BACKUP_67264.tsx
+++ src/App_BACKUP_67264.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-<<<<<<< HEAD
 import dog from "./Dog_Breeds.jpg";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): JSX.Element {
     return (
@@ -18,7 +15,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript - Alexander Marshall
             </header>
-<<<<<<< HEAD
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
@@ -58,7 +54,6 @@ function App(): JSX.Element {
                     </Col>
                 </Row>
             </Container>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -71,7 +66,7 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
+
         </div>
     );
 }
