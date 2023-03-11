cfd41f061b1718553b9d28a8de39d43082c7b7ad
Solenn Gacon <solgacon@udel.edu>
2023-03-11 10:55:59 -0500

    last commit for task about state y'all!

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -1,6 +1,57 @@
+//import { isDisabled } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+//import { start } from "repl";
+//import { idText } from "typescript";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [nbAttempts, setNbAttempts] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+
+    function decreaseByOne(): void {
+        setNbAttempts(nbAttempts - 1);
+    }
+
+    function myStartFunction(): void {
+        setInProgress(true);
+        decreaseByOne();
+    }
+
+    function myStopFunction(): void {
+        setInProgress(false);
+    }
+
+    function myMulliganFunction(): void {
+        setNbAttempts(nbAttempts + 1);
+    }
+
+    return (
+        <div>
+            <div>
+                <Button
+                    onClick={myStartFunction}
+                    disabled={inProgress === true || nbAttempts === 0}
+                >
+                    Start Quiz
+                </Button>
+            </div>
+            <div>
+                <Button
+                    onClick={myStopFunction}
+                    disabled={inProgress === false}
+                >
+                    Stop Quiz
+                </Button>
+            </div>
+            <div>
+                <Button
+                    onClick={myMulliganFunction}
+                    disabled={inProgress === true}
+                >
+                    Mulligan
+                </Button>
+                <span>Attempts: {nbAttempts}</span>
+            </div>
+        </div>
+    );
 }
