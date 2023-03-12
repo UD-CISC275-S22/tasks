995226abc41e47e1f8204d955bb9a5541f6e28a0
Heni <heni@udel.edu>
2023-03-12 19:46:54 -0400

    Complete Reveal Answer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,19 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    //return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(true);
+
+    function flipVisibility(): void {
+        // Set visible to be the logical opposite of its previous value
+        setVisible(!visible);
+    }
+
+    // Only includes <div>42</div> if `visible` is true
+    return (
+        <div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {visible && <div>42</div>}{" "}
+        </div>
+    );
 }
