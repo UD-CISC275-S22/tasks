6cfdd7844409a3f28078b99e5d4772f087f04c9a
Brandon Marafino <bmarafino@Brandons-MacBook-Pro-2.local>
2023-03-14 23:48:53 -0400

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Brandon Marafino
+                UD CISC275 with React Hooks and TypeScript Brandon Marafino2
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
