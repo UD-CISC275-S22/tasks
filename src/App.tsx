de763e55f22da9c952298b334f7bd8d6b827ade1
gavincaul <gavcaul@udel.edu>
2023-03-14 22:53:12 -0400

    saved app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
-
 function App(): JSX.Element {
     return (
         <div className="App">
