86b667c31aa3aa93531b09274eb35fe38e825f47
superamazingana <acdonato@udel.edu>
2023-03-17 10:07:11 -0400

    finished state 3.0

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { QuestionType } from "../interfaces/question";
+// import { Button } from "react-bootstrap";
+// import { QuestionType } from "../interfaces/question";
 
 {
     /* 4/4 test cases passed !!!!!!*/
 }
 
 export function ChangeType(): JSX.Element {
-    // eslint-disable-next-line prefer-const
-    let [type, setType] = useState<QuestionType>("short_answer_question");
+    const [type, setType] = useState<QuestionType>("short_answer_question");
 
     type QuestionType = "short_answer_question" | "multiple_choice_question";
     function changingType(): void {
-        if (type === "short_answer_question") {
-            type = "multiple_choice_question";
+        let newType = type;
+
+        if (newType === "short_answer_question") {
+            newType = "multiple_choice_question";
         } else {
-            type = "short_answer_question";
+            newType = "short_answer_question";
         }
-        setType(type);
+        setType(newType);
     }
     return (
         <div>
