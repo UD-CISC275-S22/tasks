c613fe6a74c2e29c378ab7ee4f2952823edabc02
superamazingana <acdonato@udel.edu>
2023-03-17 09:28:38 -0400

    completed task 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,32 @@
 import React from "react";
 import "./App.css";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { ChangeType } from "./components/ChangeType";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
+import { Counter } from "./components/Counter";
 
-export function App(): JSX.Element {
+function App(): JSX.Element {
     return (
         <div className="App">
-            <h1 className="App-header">
-                UD CISC275 with React Hooks and TypeScript Ana Donato
-            </h1>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
-            </p>
-            <img
-                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.udel.edu%2F&psig=AOvVaw2o0RdJmTlPrLZu49udOHTQ&ust=1676405165253000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJCYhJCmk_0CFQAAAAAdAAAAABAD"
-                alt="University of Delaware"
-                width={"50%"}
-            />
-            <ul>
-                <li> Red </li>
-                <li> Blue </li>
-                <li> Yellow </li>
-                <li> Green </li>
-                <li> Orange </li>
-                <li> Purple </li>
-            </ul>
-            <Button onClick={() => console.log("Hello World!")}>Cick Me</Button>
-            <Container>
-                <Row>
-                    <div
-                        style={{
-                            backgroundColor: "red",
-                            height: 50,
-                            width: 150
-                        }}
-                    >
-                        <Col> Name Year Major Concentration </Col>
-                    </div>
-                    <Col>
-                        Ana Donato Sophmore Computer Science Cybersecurity
-                    </Col>
-                </Row>
-            </Container>
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript
+            </header>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
+
 export default App;
