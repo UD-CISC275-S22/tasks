f5346ba0af0d90824e0fd087d23c90396ee1a765
Nick Lago <nick@wifi-roaming-128-4-161-145.wifi.udel.edu>
2023-03-17 01:04:04 -0400

    Task 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Button, Container, Row, Col } from "react-bootstrap";
+import "./App.css";
+import { ChangeType } from "./components/ChangeType";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
+import { Counter } from "./components/Counter";
 
 function App(): JSX.Element {
     return (
@@ -34,6 +41,19 @@ function App(): JSX.Element {
                 <li>Drake</li>
                 <li>Drake</li>
             </ul>
+            <p>Hello World</p>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
