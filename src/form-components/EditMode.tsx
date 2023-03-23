37a67e88bc80a9c6bda0a719f6612f707cf970ba
Greg <godly@udel.edu>
2023-03-23 18:15:28 -0400

    solved task 10

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,68 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function EditMode(): JSX.Element {
+    const [name, setName] = useState<string>("");
+    const [enrolled, setEnrolled] = useState<boolean>(true);
+    const [editMode, setEditMode] = useState<boolean>(false);
+
+    function updateEnrolled(event: React.ChangeEvent<HTMLInputElement>) {
+        setEnrolled(event.target.checked);
+    }
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(event.target.checked);
+    }
+
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <div>
+                {enrolled ? (
+                    <span>{name} is a student </span>
+                ) : (
+                    <span> {name} is not a student </span>
+                )}
+            </div>
+            <div>
+                <Form.Group controlId="formInputAnswer">
+                    <div>
+                        <Form.Check
+                            type="switch"
+                            id="edit-mode-check"
+                            label="Edit Mode"
+                            checked={editMode}
+                            onChange={updateEditMode}
+                        />
+                    </div>
+                    {editMode && (
+                        <div>
+                            <div>
+                                <Form.Control
+                                    value={name}
+                                    onChange={updateName}
+                                    disabled={!editMode}
+                                />
+                            </div>
+
+                            <Form.Check
+                                type="switch"
+                                id="is-student-check"
+                                label="Is Student?"
+                                checked={enrolled}
+                                onChange={updateEnrolled}
+                                disabled={!editMode}
+                                hidden={!editMode}
+                            />
+                        </div>
+                    )}
+                </Form.Group>
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +12,31 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selection, setSelection] = useState<string>(options[0]);
+    function updateSelection(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelection(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <div>
+                <h3>Multiple Choice question</h3>
+            </div>
+            <div>
+                <Form.Group controlId="chooseOptions">
+                    <Form.Label>what is you faviorate color</Form.Label>
+                    <Form.Select value={selection} onChange={updateSelection}>
+                        {options.map((option: string) => (
+                            <option key={option}>
+                                {" "}
+                                {option}
+                                {Option}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {selection === expectedAnswer}? <div>✔️</div> : <div>❌</div>
+            </div>
         </div>
     );
 }
