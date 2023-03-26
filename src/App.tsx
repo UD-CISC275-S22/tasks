0d14039e788d21528ad20f92edcec2409586abeb
Mikaylla <mikahas@udel.edu>
2023-03-26 16:58:40 -0400

    Resolved merge conflict by incorporating both suggestions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import { ChangeType } from "./components/ChangeType";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
+import { Counter } from "./components/Counter";
 
 function App(): JSX.Element {
     return (
+<<<<<<< HEAD
         <>
             <h1>Mikaylla Website</h1>
             <div className="App">
@@ -66,6 +73,25 @@ function App(): JSX.Element {
             </div>
             ;
         </>
+=======
+        <div className="App">
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript
+            </header>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
+        </div>
+>>>>>>> upstream/task-state
     );
 }
 
