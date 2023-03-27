084b3da5be7e9d58436cdd5cd674a5db8f823611
Mikaylla <mikahas@udel.edu>
2023-03-27 13:56:04 -0400

    temporary

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, isVisible] = useState<boolean>(false);
+
+    const handleClick = () => {
+        isVisible(!visible);
+    };
+
+    return (
+        <div>
+            <Button onClick={handleClick}>Reveal Answer</Button>
+            {visible && <p>42</p>}
+        </div>
+    );
 }
