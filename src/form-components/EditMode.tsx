8b851164d43243a870d2bdc5cd546751b63bc3ed
arjunman0413 <124738563+arjunman0413@users.noreply.github.com>
2023-03-28 18:03:26 -0400

    Revert "Solved forms"

--- src/form-components/EditMode.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-export function EditMode(): JSX.Element {
-    return (
-        <div>
-            <h3>Edit Mode</h3>
-        </div>
-    );
-}
--- src/form-components/GiveAttempts.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-export function GiveAttempts(): JSX.Element {
-    return (
-        <div>
-            <h3>Give Attempts</h3>
-        </div>
-    );
-}
