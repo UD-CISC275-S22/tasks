c77bd9fbcb1e62bc8e0038e2d88343d0f9746781
Thomas <tpoeml@udel.edu>
2023-03-28 17:58:10 -0400

    Task 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,97 @@
-import React from "react";
+//import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import { Button, Col, Container, Row } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
+import React, { useState } from "react";
+
+type QuestionType = "multiple_choice_question" | "short_answer_question";
 
 function App(): JSX.Element {
+    //Counter
+    const [counter, setCounter] = useState<number>(0);
+    function addOne(): void {
+        setCounter(counter + 1);
+    }
+
+    //RevealAnswer
+    const [visible, setVisible] = useState<boolean>(true);
+    function flipVisibility(): void {
+        setVisible(!visible);
+    }
+
+    //ChangeType
+    const [question, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function changeQuestionType(): void {
+        setQuestionType(
+            question === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+
+    //StartAttempt
+    const [inProgress, setProgress] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
+
+    function changeProgress(): void {
+        setProgress(inProgress === false ? true : false);
+    }
+
+    function changeAttempts(): void {
+        setAttempts(attempts == 0 ? attempts + 1 : attempts - 1);
+    }
+
+    function startQuiz(): void {
+        changeProgress();
+        changeAttempts();
+    }
+
     return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-<<<<<<< HEAD
-
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Thomas Pelosi. Hello World.
-            </p>
-
-            <h1 className="App-header">This is header text</h1>
-
-            <img
-                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AHelloWorld.svg&psig=AOvVaw18-h4-f_Lvij29bKo8zJ0s&ust=1679492579426000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJjqlciT7f0CFQAAAAAdAAAAABAD"
-                alt="Image failed to load"
-            />
-
-            <ul>
-                <li>Steve</li>
-                <li>Aegis</li>
-                <li>Joker</li>
-            </ul>
-
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-
-            <Container>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                height: 50,
-                                width: 500,
-                                backgroundColor: "red"
-                            }}
-                        >
-                            First Column
-                        </div>
-                    </Col>
-                    <Col>
-                        <div
-                            style={{
-                                height: 50,
-                                width: 500,
-                                backgroundColor: "red"
-                            }}
-                        >
-                            Second Column
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-=======
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
+        <div>
+            <div>
+                <Button onClick={addOne}>{counter}</Button>
+            </div>
+            <div>
+                <Button name="/Reveal Answer/i" onClick={flipVisibility}>
+                    Reveal Answer
+                </Button>
+                {visible && <div>42</div>}
+            </div>
+            <div>
+                <Button onClick={changeQuestionType}>Change Type</Button>
+            </div>
+            <div>
+                {question === "short_answer_question" ? (
+                    <span>Short Answer</span>
+                ) : (
+                    <span>Multiple Choice</span>
+                )}
+            </div>
+            <div>
+                <Button
+                    onClick={startQuiz}
+                    disabled={inProgress || attempts == 0}
+                >
+                    Start Quiz
+                </Button>
+                <Button onClick={changeProgress} disabled={!inProgress}>
+                    Stop Quiz
+                </Button>
+                <Button onClick={changeAttempts} disabled={attempts != 0}>
+                    Mulligan
+                </Button>
+            </div>
+            <div>
+                <span>Attempts: {attempts}</span>
+            </div>
         </div>
     );
 }
