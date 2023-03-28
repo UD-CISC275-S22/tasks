e5fe1e0f4f9152e90cb3ecae14a3cc8933f17fcc
superamazingana <acdonato@udel.edu>
2023-03-28 16:51:37 -0400

    Completed Edit Mode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,23 +1,52 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Form, FormLabel } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [isEditMode, setIsEditMode] = useState<boolean>(false);
     const [isStudent, setIsStudent] = useState<boolean>(true);
+    const [studentName, setStudentName] = useState<string>("Your Name");
 
-    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
-        setIsStudent(event.target.checked);
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setStudentName(event.target.value);
     }
 
     return (
         <div>
-            <h3>Edit Mode</h3>
-            <Form.Check
-                type="switch"
-                id="is-happy-check"
-                label="Happy?"
-                checked={isStudent}
-                onChange={updateStudent}
-            />
+            <div>
+                <h3>Edit Mode</h3>
+                <Form.Check
+                    type="switch"
+                    id="is-edit-mode"
+                    label="Editing:"
+                    checked={isEditMode}
+                    onChange={() => setIsEditMode(!isEditMode)}
+                />
+            </div>
+            <div>
+                {isEditMode ? (
+                    <div>
+                        <Form.Label> Name: </Form.Label>
+                        <Form.Control
+                            value={studentName}
+                            onChange={updateName}
+                        />
+                        <Form.Check
+                            type="switch"
+                            id="is-student-check"
+                            label="Student?"
+                            checked={isStudent}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>
+                            ) => setIsStudent(event.target.checked)}
+                        />
+                    </div>
+                ) : (
+                    <div>
+                        {studentName + " "}
+                        {isStudent ? "is a student" : "is not a student"}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
