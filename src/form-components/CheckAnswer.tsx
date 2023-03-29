f853472e71ef033c628ceba88c95afb5f58ff076
Heni <heni@udel.edu>
2023-03-28 22:33:34 -0400

    FINISH1

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
