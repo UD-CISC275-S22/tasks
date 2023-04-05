0589845e0e5c38afcb73305c3b49a7203ade9ec5
Aidan Eyre <aweyre@udel.edu>
2023-04-05 14:04:42 -0400

    finished task 8

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,40 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, changeHoliday] = useState<string>("🥂");
+
+    function nextAlphabetical(): void {
+        if (holiday === "🦃") {
+            changeHoliday("🎄");
+        } else if (holiday === "🎄") {
+            changeHoliday("🎆");
+        } else if (holiday === "🎆") {
+            changeHoliday("🎃");
+        } else if (holiday === "🎃") {
+            changeHoliday("🥂");
+        } else if (holiday === "🥂") {
+            changeHoliday("🦃");
+        }
+    }
+
+    function nextYear(): void {
+        if (holiday === "🥂") {
+            changeHoliday("🎆");
+        } else if (holiday === "🎆") {
+            changeHoliday("🎃");
+        } else if (holiday === "🎃") {
+            changeHoliday("🦃");
+        } else if (holiday === "🦃") {
+            changeHoliday("🎄");
+        } else if (holiday === "🎄") {
+            changeHoliday("🥂");
+        }
+    }
+    return (
+        <div>
+            <Button onClick={nextAlphabetical}>Advance by Alphabet</Button>
+            <Button onClick={nextYear}>Advance by Year</Button>
+            <div>Holiday: ${holiday}</div>
+        </div>
+    );
 }
--- src/objects.ts
+++ src/objects.ts
@@ -3,7 +3,7 @@ import { Question, QuestionType } from "./interfaces/question";
 /**
  * Create a new blank question with the given `id`, `name`, and `type. The `body` and
  * `expected` should be empty strings, the `options` should be an empty list, the `points`
- * should default to 1, and `published` should default to false.
+ * should default to 1, and `published` should default to false
  */
 export function makeBlankQuestion(
     id: number,
