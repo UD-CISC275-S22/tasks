3db9b06bdb071912bfa662ae9858a29217ce74c6
Aidan Eyre <aweyre@udel.edu>
2023-04-05 11:03:02 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks an TypeScript
             </header>
             <p>Aidan Eyre</p>
             <p>
