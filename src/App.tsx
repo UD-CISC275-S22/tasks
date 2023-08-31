1ed8bca458b3087d38b1914091fc005535a4fe26
nmm001001 <nmaatnening@gmail.com>
2023-08-31 15:09:00 -0400

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <body>Hello World</body>
             <p>This is Nathan Manning.</p>
         </div>
     );
