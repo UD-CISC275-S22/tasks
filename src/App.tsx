b9de0b6d7e3e9029ffe176dab3e02dc6dda30fdf
nmm001001 <nmaatnening@gmail.com>
2023-08-31 09:54:58 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>This is Nathan Manning</p>
+            <p>Nathan Manning</p>
         </div>
     );
 }
