c21b19d4bd20f33c8161c8964419d5e9cfc07f28
Bryant Ferguson <btbf569708@gmail.com>
2023-08-31 13:48:01 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Bryant Ferguson was here.
             </p>
         </div>
     );
