eaad9ad8422cace278b1e293aaeddfa20e17b805
jasoncUD <jasonc@udel.edu>
2023-09-03 22:05:41 -0400

    Added more to my name

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <header>Jason Chan</header>
+            <header>Jason Chan is me</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
