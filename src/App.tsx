1b907a1ff0f78f0a31277a01449e30d694a818ee
Priyanka Chaudhuri <priankac@udel.edu>
2023-09-05 15:53:15 -0400

    filename

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 export function App(): JSX.Element {
-    return <h1>Program</h1>;
+    return <h1>CISC275 Priyanka Chaudhuri</h1>;
     return (
         <div>
             <h1>Hello World</h1>
@@ -48,7 +48,7 @@ export function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript, Name is Priyanka
+                UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
