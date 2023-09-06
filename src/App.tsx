6bd5e59a54516226089dee83b25e7aec414d1e30
Zachary Pruett <zpruett@udel.edu>
2023-09-05 21:10:34 -0400

    cheap

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App(): JSX.Element {
                 automatically reload.
             </p>
             <p>Zachary Pruett</p>
+            <p>Hello World!</p>
         </div>
     );
 }
