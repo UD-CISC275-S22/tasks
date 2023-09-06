6d9cb20e3c192b59dd114e72f0f10b5aeefc901b
Alex Maneri <amaneri@udel.edu>
2023-09-05 21:26:20 -0400

    lets go task 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks & TypeScript
+                UD CISC275 with React Hooks and TypeScript
             </header>
-            <h2>Alex M</h2>
-            <h3>Hello World</h3>
+            <h1>Alex M</h1>
+            <h2>Hello World</h2>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
