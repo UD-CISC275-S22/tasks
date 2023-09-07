91b08215d6fe67b2e62fb363becb1695fad55bb2
durga <durgamabhilash@gmail.com>
2023-09-06 23:01:03 -0400

    Added name Abhilash Durgam to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+                UM COS420 with React Hooks and TypeScript Abhilash Durgam
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
