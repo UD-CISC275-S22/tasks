b99744daafe9858483bad7052b18dc20408d6cd0
Priyanka Chaudhuri <priankac@udel.edu>
2023-09-08 15:34:21 -0400

    run

--- src/text.test.tsx
+++ src/text.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-Hello World
+
 test("renders the text 'Hello World' somewhere", () => {
     render(<App />);
     const texts = screen.getAllByText(/Hello World/);
