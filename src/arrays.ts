369b73149657d9941a5583e8557500c66b28ddbd
leochen <leochen@udel.edu>
2023-09-10 18:24:36 -0400

    Completed tripleNumbers()

--- src/arrays.ts
+++ src/arrays.ts
@@ -15,7 +15,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const doubled = numbers.map((num: number): number => num * 3);
+    return doubled;
 }
 
 /**
