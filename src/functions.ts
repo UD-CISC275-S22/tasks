c39b09398606880cbda71a60348d095092d03805
leochen <leochen@udel.edu>
2023-09-10 14:54:04 -0400

    Completed add3()

--- src/functions.ts
+++ src/functions.ts
@@ -12,7 +12,11 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum = 0;
+    sum += first > 0 ? first : 0;
+    sum += second > 0 ? second : 0;
+    sum += third > 0 ? third : 0;
+    return sum;
 }
 
 /**
