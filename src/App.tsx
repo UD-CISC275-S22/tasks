c36e5f09f93fd4a6984815176e4082aae98b1b34
Shreyash Parajuli <shreyash.parajuli@maine.edu>
2023-09-13 13:11:58 -0400

    Completed the assignment

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
+                <p>Hello World</p>
                 UM COS420 with React Hooks and TypeScript
             </header>
             <p>
