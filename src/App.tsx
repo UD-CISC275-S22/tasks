d2f8eadc358f80f519d145f60846edbcd61dcef9
Priyanka Chaudhuri <priankac@udel.edu>
2023-09-15 12:50:47 -0400

    filename

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,8 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 export function App(): JSX.Element {
-    return <h1>CISC275 Hello World HTML</h1>;
-    return (
-        <div>
-            <h1>Hello World</h1>
-            <img
-                src="../assets/images/pet-ada.jpg"
-                alt="A picture of my dog Ada"
-            />
-        </div>
-    );
-    return (
-        <div>
-            <Button>Log Hello World</Button>
-        </div>
-    );
-    return (
-        <div>
-            Ordered List:
-            <ol>
-                <li>chair</li>
-                <li>books</li>
-                <li>ice</li>
-            </ol>
-        </div>
-    );
-    return (
-        <div>
-            <Button onClick={() => console.log("Hello World!")}>
-                Click Me
-            </Button>
-        </div>
-    );
-    return (
-        <div>
-            <Container>
-                <Row>
-                    <Col>First column.</Col>
-                    <Col>Column</Col>
-                </Row>
-            </Container>
-        </div>
-    );
+    return <h1>CISC275 Hello World</h1>;
+
     return (
         <div className="App">
             <header className="App-header">
@@ -56,42 +16,55 @@ export function App(): JSX.Element {
             </p>
         </div>
     );
-    return <div>
-        <ol>
-            <li>pen</li>
-            <li>sharpener</li>
-            <li>ruler</li>
-        </ol>
-    </div>;
-    
-    return <div>
-        <h1>This is a Capybara</h1>
-        <img src="https://dsc.cloud/50757d/Capybara.jpg.webp" alt="This is a Capybara" />
-    </div>;
 
-    return <div>
-        <Button> Log Hello World</Button>
-    </div>
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <img
+                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.computerhope.com%2Fjargon%2Fd%2Fdesktopc.htm&psig=AOvVaw1DMQo2OekdIuCIRyYy30VA&ust=1694879818062000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNCm5739rIEDFQAAAAAdAAAAABAK"
+                alt="This is a Computer"
+            />
+        </div>
+    );
+    return (
+        <div>
+            <ol>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>TypeScript</li>
+            </ol>
+        </div>
+    );
 
-    return <div>
-        <Button onClick={ () => console.log("Hello World!")}>Log Hello World</Button>
-    </div>;
-    return <div>
-        <Container>
-            <Row>
-                <Col>First column
-                <div id="bx"></div>
-            
-                
-                </Col>
-                <Col>
-                <div id="bx"></div>
-                Hello world
-                
-                </Col>
-            </Row>
-        </Container>
-    </div>;
+    return (
+        <div>
+            <Button> Log Hello World</Button>
+        </div>
+    );
+
+    return (
+        <div>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+        </div>
+    );
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        Shape
+                        <div className="shape"></div>
+                    </Col>
+                    <Col>
+                        <div className="shape"></div>
+                        Hello world
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
