1b6141e9f68464f5333fd1f9663218e970584bbb
leochen <leochen@udel.edu>
2023-09-17 16:39:27 -0400

    Completed addOption()

--- src/objects.ts
+++ src/objects.ts
@@ -124,7 +124,9 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const newOptions = [...question.options, newOption];
+    const newQuestion = { ...question, options: newOptions };
+    return newQuestion;
 }
 
 /**
