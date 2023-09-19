a7ef1bd4ae7271c910556f303da9d14571bf3527
Priyanka Chaudhuri <priankac@udel.edu>
2023-09-18 22:39:58 -0400

    filename

--- src/App.tsx
+++ src/App.tsx
@@ -4,48 +4,42 @@ import "./App.css";
 
 export function App(): JSX.Element {
     return <h1>CISC275 Hello World HTML</h1>;
+
+
     return <div>
-            <h1>Hello World</h1>
-            <img
-                src="../assets/images/pet-ada.jpg"
-                alt="A picture of my dog Ada"
-            />
-        </div>;
-        
-    return (
-        <div>
+        <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.pcworld.com%2Fwp-content%2Fuploads%2F2023%2F07%2Fdell-g2723h-1.jpg%3Fquality%3D50%26strip%3Dall%26w%3D1024&tbnid=RKILh1veEncHjM&vet=12ahUKEwihsdWsz7WBAxUUFlkFHStWDpwQMygQegUIARCVAQ..i&imgrefurl=https%3A%2F%2Fwww.pcworld.com%2Farticle%2F1787245%2Fbest-budget-monitors.html&docid=wZ_EhPiYRkO6wM&w=1024&h=682&q=computer&client=safari&ved=2ahUKEwihsdWsz7WBAxUUFlkFHStWDpwQMygQegUIARCVAQ" alt="pic" />
+    </div>;
+    
+    return <div>
+    <ol>
+        <li>Python</li>
+        <li>Java</li>
+        <li>C</li>
+    </ol>
+    </div>;
+
+    return <div>
             <Button>Log Hello World</Button>
         </div>
-    );
-    return (
-        <div>
-            Ordered List:
-            <ol>
-                <li>chair</li>
-                <li>books</li>
-                <li>ice</li>
-            </ol>
-        </div>
-    );
-    return (
-        <div>
+
+
+
+    return <div>
             <Button onClick={() => console.log("Hello World!")}>
                 Click Me
             </Button>
-        </div>
-    );
-    return (
-        <div>
+        </div>;
+
+    return <div>
             <Container>
                 <Row>
                     <Col>First column.</Col>
                     <Col>Column</Col>
                 </Row>
             </Container>
-        </div>
-    );
-    return (
-        <div className="App">
+        </div>;
+
+    return <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
@@ -53,20 +47,7 @@ export function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-        </div>
-    );
-    return <div>
-        <ol>
-            <li>pen</li>
-            <li>sharpener</li>
-            <li>ruler</li>
-        </ol>
-    </div>;
-    
-    return <div>
-        <h1>This is a Capybara</h1>
-        <img src="https://dsc.cloud/50757d/Capybara.jpg.webp" alt="This is a Capybara" />
-    </div>;
+        </div>;
 
     return <div>
         <Button> Log Hello World</Button>
@@ -75,17 +56,18 @@ export function App(): JSX.Element {
     return <div>
         <Button onClick={ () => console.log("Hello World!")}>Log Hello World</Button>
     </div>;
+
     return <div>
         <Container>
             <Row>
-                <Col>First column
-                <div id="bx"></div>
+                <Col>
+                <div className="sq"></div>
             
                 
                 </Col>
                 <Col>
-                <div id="bx"></div>
-                Hello world
+                <div className="sq"></div>
+                
                 
                 </Col>
             </Row>
