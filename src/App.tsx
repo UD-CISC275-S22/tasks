db209a520a5fc9956b1b300beb0aaf87f2e893d7
Matthew Conlon <mpconlon@udel.edu>
2023-09-19 07:48:54 -0400

    uninstalled and reinstalled node attempting to run tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                Matthew Conlon UD CISC275 Hello World!
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <header className="App-header">Matthew Conlon UD CISC275</header>
+            <p>yeah dog! Hello World</p>
+            <img
+                src="./Images/cyclohexane.jpg"
+                alt="A picture of a chair conformation of cyclohexane"
+            />
+            Top three breakfast cereals:
+            <ul>
+                <li>3: Oops! All Berries</li>
+                <li>2: Captain Crunch</li>
+                <li>1: Cinnamon Toast Crunch</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
