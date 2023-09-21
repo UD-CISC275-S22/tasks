76d895e00db647b782d0eafdaa84f5d194ddd50c
leochen <leochen@udel.edu>
2023-09-21 12:45:28 -0400

    Completed getPublishedQuestions()

--- src/nested.ts
+++ src/nested.ts
@@ -6,7 +6,10 @@ import { Question, QuestionType } from "./interfaces/question";
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-    return [];
+    const published = questions.filter(
+        (question: Question): boolean => question.published
+    );
+    return published;
 }
 
 /**
