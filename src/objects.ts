40536a29702f9b508c26170fda8ebf60a2c99e2c
Zachary Pruett <zpruett@udel.edu>
2023-09-21 20:55:44 -0400

    3 left

--- src/objects.ts
+++ src/objects.ts
@@ -134,7 +134,11 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const newQuestion = { ...question };
+
+    newQuestion.published = !newQuestion.published;
+
+    return newQuestion;
 }
 
 /**
