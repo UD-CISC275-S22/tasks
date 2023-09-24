08bd58e4367de0c94a97bf21b8b727c4f0055444
Henry Grant <Henryg@udel.edu>
2023-09-24 14:27:16 -0400

    Done getNonEmptyQuestions

--- src/nested.ts
+++ src/nested.ts
@@ -15,7 +15,13 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    return questions.filter((question) => {
+        return (
+            question.body.trim() !== "" ||
+            question.expected.trim() !== "" ||
+            question.options.length > 0
+        );
+    });
 }
 
 /***
