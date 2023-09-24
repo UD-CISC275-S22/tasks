734e7fea91367d0b8c535fe133d84c985cc8f8c3
Matthew Conlon <mpconlon@udel.edu>
2023-09-24 15:11:08 -0400

    fixed header to say task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">Matthew Conlon UD CISC275</header>
-            <p>yeah dog! Hello World</p>
-            <img
-                src="./Images/cyclohexane.jpg"
-                alt="A picture of a chair conformation of cyclohexane"
-            />
+            <header className="App-header"> Matthew Conlon UD CISC275 </header>
+            <h1> Generic Task 3 header</h1>
+            <p> Hello World!</p>
+            <Container>
+                <Row>
+                    <Col
+                        style={{
+                            border: "3px red",
+                            height: "3px red",
+                            backgroundColor: "solid red"
+                        }}
+                    >
+                        yeah dog!
+                    </Col>
+                    <Col
+                        style={{
+                            border: "3px red",
+                            height: "3px red",
+                            backgroundColor: "solid red"
+                        }}
+                    >
+                        <img
+                            src="./Images/cyclohexane.jpg"
+                            alt="A picture of a chair conformation of cyclohexane"
+                        />
+                    </Col>
+                </Row>
+            </Container>
             Top three breakfast cereals:
             <ul>
                 <li>3: Oops! All Berries</li>
