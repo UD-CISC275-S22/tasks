3959f82d3b4e35c1708c3cd6aef46714c35493cf
Zachary Pruett <zpruett@udel.edu>
2023-09-25 02:41:24 -0400

    12 left

--- src/nested.ts
+++ src/nested.ts
@@ -52,7 +52,10 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    return [];
+    const find = questions.filter(
+        (question: Question): boolean => question.id != id
+    );
+    return find;
 }
 
 /***
