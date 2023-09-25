85309b9c62a4c785c92787484bb01dd80cd1dc16
Zachary Pruett <zpruett@udel.edu>
2023-09-25 02:51:36 -0400

    10 left

--- src/nested.ts
+++ src/nested.ts
@@ -72,7 +72,12 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const total = questions.reduce(
+        (Total: number, question: Question) => Total + question.points,
+        0
+    );
+
+    return total;
 }
 
 /***
