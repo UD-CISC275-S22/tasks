b1354cbb8178537121a4630c48c3aa1488540d11
leochen <leochen@udel.edu>
2023-09-27 16:17:51 -0400

    Completed ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,22 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [state, setState] = useState<QuestionType>("short_answer_question");
+    function changeState(): void {
+        setState(
+            state === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+    return (
+        <div>
+            <Button onClick={changeState}>Change Type</Button>
+            {state === "short_answer_question" ? (
+                <div>Short Answer</div>
+            ) : (
+                <div>Multiple Choice</div>
+            )}
+        </div>
+    );
 }
