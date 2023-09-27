22a4b2d3466e474c0ddf5f81c4ca3e72c1fcdfa9
leochen <leochen@udel.edu>
2023-09-27 18:16:21 -0400

    Completed StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,32 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [inProgress, setProgress] = useState<boolean>(false);
+    function minusAttempts(): void {
+        setAttempts(attempts - 1);
+    }
+    function addAttempts(): void {
+        setAttempts(attempts + 1);
+    }
+    function changeProgress(): void {
+        setProgress(!inProgress);
+    }
+    function update(): void {
+        minusAttempts();
+        changeProgress();
+    }
+    return (
+        <div>
+            <Button onClick={update} disabled={!inProgress || attempts === -1}>
+                Start Quiz
+            </Button>
+            <Button onClick={update} disabled={inProgress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={addAttempts} disabled={!inProgress}>
+                Mulligan
+            </Button>
+        </div>
+    );
 }
