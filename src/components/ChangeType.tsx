f1b1299797b0661c89f515e060a286cf42dee22c
Zachary Pruett <zpruett@udel.edu>
2023-09-27 13:01:40 -0400

    Two state complete

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,29 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+
+    function setNewType(): void {
+        if (type === "short_answer_question") {
+            setType("multiple_choice_question");
+        } else {
+            setType("short_answer_question");
+        }
+    }
+
+    if (type === "short_answer_question") {
+        return (
+            <div>
+                <Button onClick={setNewType}>Change Type</Button>
+                <div>Short Answer</div>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <Button onClick={setNewType}>Change Type</Button>
+                <div>Multiple Choice</div>
+            </div>
+        );
+    }
 }
