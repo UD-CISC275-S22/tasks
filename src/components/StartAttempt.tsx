76bd8e5dc361b40b0b32eb50d48937b9c2a95612
Brandon Marafino <Marafino@udel.edu>
2023-09-28 00:56:38 -0400

    modified:   src/components/StartAttempt.tsx

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
+//import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
 
 export function StartAttempt(): JSX.Element {
     const [run, toggleRun] = useState<boolean>(true);
