8952f5508e75e7e09521a83b69da23443d532a43
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-09-27 21:45:42 -0400

    new updates

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 
 /**
@@ -12,5 +12,39 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDice, setLeftDice] = useState<number>(1);
+    const [rightDice, setRightDice] = useState<number>(6);
+    const [gameOutcome, setGameOutcome] = useState<string | null>(null);
+
+    const rollLeftDice = () => {
+        const newValue = d6();
+        setLeftDice(newValue);
+        checkOutcome(newValue, rightDice);
+    };
+
+    const rollRightDice = () => {
+        const newValue = d6();
+        setRightDice(newValue);
+        checkOutcome(leftDice, newValue);
+    };
+
+    const checkOutcome = (left: number, right: number) => {
+        if (left === 1 && right === 1) {
+            setGameOutcome("Lose");
+        } else if (left === right) {
+            setGameOutcome("Win");
+        } else {
+            setGameOutcome(null); // Reset the game outcome
+        }
+    };
+
+    return (
+        <div>
+            <div data-testid="left-die">{leftDice}</div>
+            <div data-testid="right-die">{rightDice}</div>
+            <button onClick={rollLeftDice}>Roll Left</button>
+            <button onClick={rollRightDice}>Roll Right</button>
+            {gameOutcome && <div>{gameOutcome}</div>}
+        </div>
+    );
 }
