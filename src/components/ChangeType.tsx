d248dff50020d92865093b554a160a8a7ae8f5b3
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-09-27 21:40:20 -0400

    updated

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,25 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    const toggleQuestionType = () => {
+        setQuestionType((prevType) =>
+            prevType === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    };
+
+    return (
+        <div>
+            {/* Displaying the type in a more human-readable format */}
+            {questionType === "short_answer_question"
+                ? "Short Answer"
+                : "Multiple Choice"}
+            <Button onClick={toggleQuestionType}>Change Type</Button>
+        </div>
+    );
 }
