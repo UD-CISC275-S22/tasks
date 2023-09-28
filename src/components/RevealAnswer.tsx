e97e528550894d3bb0d32c83f4095b2d27c0f2d4
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-09-27 21:42:02 -0400

    new updates

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+    };
+
+    return (
+        <div>
+            <Button onClick={toggleVisibility}>Reveal Answer</Button>
+            {isVisible && <p>The answer is: 42</p>}
+        </div>
+    );
 }
