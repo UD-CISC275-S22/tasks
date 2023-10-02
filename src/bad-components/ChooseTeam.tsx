0de60418c13021bfd435393422e387a1087efbdf
Zachary Pruett <zpruett@udel.edu>
2023-10-02 13:02:28 -0400

    Complete but bug

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -11,21 +11,19 @@ const PEOPLE = [
 ];
 
 export function ChooseTeam(): JSX.Element {
-    const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
+    const [allOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
-        if (!team.includes(newMember)) {
-            team.push(newMember);
+    function chooseMember(name: string) {
+        const addMember = [...team];
+        if (!team.includes(name)) {
+            addMember.push(name);
+            setTeam(addMember);
         }
-        */
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        setTeam([]);
     }
 
     return (
@@ -36,7 +34,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -13,38 +13,38 @@ function ShoveBoxButton({
     );
 }
 
-function MoveableBox(): JSX.Element {
-    const [position, setPosition] = useState<number>(10);
-    return (
-        <div
-            data-testid="moveable-box"
-            style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: "lightblue",
-                border: "1px solid blue",
-                display: "inline-block",
-                verticalAlign: "bottom",
-                marginLeft: position + "px"
-            }}
-        ></div>
-    );
-}
-
 export function ShoveBox(): JSX.Element {
+    const [position, setPosition] = useState<number>(10);
+
+    function MoveableBox(): JSX.Element {
+        return (
+            <div
+                data-testid="moveable-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: "lightblue",
+                    border: "1px solid blue",
+                    display: "inline-block",
+                    verticalAlign: "bottom",
+                    marginLeft: position + "px"
+                }}
+            ></div>
+        );
+    }
     const box = MoveableBox();
 
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}</span>
             <div>
                 <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
+                    position={position}
+                    setPosition={setPosition}
                 ></ShoveBoxButton>
                 {box}
-            </div> */}
+            </div>
         </div>
     );
 }
