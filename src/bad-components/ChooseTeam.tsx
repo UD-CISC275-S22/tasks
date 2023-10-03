7fca67dd52cce01cc98cd553972c0f30eb01cb15
Brandon Marafino <Marafino@udel.edu>
2023-10-03 00:17:45 -0400

    Removed unused function

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -11,7 +11,7 @@ const PEOPLE = [
 ];
 
 export function ChooseTeam(): JSX.Element {
-    const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
+    const allOptions = PEOPLE;
     const [team, setTeam] = useState<string[]>([]);
 
     function chooseMember(newMember: string) {
