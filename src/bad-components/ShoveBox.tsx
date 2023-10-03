c4cccf19a3e8cc3c9077f0af93a19aeeda9f4add
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-10-02 23:18:52 -0400

    new updates

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-function ShoveBoxButton({
-    position,
-    setPosition
-}: {
+interface ShoveBoxButtonProps {
     position: number;
     setPosition: (newPosition: number) => void;
-}) {
+}
+
+function ShoveBoxButton({ position, setPosition }: ShoveBoxButtonProps) {
     return (
-        <Button onClick={() => setPosition(4 + position)}>Shove the Box</Button>
+        <Button onClick={() => setPosition(position + 4)}>Shove the Box</Button>
     );
 }
 
-function MoveableBox(): JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+interface MoveableBoxProps {
+    position: number;
+}
+
+function MoveableBox({ position }: MoveableBoxProps): JSX.Element {
     return (
         <div
             data-testid="moveable-box"
@@ -25,26 +27,23 @@ function MoveableBox(): JSX.Element {
                 border: "1px solid blue",
                 display: "inline-block",
                 verticalAlign: "bottom",
-                marginLeft: position + "px"
+                marginLeft: `${position}px`
             }}
         ></div>
     );
 }
 
 export function ShoveBox(): JSX.Element {
-    const box = MoveableBox();
+    const [position, setPosition] = useState<number>(10);
 
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}px from the left</span>
             <div>
-                <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
-                ></ShoveBoxButton>
-                {box}
-            </div> */}
+                <ShoveBoxButton position={position} setPosition={setPosition} />
+                <MoveableBox position={position} />
+            </div>
         </div>
     );
 }
