8034fe6faf4191b6a91940554b579756dab47661
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-10-02 23:18:22 -0400

    new updates

--- src/bad-components/DoubleHalfState.tsx
+++ src/bad-components/DoubleHalfState.tsx
@@ -1,3 +1,6 @@
 import { useState } from "react";
 
-export const [dhValue, setDhValue] = useState<number>(10);
+export function useDoubleHalfState() {
+    const [dhValue, setDhValue] = useState<number>(10);
+    return { dhValue, setDhValue };
+}
