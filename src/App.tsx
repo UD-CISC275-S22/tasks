6183110910060c72061bb9e5a0b0e9776cfb432c
Matthew Mestre <mestre@udel.edu>
2023-10-04 21:31:31 -0400

    progress #1

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,6 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <hr></hr>
-<<<<<<< HEAD
-            <DoubleHalf></DoubleHalf>
-=======
             <CheckAnswer expectedAnswer="42"></CheckAnswer>
             <hr></hr>
             <GiveAttempts></GiveAttempts>
@@ -40,7 +37,6 @@ function App(): JSX.Element {
             ></MultipleChoiceQuestion>
             <hr></hr>
             {/* <DoubleHalf></DoubleHalf> */}
->>>>>>> upstream/task-forms
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group className="mb-3" controlId="seeExpectedAnswer">
+                <Form.Label>Answer here:</Form.Label>
+                <Form.Control value={answer} onChange={updateAnswer} />
+            </Form.Group>
+            <div>
+                Is your answer correct?:{" "}
+                {answer === expectedAnswer ? <span>✔️</span> : <span>❌</span>}
+            </div>
         </div>
     );
 }
