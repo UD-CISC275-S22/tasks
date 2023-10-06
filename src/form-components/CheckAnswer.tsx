ca82e418f35c130df0598500beb2760d496f2ef8
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-10-06 00:42:41 -0400

    new updatas

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,26 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState("");
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <input
+                type="text"
+                value={userAnswer}
+                onChange={(e) => setUserAnswer(e.target.value)}
+                aria-label="user-answer"
+            />
+            {userAnswer === expectedAnswer ? (
+                <span role="img" aria-label="correct">
+                    ✔️
+                </span>
+            ) : (
+                <span role="img" aria-label="incorrect">
+                    ❌
+                </span>
+            )}
         </div>
     );
 }
