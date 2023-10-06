8f935b5117453b3282524a27136807d1eb393444
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-10-06 00:44:45 -0400

    new updatas

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,39 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedOption, setSelectedOption] = useState("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedOption(event.target.value);
+    };
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <select
+                value={selectedOption}
+                onChange={handleChange}
+                role="combobox"
+            >
+                <option value="" disabled>
+                    Select an option
+                </option>
+                {options.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+
+            {selectedOption === expectedAnswer ? (
+                <span role="img" aria-label="correct">
+                    ✔️
+                </span>
+            ) : (
+                <span role="img" aria-label="incorrect">
+                    ❌
+                </span>
+            )}
         </div>
     );
 }
