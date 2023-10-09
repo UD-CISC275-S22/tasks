b800b69cbab92fbfcc9a5c3b9ef9d25ac0121832
raymundo <rjen@udel.edu>
2023-10-09 14:22:19 -0400

    Added names

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App(): JSX.Element {
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
+		Raymundo Escobar
             </p>
         </div>
     );
