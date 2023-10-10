1212173fa3fe4633adb420fc7cd50f250d373d3e
Matthew Conlon <mpconlon@udel.edu>
2023-10-10 08:57:31 -0400

    fixing name3 add

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">Final Project</header>
-            <p>
-                James Lloyd, Kerry Ferguson, Matthew Conlon, name4, Arnav
-                Baliyan
-            </p>
+            <p>James Lloyd, Kerry Ferguson, name 3, name4, Arnav Baliyan</p>
         </div>
     );
 }
