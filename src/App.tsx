d167ef4280fae3ad1214da83e0b8884d29e5d421
kozmiik <132161110+kozmiik@users.noreply.github.com>
2023-10-11 13:32:18 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">Final Project</header>
             <p>
-                James Lloyd, Kerry Ferguson, Matthew Conlon, name4, Arnav
+                James Lloyd, Kerry Ferguson, Matthew Conlon, Caleb Sachetti, Arnav
                 Baliyan
             </p>
         </div>
