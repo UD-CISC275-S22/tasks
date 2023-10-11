2cb6f0cfdac07f1ad24944a30e17064d306ab67e
Aidan <aidwbell@udel.edu>
2023-10-11 12:41:55 -0400

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Aidan Bell
+                UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
+                <p>Aidan Bell</p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
