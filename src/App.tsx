567185a75fcdf6a2410e1c3a44979a5f282f7d5f
Mann <mannp@udel.edu>
2023-10-11 22:14:36 -0400

    New name added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div>
-            <p>Andrew Fonseca, Shrey Patel</p>
+            <p>Andrew Fonseca, Shrey Patel, Mann Patel</p>
         </div>
     );
 }
