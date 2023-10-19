d95f5ed776f80197ccd173315d0bb445cbe103ea
Nicole Kinsella <nkinsell@udel.edu>
2023-10-19 15:21:38 -0400

    Added class interface

---
+++ src/interfaces/class.ts
@@ -0,0 +1,8 @@
+export interface Quiz {
+    title: string;
+    courseCode: string;
+    preReq: [];
+    credits: number;
+    semester: "Fall" | "Winter" | "Spring" | "Winter";
+    year: number;
+}
