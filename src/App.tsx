06a74c1f52e05f30f65705e7074128e303b86b87
AntoineGravat <antoineg@udel.edu>
2023-10-22 12:18:49 -0400

    Changed header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import Image1 from "./Images/Delaware-Blue-Hens-Logo.png";
+import Image2 from "./Images/Udel-Crest.png";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={Image1} className="logo" />
-                <h1 className="Title">Udel Course Scheduler</h1>
+                <h1 className="Title">Udel CS Course Scheduler</h1>
+                <img src={Image2} className="logo" />
             </header>
         </div>
     );
