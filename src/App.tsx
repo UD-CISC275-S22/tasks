13236b8178059b1eaf13a8ffd58b95ffd84fd5ef
Zachary <zpruett@udel.edu>
2023-10-22 17:22:02 -0500

    working on add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./App.css";
 import Image1 from "./Images/Delaware-Blue-Hens-Logo.png";
+import { Button } from "react-bootstrap";
 import { AddClass } from "./semester-modification/AddClass";
+import { classes } from "./Interface/classes";
 import { semester } from "./Interface/semester";
 
 function App(): JSX.Element {
+    //Just testing data comment in if needed but will be commented out-----------------------
+    const work: classes = {
+        code: "A",
+        title: "A",
+        credits: 3,
+        preReq: [""],
+        schedule: { day: ["Monday"], time: "12:15" },
+        location: "A",
+        instructor: "A"
+    };
+    const testing: semester = {
+        classList: [
+            {
+                code: "A",
+                title: "A",
+                credits: 3,
+                preReq: [""],
+                schedule: { day: ["Monday"], time: "12:15" },
+                location: "A",
+                instructor: "A"
+            }
+        ]
+    };
+    //-----------------------------------------------------------------------------------------
     return (
         <div className="App">
             <header className="App-header">
@@ -12,7 +38,10 @@ function App(): JSX.Element {
                 <h1 className="Title">Udel Course Scheduler</h1>
             </header>
             <div>
-                <AddClass></AddClass>
+                Length of semester classes list: {testing.classList.length}
+            </div>
+            <div>
+                <AddClass {...testing} {...work}></AddClass>
             </div>
         </div>
     );
--- src/semester-modification/AddClass.tsx
+++ src/semester-modification/AddClass.tsx
@@ -6,29 +6,18 @@ import { semester } from "../Interface/semester";
 
 //set up forms to enter information then press button to add information to schedule
 
-//returns the class attempting to be added
-function Adding(
-    code: string,
-    title: string,
-    credits: number,
-    preReq: string[],
-    schedule: dateTime,
-    location: string,
-    instructor: string
-): classes {
-    const working: classes = {
-        code: code,
-        title: title,
-        credits: credits,
-        preReq: preReq,
-        schedule: schedule,
-        location: location,
-        instructor: instructor
-    };
-    return working;
-}
-//button that takes answers from forms then runs Adding to create a new course
 //after add the class to the end of the semester array
-export function AddClass(): JSX.Element {
-    return <div>Hello</div>;
+export function AddClass(schedule: semester, newClass: classes): JSX.Element {
+    function addNewClass() {
+        const holder = schedule;
+        holder.classList.push(newClass);
+
+        schedule.classList = holder.classList;
+    }
+
+    return (
+        <div>
+            <Button onClick={addNewClass}>Add Class</Button>
+        </div>
+    );
 }
