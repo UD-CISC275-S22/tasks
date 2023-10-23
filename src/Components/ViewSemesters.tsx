7b5f822bda5e7bfd17332e249a2153e86d76b9b6
westa <westa@udel.edu>
2023-10-23 12:53:35 -0400

    Setting up files

---
+++ src/Components/ViewSemesters.tsx
@@ -0,0 +1,4 @@
+//task 1: shows a single semester
+//task 2: shows multiple semesters
+//task 3: clear out semester
+//task 4: insert or remove a course in a semester
---
+++ src/Interfaces/degree.ts
@@ -0,0 +1,2 @@
+//look at cisc udel degree requirement examples
+//ex. what does the core need, capstone, options for lab science, etc.
