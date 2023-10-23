a6db010f693f84f85e1123bd1ddd8529157bd817
Adam <adambeck@udel.edu>
2023-10-23 19:42:39 -0400

    Added data for degrees and made interface, added 2 buttons on main page and added title CIS scheduler

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import React from "react";
 import "./App.css";
 
 function App(): JSX.Element {
+    const handleCreatePlan = () => {
+        console.log("Create plan button clicked");
+    };
+
+    const handleImportCSV = () => {
+        console.log("Import csv button clicked");
+    };
+
     return (
         <div className="App">
+            <h1 className="App-title">CIS scheduler</h1>
             <header className="App-header">
                 Adam Beck, Zach Reggio, Sam Ferguson, Brandon Marafino, Adam Liu
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <div className="button">
+                <button className="buttonSpacing" onClick={handleCreatePlan}>
+                    Create New Plan
+                </button>
+                <button onClick={handleImportCSV}>Import CSV</button>
+            </div>
         </div>
     );
 }
