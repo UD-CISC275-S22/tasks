53c421220852ccc8a5bcff1d222e699e7bcaa722
AntoineGravat <antoineg@udel.edu>
2023-10-23 14:46:57 -0400

    Added Main Buttons and changed Apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 import Image1 from "./Images/Delaware-Blue-Hens-Logo.png";
 //import { AddClass } from "./semester-modification/AddClass";
+//import { MainButtons } from "./MainScreen/MainButtons";
 import Image2 from "./Images/Udel-Crest.png";
+import { MainButtons } from "./MainScreen/MainButtons";
+//import { Button, Container, Row, Col } from "react-bootstrap";
+//import { useState } from "react";
 
 function App(): JSX.Element {
     return (
@@ -12,7 +16,8 @@ function App(): JSX.Element {
                 <h1 className="Title">Udel CS Course Scheduler</h1>
                 <img src={Image2} className="logo" />
             </header>
-            <div></div>
+            <hr></hr>
+            <MainButtons></MainButtons>
         </div>
     );
 }
