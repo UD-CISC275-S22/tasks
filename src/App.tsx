cd194ac8883038622b33051ed7e99f45ddec1eab
skyefi <nickyreigel@gmail.com>
2023-10-23 13:38:32 -0400

    Test of generateCSV working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
-import { generateCSV } from "./generateCSV";
+import { GenerateCSV } from "./generateCSV";
+
+const data: string[][] = [
+    ["Last Name, First Name"],
+    ["Nicky", "Reigel"],
+    ["Aidan", "Bell"]
+];
 
 function App(): JSX.Element {
     return (
@@ -9,14 +15,15 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Aidan Bell Edit <code>src/App.tsx</code> and save. This page
-                will automatically reload.
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload.
+                <p>Aidan Bell </p>
                 <p>Lawrence Collins </p>
                 <p>Nicky Reigel</p>
                 <p>Melvin Rau</p>
                 <p>Victor Vasquez</p>
             </p>
-            <generateCSV data="" filename="test"></generateCSV>
+            <GenerateCSV data={data} filename="testexport"></GenerateCSV>
         </div>
     );
 }
--- src/generateCSV.tsx
+++ src/generateCSV.tsx
@@ -3,7 +3,13 @@ import "./App.css";
 import { CSVLink } from "react-csv";
 import { Button } from "react-bootstrap";
 
-export function generateCSV(data: string, filename: string): JSX.Element {
+export function GenerateCSV({
+    data,
+    filename
+}: {
+    data: string[][];
+    filename: string;
+}): JSX.Element {
     return (
         <CSVLink data={data} filename={filename}>
             <Button>Download CSV</Button>
