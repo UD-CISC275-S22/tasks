e60af3b749fe459e9e65d15371a435fe2e0cf2d4
Nicole Kinsella <nkinsell@udel.edu>
2023-10-23 23:59:54 -0400

    Input class data 10/23

--- src/interfaces/class.ts
+++ src/interfaces/class.ts
@@ -1,8 +1,10 @@
 export interface Class {
     courseTitle: string;
     courseCode: string;
+    numPreReqs: number;
     preReqs: string[];
     credits: number;
     semester: "Fall" | "Winter" | "Spring" | "Summer";
     year: number;
+    taken: boolean;
 }
