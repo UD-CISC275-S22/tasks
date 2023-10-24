00c904483ca012ac3a42c0bb6500e3c63d19a253
Lawrence <larryc@udel.edu>
2023-10-24 11:15:19 -0400

    commits

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,42 +2,40 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    // const [attempts, setAttempts] = useState<number>(4);
-    // const [quizInProgress, setQuizInProgress] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
+    const [quizInProgress, setQuizInProgress] = useState<boolean>(false);
 
-    // const startQuiz = () => {
-    //     if (attempts > 0) {
-    //         setQuizInProgress(true);
-    //         setAttempts(attempts - 1);
-    //     }
-    // };
+    const startQuiz = () => {
+        if (attempts > 0) {
+            setQuizInProgress(true);
+            setAttempts(attempts - 1);
+        }
+    };
 
-    // const stopQuiz = () => {
-    //     setQuizInProgress(false);
-    // };
+    const stopQuiz = () => {
+        setQuizInProgress(false);
+    };
 
-    // const giveMulligan = () => {
-    //     setAttempts(attempts - 1);
-    // };
+    const giveMulligan = () => {
+        setAttempts(attempts - 1);
+    };
 
     return (
         <div>
-            {/* <Button
+            <Button
                 onClick={startQuiz}
                 disabled={quizInProgress || attempts === 0}
             >
-                {" "}
-                Start Quiz{" "}
+                Start Quiz
             </Button>
             <Button onClick={stopQuiz} disabled={!quizInProgress}>
-                {" "}
                 Stop Quiz
             </Button>
             <Button onClick={giveMulligan} disabled={quizInProgress}>
                 {" "}
                 Mulligan{" "}
             </Button>
-            <p> Attempts left: {attempts}</p> */}
+            <p> Attempts left: {attempts}</p>
         </div>
     );
 }
