f4eb2b68878aa2da4d61f0c47abe0ddcb8410a42
Miryam <miryam9000@EMAIL>
2023-10-27 12:09:08 -0400

    edited addSemester and removeSemester

--- src/Components/InsertRemoveSemestert.tsx
+++ src/Components/InsertRemoveSemestert.tsx
@@ -5,29 +5,17 @@ import sample from "../data/allClasses.json";
 
 
 export function InsertRemoveSemestert(): JSX.Element {
-    //perhaps make a button to add
-    //then a button to remove the semster
-    //each row will have a button to either add or remove a semester
-
-    //make an interface for semester, make it as it's own file, to use it elsewhere
-    //state in the main file, semesters-list of semesters
-    //to add -make a function, to remove you make a function 
-    const SEMESTER = sample.map(
-    (semester): Semester => ({
-        ...quiz,
-        questionList: quiz.questionList.map(
-            (q): Question => ({
-                ...q,
-                submission: "",
-                type: q.type as QuestionType
-            })
-        )
-    })
-);
-
+    const SEMESTER = sample.map();
 
     const [semester, setSemester] = useState<Class[]>(SEMESTER);
 
+    function addSemester(courseCode: string) {
+
+    }
+
+    function removeSemester(courseCode: string) {
+        setSemester(semester.filter((q: Class): boolean => courseCode !== q.courseCode));
+    }
 
 
     return ();
