eee28c79a532f0e4ec88fabd0b1934b8b3189acf
nmm001001 <nmaatnening@gmail.com>
2023-10-29 19:02:52 -0400

    Nathan Commit 10/29

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./App.css";
 import Image1 from "./Images/Delaware-Blue-Hens-Logo.png";
+import { SemesterTable } from "./semester/SemesterTable";
+//import { MainButtons } from "./MainScreen/MainButtons";
+import Image2 from "./Images/Udel-Crest.png";
+import { MainButtons } from "./MainScreen/MainButtons";
+import WelcomeMessage from "./welcome";
+//import { Button, Container, Row, Col } from "react-bootstrap";
+//import { useState } from "react";
 
 function App(): JSX.Element {
     return (
@@ -10,6 +17,12 @@ function App(): JSX.Element {
                 <h1 className="Title">Udel CS Course Scheduler</h1>
                 <img src={Image2} className="logo" />
             </header>
+            <WelcomeMessage></WelcomeMessage>
+            <div>
+                <SemesterTable></SemesterTable>
+            </div>
+            <hr></hr>
+            <MainButtons></MainButtons>
         </div>
     );
 }
