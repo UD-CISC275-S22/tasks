68e7d361eaae92d8bc202ebe663b09afff369c30
ddawood <ddawood@udel.edu>
2023-11-06 13:23:21 -0500

    adding requirements and semester interface

---
+++ src/Interfaces/requirements.ts
@@ -0,0 +1,227 @@
+/*This is the degree requirements for both CSBA and CSBS,
+it also includes university and college breadth requirements.
+Displayed order is slightly different from catalog, scroll down
+to see breadths.*/
+
+/*Core Class Requirements */
+/*CSBA*/
+export const CoreBA = [
+    "CISC108",
+    "CISC181",
+    "CISC210",
+    "CISC220",
+    "CISC260",
+    "CISC275",
+    "MATH210",
+    "MATH241",
+    "ENGL11O"
+];
+
+/*CSBS*/
+export const CoreBS = [
+    "CISC108",
+    "CISC181",
+    "CISC210",
+    "CISC220",
+    "CISC260",
+    "CISC275",
+    "CISC303",
+    "CISC320",
+    "CISC361",
+    "CISC372",
+    "MATH210",
+    "MATH241",
+    "MATH242",
+    "CISC355",
+    "ENGL11O"
+];
+
+/*University and College Requirements*/
+export const math = ["MATH205", "MATH350"];
+export const secondWriting = ["ENGL312", "ENGL410"];
+export const capstoneOp1 = ["CISC498", "CISC499"];
+export const capstoneOp2 = ["UNIV401", "UNIV402"];
+export const additionalReqs = ["CISC304", "MATH349"];
+export const labSciences = [
+    "BISC207",
+    "BISC208",
+    "CHEM103",
+    "CHEM133",
+    "CHEM104",
+    "CHEM134",
+    "GEOL105",
+    "GEOL1115",
+    "GEOL107",
+    "GEOL107",
+    "GEOL110",
+    "PHYS207",
+    "PHYS227",
+    "PHYS208",
+    "PHYS228"
+];
+
+/* Concentrations */
+
+export const ArtificialIntelligence = [
+    "CISC304",
+    "CISC442",
+    "CISC481",
+    "CISC483",
+    "CISC484"
+];
+
+export const Bioinformatics = [
+    "BISC207",
+    "BISC208",
+    "BISC401",
+    "CHEM103",
+    "CHEM133",
+    "CHEM104",
+    "CHEM134",
+    "CISC372",
+    "CISC436",
+    "MATH242",
+    "MATH349",
+    [
+        ["CHEM213", "CHEM215"],
+        ["CHEM321", "CHEM325"]
+    ],
+    ["CISC483", "CISC484"]
+];
+
+export const Cybersecurity = [
+    "CISC361",
+    "CISC372",
+    "CISC450",
+    "CISC464",
+    "CPEG465",
+    "CISC494",
+    ["CPEG472", "CPEG473", "CPEG475", "CPEG476", "CPEG495"]
+];
+
+export const DataScience = [
+    "CISC304",
+    "CISC372",
+    "CISC437",
+    "CISC481",
+    "MATH205",
+    "MATH242",
+    "MATH243",
+    "MATH349",
+    ["CISC483", "CISC484"],
+    ["MATH302", "MATH350", "MATH426"]
+];
+
+export const HighPerformanceComputingMath = [
+    "CISC360",
+    "CISC361",
+    "CISC372",
+    "CISC450",
+    "CISC471",
+    "MATH242",
+    "MATH243",
+    "MATH351",
+    "MATH428",
+    ["CISC300+", "MATH205", "MATH350", "CISC387", "CISC487"]
+];
+
+export const HighPerformanceComputingData = [
+    "CISC360",
+    "CISC361",
+    "CISC372",
+    "CISC450",
+    "CISC471",
+    "MATH242",
+    "MATH243",
+    "CISC437",
+    "MATH350",
+    "MATH450",
+    ["CISC483", "CISC484"],
+    [
+        "CISC300+",
+        "MATH302",
+        "MATH349",
+        "MATH451",
+        "MATH535",
+        "CISC387",
+        "CISC487"
+    ]
+];
+
+export const SystemsNetworks = [
+    "CISC360",
+    "CISC361",
+    "CISC372",
+    "CISC450",
+    "CISC471",
+    ["MATH205", "MATH350"],
+    [
+        "CISC464",
+        "CPEG465",
+        "CPEG470",
+        "CPEG476",
+        "CPEG473",
+        "CPEG475",
+        "CPEG497"
+    ],
+    [
+        "CISC437",
+        "CISC453",
+        "CISC459",
+        "CISC464",
+        "CISC474",
+        "CISC475",
+        "CISC479",
+        "CPEG473",
+        "CPEG497"
+    ]
+];
+
+export const TheoryComputationDiscrete = [
+    "CISC304",
+    "CISC401",
+    "MATH242",
+    "MATH349",
+    ["CISC4O4", "MATH245", "MATH315", "MATH451"]
+];
+
+export const TheoryComputationCont = [
+    "CISC304",
+    "CISC401",
+    "MATH242",
+    "MATH349",
+    ["MATH243", "MATH302", "MATH535", "MATH426"]
+];
+
+export const Concentrations = [
+    ArtificialIntelligence,
+    Bioinformatics,
+    Cybersecurity,
+    DataScience,
+    HighPerformanceComputingMath,
+    HighPerformanceComputingData,
+    SystemsNetworks,
+    TheoryComputationDiscrete,
+    TheoryComputationCont
+];
+
+/*University FYE Requirement*/
+export const FYE = ["ARSC116", "BHAN135"];
+
+/*University DLE Requirement*/
+export const DLE = ["ACCT363", "ACCT364"];
+
+/*University Multicultural Requirement*/
+export const Multicultural = ["AFRA110", "AFRA193"];
+
+/*University Breadth A Requirement*/
+export const BreadthA = ["AFRA206", "AFRA250"];
+
+/*University Breadth B Requirement*/
+export const BreadthB = ["AFRA110", "AFRA193"];
+
+/*University Breadth C Requirement*/
+export const BreadthC = ["AFRA205", "AFRA223"];
+
+/*University Breadth D Requirement*/
+export const BreadthD = ["AGRI100", "ANFS101"];
