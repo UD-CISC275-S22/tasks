c6155d6a70be948227c2f49f8269215d982c1f78
ddawood <ddawood@udel.edu>
2023-11-07 17:07:13 -0500

    edited layout and added welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,22 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-                <p>CISC275 Final-Project</p>
+                UD CISC275 - Degree Planner
+                <p>Matthew Mestre</p>
+                <p>Bryant Ferguson</p>
+                <p>Ava West</p>
+                <p>Malika Iyer</p>
+                <p>Dina Dawood</p>
             </header>
-            <header>Matthew Mestre</header>
-            <header>Bryant Ferguson </header>
-            <header>Ava West</header>
-            <header>Malika Iyer</header>
-            <header>Dina Dawood</header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                Welcome to your personal Degree Planner! Some basic tools have
+                been designed to make your visit accessible to your needs; this
+                includes creating new plans from scratch and/or compiling data
+                from a csv file. You will be able to add, remove, edit, and save
+                various courses/semesters in a multitude of plans. Scroll to the
+                bottom of the page for more instructions (will enhance this
+                later by making an expandable/disposable button near the top of
+                the page).
             </p>
             <hr></hr>
             <ViewSemester></ViewSemester>
