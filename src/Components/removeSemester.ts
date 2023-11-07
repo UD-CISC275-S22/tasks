944d5146f5403b7e056037b788bf505e97e824eb
ddawood <ddawood@udel.edu>
2023-11-07 16:54:03 -0500

    base file for removing a single semester

---
+++ src/Components/removeSemester.ts
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import { Course } from "../Interfaces/course";
+import { Semester } from "../Interfaces/semester";
+import { Degree } from "../Interfaces/degree";
+
+export function removeSemester({
+    
+})
