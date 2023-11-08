1196ec39c9a7221f2f6cab51061e7198cb242581
Joshua Babbitt <boshuaj@udel.edu>
2023-11-08 13:20:11 -0500

    testing

---
+++ src/Components/EditDelete.tsx
@@ -0,0 +1,10 @@
+import React from React;
+import { Class } from "../interfaces/class";
+import QuickAdd from "./QuickAdd";
+import SlowAdd from "./SlowAdd";
+
+
+
+function EditDelete(): JSX.Element {
+    return <div></div>;
+}
