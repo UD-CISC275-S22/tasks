7ddab72210de8c8034b4cec2e693d1ea1f10a128
JAMES <jclloyd@udel.edu>
2023-11-09 18:20:51 -0500

    added interfaces and changed the way the table is implemented

---
+++ src/classData.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+export const data = [
+    { code: "EGGG101", name: "Introduction to Engineering", credits: 3 },
+    { code: "CISC108", name: "Introduction to Computer Science I", credits: 3 },
+    { code: "MATH241", name: "Analytic Geomnetry & Calculus A", credits: 4 },
+    { code: "ENGL110", name: "Seminar in Composition", credits: 3 },
+    { code: "PHIL101", name: "Great Western Philosophers", credits: 3 }
+];
