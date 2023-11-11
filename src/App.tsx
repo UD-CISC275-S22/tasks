3e6b0d3cff450ba3112804d49d5b1bb07d5b2c39
KerryFerguson04 <kcf@udel.edu>
2023-11-11 17:50:15 -0500

    committing to be able to merge and get the newer changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,273 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App(): JSX.Element {
+    const [newName, setNewName] = useState<string>("New Name");
     return (
         <div className="App">
             <header className="App-header">Final Project</header>
             <p>James Lloyd, Kerry Ferguson, name3, name4, name5</p>
-            <p> List of Degree Plans: </p>
+            <p>Sample Degree Plan</p>
+            <p>Fall 2023 Courses: </p>
             <table>
                 <tr>
-                    <td>First Year</td>
+                    <td>Course</td>
+                    <td>Credits</td>
                 </tr>
                 <tr>
-                    <td> Fall Semester</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td> Spring Semester </td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>EGG</td>
-                    <td>101</td>
-                    <td>Introduction to Engineering (FYE)</td>
-                    <td>2</td>
-                    <td>EGG</td>
-                    <td>101</td>
-                    <td>Introduction to Engineering (FYE)</td>
+                    <td>EGG 101: Introduction to Engineering (FYE)</td>
                     <td>2</td>
                 </tr>
                 <tr>
-                    <td>CISC</td>
-                    <td>108</td>
-                    <td>Introduction to Computer Science I</td>
-                    <td>3</td>
-                    <td>CISC</td>
-                    <td>108</td>
-                    <td>Introduction to Computer Science I</td>
+                    <td>CISC 108: Introduction to Computer Science I</td>
                     <td>3</td>
                 </tr>
                 <tr>
-                    <td>MATH</td>
-                    <td>241</td>
-                    <td>Analytic Geomnetry & Calculus A</td>
-                    <td>4</td>
-                    <td>MATH</td>
-                    <td>241</td>
-                    <td>Analytic Geomnetry & Calculus A</td>
+                    <td>MATH 241: Analytic Geomnetry & Calculus A</td>
                     <td>4</td>
                 </tr>
                 <tr>
-                    <td>ENGL</td>
-                    <td>110</td>
-                    <td>Seminar in Composition</td>
-                    <td>3</td>
-                    <td>ENGL</td>
-                    <td>110</td>
-                    <td>Seminar in Composition</td>
+                    <td>ENGL 110: Seminar in Composition</td>
                     <td>3</td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td>Breadth Requirement Elective 1</td>
-                    <td>3</td>
-                    <td></td>
-                    <td></td>
                     <td>Breadth Requirement Elective 1</td>
                     <td>3</td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>15</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>15</td>
-                </tr>
-                <tr>
-                    <td>Second Year</td>
-                </tr>
-                <tr>
-                    <td>EGG</td>
-                    <td>101</td>
-                    <td>Introduction to Engineering (FYE)</td>
-                    <td>2</td>
-                    <td>EGG</td>
-                    <td>101</td>
-                    <td>Introduction to Engineering (FYE)</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>CISC</td>
-                    <td>108</td>
-                    <td>Introduction to Computer Science I</td>
-                    <td>3</td>
-                    <td>CISC</td>
-                    <td>108</td>
-                    <td>Introduction to Computer Science I</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td>MATH</td>
-                    <td>241</td>
-                    <td>Analytic Geomnetry & Calculus A</td>
-                    <td>4</td>
-                    <td>MATH</td>
-                    <td>241</td>
-                    <td>Analytic Geomnetry & Calculus A</td>
-                    <td>4</td>
-                </tr>
-                <tr>
-                    <td>ENGL</td>
-                    <td>110</td>
-                    <td>Seminar in Composition</td>
-                    <td>3</td>
-                    <td>ENGL</td>
-                    <td>110</td>
-                    <td>Seminar in Composition</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td>Breadth Requirement Elective 1</td>
-                    <td>3</td>
-                    <td></td>
-                    <td></td>
-                    <td>Breadth Requirement Elective 1</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>15</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>15</td>
-                </tr>
-                <tr>
-                    <td>Third Year</td>
-                </tr>
-                <tr>
-                    <td>EGG</td>
-                    <td>101</td>
-                    <td>Introduction to Engineering (FYE)</td>
-                    <td>2</td>
-                    <td>EGG</td>
-                    <td>101</td>
-                    <td>Introduction to Engineering (FYE)</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>CISC</td>
-                    <td>108</td>
-                    <td>Introduction to Computer Science I</td>
-                    <td>3</td>
-                    <td>CISC</td>
-                    <td>108</td>
-                    <td>Introduction to Computer Science I</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td>MATH</td>
-                    <td>241</td>
-                    <td>Analytic Geomnetry & Calculus A</td>
-                    <td>4</td>
-                    <td>MATH</td>
-                    <td>241</td>
-                    <td>Analytic Geomnetry & Calculus A</td>
-                    <td>4</td>
-                </tr>
-                <tr>
-                    <td>ENGL</td>
-                    <td>110</td>
-                    <td>Seminar in Composition</td>
-                    <td>3</td>
-                    <td>ENGL</td>
-                    <td>110</td>
-                    <td>Seminar in Composition</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td>Breadth Requirement Elective 1</td>
-                    <td>3</td>
-                    <td></td>
-                    <td></td>
-                    <td>Breadth Requirement Elective 1</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>15</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>15</td>
-                </tr>
-                <tr>
-                    <td>Fourth Year</td>
-                </tr>
-                <tr>
-                    <td>EGG</td>
-                    <td>101</td>
-                    <td>Introduction to Engineering (FYE)</td>
-                    <td>2</td>
-                    <td>EGG</td>
-                    <td>101</td>
-                    <td>Introduction to Engineering (FYE)</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>CISC</td>
-                    <td>108</td>
-                    <td>Introduction to Computer Science I</td>
-                    <td>3</td>
-                    <td>CISC</td>
-                    <td>108</td>
-                    <td>Introduction to Computer Science I</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td>MATH</td>
-                    <td>241</td>
-                    <td>Analytic Geomnetry & Calculus A</td>
-                    <td>4</td>
-                    <td>MATH</td>
-                    <td>241</td>
-                    <td>Analytic Geomnetry & Calculus A</td>
-                    <td>4</td>
-                </tr>
-                <tr>
-                    <td>ENGL</td>
-                    <td>110</td>
-                    <td>Seminar in Composition</td>
-                    <td>3</td>
-                    <td>ENGL</td>
-                    <td>110</td>
-                    <td>Seminar in Composition</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td>Breadth Requirement Elective 1</td>
-                    <td>3</td>
-                    <td></td>
-                    <td></td>
-                    <td>Breadth Requirement Elective 1</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>15</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>Total Number of Credits:</td>
                     <td>15</td>
                 </tr>
             </table>
