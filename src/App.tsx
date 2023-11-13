32cbaa1d8d468d5be83f66ced59c34241303bec4
Matthew Conlon <mpconlon@udel.edu>
2023-11-12 22:39:51 -0500

    updated modal things

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,20 @@ import { ModalBody } from "react-bootstrap";
 //import { Button } from "react-bootstrap";
 //import { data } from "./classData";
 function App(): JSX.Element {
+    const modal = document.getElementById("myModal") as HTMLElement;
+    const btn = document.getElementById("myBtn") as HTMLElement;
+    const span = document.getElementsByClassName("close")[0] as HTMLElement;
+    btn.onclick = () => {
+        modal.style.display = "block";
+    };
+    span.onclick = () => {
+        modal.style.display = "none";
+    };
+    window.onclick = (event: Event) => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
     return (
         <div className="App">
             <header className="App-header">UD Course Planner</header>
@@ -12,6 +26,13 @@ function App(): JSX.Element {
                 James Lloyd, Kerry Ferguson, Matthew Conlon, Caleb Sachetti,
                 Arnav Baliyan
             </p>
+            <button id="myBtn">Show Instructions</button>
+            <div id="myModal" className="modal">
+                <div className="modal-content">
+                    <span className="close">&times;</span>
+                    <p>Sample Text</p>
+                </div>
+            </div>
             <p> List of Degree Plans: </p>
             <table>
                 <tr>
@@ -20,73 +41,6 @@ function App(): JSX.Element {
                     <th>Credits</th>
                 </tr>
             </table>
-            <div className="App">
-                <button className="btn" onClick={toggleModal}>
-                    Show Instructions
-                </button>
-                <Modal
-                    isOpen={isOpen}
-                    onRequestClose={toggleModal}
-                    contentLabel="My dialog"
-                >
-                    <p>Hello CISC or INSY Majors/Minors</p>
-                    <ModalBody>
-                        <p>
-                            To get started, you should click <b>Add New Plan</b>{" "}
-                            to create a new plan that can be renamed by clicking
-                            the pencil right next to the New Plan that pops up.
-                            To get the new name to appear in the dropdown of
-                            plans, you should click the <b>Save Plan Changes</b>{" "}
-                            button.
-                        </p>
-                        <p>
-                            From there, click on <b>Add Semester</b> to add a
-                            semeseter. You can change the name of the semester
-                            in the same way you did with the plan name. Then, to
-                            add a course, click <b>Add Course</b>.{" "}
-                        </p>
-                        <p>
-                            From the dropdown, type in your course code, and
-                            click the course code from the dropdown to confirm
-                            the choice. Then click <b>Add Course</b> to add the
-                            course to the semester table. If the course is not
-                            to your liking when in the semester table, you can
-                            hit the <b>Edit Course</b> button to alter the
-                            course. To fully save the changes, click the{" "}
-                            <b>Save Plan Changes</b> button.
-                        </p>
-                        <p>
-                            Should you want to delete a course, semester, or
-                            plan, click the button, there is a button for each.
-                        </p>
-                        <p>
-                            Some other additional features that are available
-                            are the degree requirements, which can be seen from
-                            clicking the <b>Degree Requirements</b> button. In
-                            here, you can see all the courses that you need to
-                            take along with some other breadth requirements for
-                            a basic Computer Science degree. Courses and
-                            breadths that are satisfied will have a check mark
-                            next to them and those that are not satisfied will
-                            have an x mark next to them.
-                        </p>
-                        <p>
-                            <b>
-                                Most importantly, make sure to click Save Plan
-                                Changes if you want changes kept before you
-                                switch plans or click off the site.
-                            </b>
-                        </p>
-                    </ModalBody>
-                    <button
-                        onClick={toggleModal}
-                        className="btnclose"
-                        aria-label="Close"
-                    >
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </Modal>
-            </div>
         </div>
     );
 }
