b5d44332536c61558917c19b4f777af1912a77ce
Matthew Conlon <mpconlon@udel.edu>
2023-11-12 22:57:32 -0500

    removed bugged modal content

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,6 @@ import "./App.css";
 //import { Button } from "react-bootstrap";
 //import { data } from "./classData";
 function App(): JSX.Element {
-    const modal = document.getElementById("myModal") as HTMLElement;
-    const btn = document.getElementById("myBtn") as HTMLElement;
-    const span = document.getElementsByClassName("close")[0] as HTMLElement;
-    btn.onclick = () => {
-        modal.style.display = "block";
-    };
-    span.onclick = () => {
-        modal.style.display = "none";
-    };
-    window.onclick = (event: Event) => {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    };
     return (
         <div className="App">
             <header className="App-header">UD Course Planner</header>
