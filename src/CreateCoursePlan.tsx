e3e22dada68854ad8cf5852573f2ad51ca153321
Brandon Marafino <Marafino@udel.edu>
2023-11-15 12:51:05 -0500

    new file:   src/CreateCoursePlan.tsx 	new file:   src/data/couresplans.json

---
+++ src/CreateCoursePlan.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import { Course } from "./interfaces/course";
+// import { CoursePlan } from "./interfaces/semester";
+// import { JsxElement } from "typescript";
+//Courses:Course[],CurCoureplan:CoursePlan
+export function CoursePlanEditor(): JSX.Element {
+    return (
+        <div className="Containter-lg">
+            <div>
+                <div className="row">
+                    <div className="col-lg-3">List</div>
+                    <div className="col-lg-9">Courses</div>
+                </div>
+            </div>
+        </div>
+    );
+}
