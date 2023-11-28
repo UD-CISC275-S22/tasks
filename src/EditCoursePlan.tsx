b37876e7ff4fd3d3514f7360c179e2fa2ce4b657
Adam <adambeck@udel.edu>
2023-11-28 15:38:38 -0500

    added add semester button and fixed remove button

--- src/EditCoursePlan.tsx
+++ src/EditCoursePlan.tsx
@@ -250,7 +250,7 @@ function Year({
                 </tbody>
             </Table>
             <Button variant="primary" onClick={addSemesterToYear}>
-                Add Semester
+                Add semester
             </Button>
         </div>
     );
