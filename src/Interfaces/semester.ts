af30b836fdc0672ea51f59d2357deea18909fab4
ddawood <ddawood@udel.edu>
2023-11-29 16:38:58 -0500

    comments in and edit in variable name

--- src/Interfaces/semester.ts
+++ src/Interfaces/semester.ts
@@ -1,10 +1,15 @@
 import { Course } from "./course";
 
 export interface Semester {
+    //random id number
     id: number;
+    //name of semester
     title: string;
+    //year of occuring semester
     year: number;
     //totalCredits: number;
-    description: string;
+    //notes and additional info added by user
+    notes: string;
+    //list of courses in semester
     courseList: Course[];
 }
