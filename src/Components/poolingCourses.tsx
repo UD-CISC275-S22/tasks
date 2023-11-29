a1ebe4ab362878ee9fd9dbf5280344b95b75e56a
ddawood <ddawood@udel.edu>
2023-11-29 13:42:20 -0500

    pooling objects to make loading faster

---
+++ src/Components/poolingCourses.tsx
@@ -0,0 +1,7 @@
+export {}
+// import React from "react";
+// // import { ViewRequirements } from "./Requirements";
+// import { Semester } from "../Interfaces/semester";
+// import { Course } from "../Interfaces/course";
+// import { Plan } from "../Interfaces/plan";
+// import { courseOrigin } from "./courseOrigin";
