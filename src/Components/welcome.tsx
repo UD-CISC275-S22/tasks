4f7615b8ea1f30c17832588df13c23a685f21857
ddawood <ddawood@udel.edu>
2023-12-03 12:07:00 -0500

    debugged some lines in welcome.tsx

--- src/Components/welcome.tsx
+++ src/Components/welcome.tsx
@@ -1,31 +1,27 @@
-import React from "react";
-import { Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 
-export function Welcome({
-    show,
-    handleClose
-}: {
-    show: boolean;
-    handleClose: () => void;
-}): JSX.Element {
+export function Welcome() {
+    const [show, settingDisplay] = useState(true);
+    const handleClose = () => settingDisplay(false);
+    const handleShow = () => settingDisplay(true);
+
     return (
         <div>
+            <Button onClick={handleShow}> Need Help❓</Button>
             <Modal show={show} close={handleClose} animation={false}>
                 <Modal.Header closeButton>
                     <Modal.Title>Degree Planner</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <header>
-                        Welcome to your personal Degree Planner! Some basic
-                        tools have been designed to make your visit accessible
-                        to your needs; this includes creating new plans from
-                        scratch and/or compiling data from a csv file. You will
-                        be able to add, remove, edit, and save various
-                        courses/semesters in a multitude of plans. Scroll to the
-                        bottom of the page for more instructions (will enhance
-                        this later by making an expandable/disposable button
-                        near the top of the page).
-                    </header>
+                    Welcome to your personal Degree Planner! Some basic tools
+                    tools have been designed to make your visit accessible to
+                    your needs; this includes creating new plans from scratch
+                    and/or compiling data from a csv file. You will be able to
+                    add, remove, edit, and save various courses/semesters in a
+                    multitude of plans. Scroll to the bottom of the page for
+                    more instructions (will enhance this later by making an
+                    expandable/disposable button near the top of the page).
                 </Modal.Body>
             </Modal>
         </div>
