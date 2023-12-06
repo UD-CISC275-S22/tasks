d3d00ac10afc45e5097cc0f4b539f1f6ec0846b8
westa <westa@udel.edu>
2023-12-06 13:53:24 -0500

    Unifisniehd export

--- src/Components/Buttons/ExportCSV.tsx
+++ src/Components/Buttons/ExportCSV.tsx
@@ -1,26 +1,75 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { Plan } from "../../Interfaces/plan";
+import { CSVLink } from "react-csv";
+import { useTable } from "react-table";
 
 export const ExportCSV = ({ plans }: { plans: Plan[] }) => {
+    const columns = [
+        { Header: "ID", accessor: "id" },
+        { Header: "Title", accessor: "title" },
+        { Header: "Concentration", accessor: "concentration" },
+        { Header: "Total Credits", accessor: "credits" },
+        { Header: "Semesters", accessor: "semesters" }
+    ];
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+        useTable({ columns, data });
     function toCSV(): JSX.Element {
-        //map( string => ${q.id},${q.name},${q.options}, etc. )
-        //or .join them all
-        //deep copy
-        const planCopy = plans.map((plan: Plan) => ({
-            ...plan,
-            semesters: [...plan.semesters]
-        }));
-        const planComma = planCopy
-            .map(
-                (plan: Plan): string =>
-                    `${plan.id},${plan.title},${plan.concentration},${plan.credits},
-                    ${plan.semesters}`
-            )
-            .join("\n");
-        const ResultString =
-            "id,title,concentration,credits,semesters" + "\n" + planComma;
-        return ();
+        const csvData = [
+            ["ID", "Title", "Concentration", "Total Credits", "Semesters"],
+            ...plans.map(({ id, title, concentration, credits, semesters }) => [
+                id,
+                title,
+                concentration,
+                credits,
+                semesters
+            ])
+        ];
+        return (
+            <div className="export">
+                {/* Export Button Start */}
+                <CSVLink
+                    className="downloadbtn"
+                    filename="my-plans.csv"
+                    data={csvData}
+                >
+                    Export Plans to CSV
+                </CSVLink>
+                {/* Export Button End */}
+                {/* Table Start */}
+                <table {...getTableProps()}>
+                    <thead>
+                        {headerGroups.map((headerGroup) => (
+                            <tr
+                                key={id}
+                                data={...headerGroup.getHeaderGroupProps()}
+                            >
+                                {headerGroup.headers.map((column) => (
+                                    <th {...column.getHeaderProps()}>
+                                        {column.render("Header")}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                        {rows.map((row) => {
+                            prepareRow(row);
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map((cell) => (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render("Cell")}
+                                        </td>
+                                    ))}
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+                {/* Table End */}
+            </div>
+        );
     }
     return (
         <Button
