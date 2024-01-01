1b5b159ce64d4aaa48ea003da3f3a0be5583e7a0
Priyanka Chaudhuri <priankac@udel.edu>
2024-01-01 16:30:34 -0500

    filename

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 export function App(): JSX.Element {
-    return <h1>CISC275 Hello World</h1>;
-
     return (
         <div className="App">
             <header className="App-header">
@@ -14,55 +12,47 @@ export function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-        </div>
-    );
 
-    return (
-        <div>
-            <h1>Hello World</h1>
-            <img
-                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.computerhope.com%2Fjargon%2Fd%2Fdesktopc.htm&psig=AOvVaw1DMQo2OekdIuCIRyYy30VA&ust=1694879818062000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNCm5739rIEDFQAAAAAdAAAAABAK"
-                alt="This is a Computer"
-            />
-        </div>
-    );
-    return (
-        <div>
-            <ol>
-                <li>one</li>
-                <li>two</li>
-                <li>three</li>
-            </ol>
-        </div>
-    );
+            <div>
+                <h1>Hello World</h1>
+                <img
+                    src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.computerhope.com%2Fjargon%2Fd%2Fdesktopc.htm&psig=AOvVaw1DMQo2OekdIuCIRyYy30VA&ust=1694879818062000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNCm5739rIEDFQAAAAAdAAAAABAK"
+                    alt="This is a Computer"
+                />
+            </div>
 
-    return (
-        <div>
-            <Button> Log Hello World</Button>
-        </div>
-    );
+            <div>
+                <ol>
+                    <li>one</li>
+                    <li>two</li>
+                    <li>three</li>
+                </ol>
+            </div>
 
-    return (
-        <div>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-        </div>
-    );
-    return (
-        <div>
-            <Container>
-                <Row>
-                    <Col>
-                        Shape
-                        <div className="shape"></div>
-                    </Col>
-                    <Col>
-                        <div className="shape"></div>
-                        Hello world
-                    </Col>
-                </Row>
-            </Container>
+            <div>
+                <Button> Log Hello World</Button>
+            </div>
+
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
+
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            Shape
+                            <div className="shape"></div>
+                        </Col>
+                        <Col>
+                            <div className="shape"></div>
+                            Hello world
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
