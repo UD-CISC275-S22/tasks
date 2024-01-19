967d0dc0cc9e1beb1e2782a2175671992429a9f6
Daniel Plante <dplante@stetson.edu>
2024-01-18 22:09:51 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <h1>Daniel Plante</h1>
+            <h2>Hello World</h2>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
