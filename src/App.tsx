ae2afa23da89a728eb8a745d4b969d095cfa4a50
AlexLee729 <alexblee1118@gmail.com>
2024-02-05 16:51:22 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
+                Name: Alexander Lee Edit <code>src/App.tsx</code> and save. This
                 automatically reload.
             </p>
         </div>
