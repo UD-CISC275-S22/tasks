50d046d3136e469aa857e2bf576910c0865e42a0
Roger <57421158+RogerCronin@users.noreply.github.com>
2024-02-06 11:37:17 -0500

    add hello world

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Roger Cronin.
+                automatically reload. Roger Cronin. Hello World!
             </p>
         </div>
     );
