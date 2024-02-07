bb4639b6927968246a7ae0a9bd10e0d7c5c855bc
IsaacWeber1 <isaacgweber@gmail.com>
2024-02-07 10:14:23 -0500

    Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <h1>Hello World</h1>
+            <p>Hello World</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically self destruct in five seconds.
