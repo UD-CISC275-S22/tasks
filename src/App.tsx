c51e476cb4b35e7072aa5b1d96eacc2800aba5fa
RIcky Kiamilev <rickykiamilev@wifi-roaming-128-4-138-41.wifi.udel.edu>
2024-02-07 12:20:34 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Ricky Kiamilev UD CISC275 with React Hooks and TypeScript
+                Ricky Kiamilev. UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Ricky Kiamilev <code>src/App.tsx</code> and save. This page will
