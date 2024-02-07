fd52de33c36c19052c96d6b813d5ced80995ff9c
jaburger <jaburger@udel.edu>
2024-02-07 11:46:32 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>
+                Justin Burger <></>
+            </p>
         </div>
     );
 }
