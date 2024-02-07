e36f6ac217c177763e517d0a57eccd83ffd50775
JacobW <jacobwhi@udel.edu>
2024-02-07 16:01:40 -0500

    i added my name again

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Jacob Whitman
+                automatically reload. Jacob Whitman automatically
             </p>
         </div>
     );
