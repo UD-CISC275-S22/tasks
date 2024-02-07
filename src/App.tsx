56a598c43e70b518f921f7aabb2c11b7ae5ffad9
Drew Crispino <andrewvcrispino@gmail.com>
2024-02-07 10:03:12 -0500

    Added Hello World to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Edited by Drew Crispino.
+                automatically reload. Edited by Drew Crispino. Hello World.
             </p>
         </div>
     );
