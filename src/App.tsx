3dc15f5315dc72442863eb5aabd72465c37045c4
Miles Gaydos <mdgaydos@udel.edu>
2024-02-08 12:13:14 -0500

    Pull Request task

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Miles Gaydos
+                automatically reload. Miles Gaydos says Hello World
             </p>
         </div>
     );
