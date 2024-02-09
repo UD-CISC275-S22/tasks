d9a2eac3b737a1f8ddfbe6611e000d9f20565024
ChristopherRasquin <crasquin@udel.edu>
2024-02-09 12:52:28 -0500

    attempt 4 at basic css/html

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App(): JSX.Element {
             </p>
             <h1>Cool Image Here</h1>
             <img
-                src="../assets/images/Green,_yellow_snake.jpg"
+                src="https://upload.wikimedia.org/wikipedia/commons/b/b4/Green%2C_yellow_snake.jpg"
                 alt="Its a cool snake"
             />
             Movie List:
