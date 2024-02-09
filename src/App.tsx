51b844064164e4deb8e7c75d80fd7b690a2e11bd
Saaketh <saakethp@udel.edu>
2024-02-09 14:22:12 -0500

    added picture

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App(): JSX.Element {
                 Saaketh Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World.
             </p>
+            <img src="//i.imgur.com/6lRuDdL.jpg" alt="me and my friends cars" />
         </div>
     );
 }
-
 export default App;
