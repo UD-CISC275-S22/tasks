f7c720ef4c793f9cff8fd1cd115e119629a91e20
JacobW <jacobwhi@udel.edu>
2024-02-10 18:55:21 -0500

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Jacob Whitman automatically
+                automatically reload. Jacob Whitman automatically. Hello World!
             </p>
         </div>
     );
