1dd1198bf6644d5cdcc8248480e79c646b786a7b
JacobW <jacobwhi@udel.edu>
2024-02-11 16:05:35 -0500

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -11,6 +12,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Jacob Whitman automatically. Hello World!
             </p>
+            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
         </div>
     );
 }
