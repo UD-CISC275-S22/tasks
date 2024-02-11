e7299752c94ec52fa4adab478cb8bab7bae45346
JacobW <jacobwhi@udel.edu>
2024-02-11 17:15:10 -0500

    just added a bit more

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import "./App.css";
 import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
+    //TODO:  Heading??? idk Also the red box, image and button
     return (
         <div className="App">
-            <span style={{ color: "red" }}>
-                <h1 className="App-header">
-                    UD CISC275 with React Hooks and TypeScript
-                </h1>
-            </span>
+            <h1 className="App-header">
+                UD CISC275 with React Hooks and TypeScript
+            </h1>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Jacob Whitman automatically. Hello World!
