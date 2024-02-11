fe450d62299321509a97283a6ea9b47e8487b595
Connor Jackson <cjac@udel.edu>
2024-02-11 18:13:13 -0500

    Added Hello World to App

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App(): JSX.Element {
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. I love Big Chungus and he is a very big
-                bunny!
+                bunny! Hello World
             </p>
         </div>
     );
