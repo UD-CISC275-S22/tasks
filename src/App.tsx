f0f0b252894eb93e5a162ad5b19ab6102b1fca6e
JacobW <jacobwhi@udel.edu>
2024-02-11 16:11:10 -0500

    added column

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -12,6 +12,16 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Jacob Whitman automatically. Hello World!
             </p>
+            <Container>
+                <Row>
+                    <Col>First column.</Col>
+                    <Col>
+                        Second column. You can put whatever you want in here,
+                        and it will be on the right side. Maybe try adding an
+                        image?
+                    </Col>
+                </Row>
+            </Container>
             <Button onClick={() => console.log("I am logged")}>Click Me</Button>
         </div>
     );
