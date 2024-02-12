f2c17264c316fe71222ecc95da4c4931e2881283
KUSHAL <kushal.gurrapu@gmail.com>
2024-02-12 16:59:11 -0500

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript <br>Hello World!</br>
+                UD CISC275 with React Hooks and TypeScript
             </header>
             <p>Kushal Gurrapu</p>
+            <p>Hello World</p>
         </div>
     );
 }
