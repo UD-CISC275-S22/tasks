a423139f01fab6257f389781d0302db903fa5515
joanna <joancris@udel.edu>
2024-02-12 21:56:54 -0500

    Added "Hello World" to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>Joanna Crisomia</p>
+            <p>Hello World</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
