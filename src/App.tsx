b41360e2ffffe1283c8a8c20345c63a786ebb74a
Qingjian <xqj@udel.edu>
2024-02-13 13:03:24 -0800

    Added Holle world to App

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <p>''Hello World''</p>
             <p>
                 Edit <code>src/App.tsx return Qingjian Xu</code> and save. This
                 page will automatically reload.
