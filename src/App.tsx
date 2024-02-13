50b43ce5c93a7cf18c663fcf8fbfc3f1c31ba0bf
Brandon Cell <bcell@udel.edu>
2024-02-13 13:24:34 -0500

    Added 'Hello World'

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,11 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-                Brandon Cell
+                UD CISC275 with React Hooks and TypeScript Brandon Cell
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Hello World
             </p>
         </div>
     );
