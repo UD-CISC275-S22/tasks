b1ddf923ec9534ff25dda13b07663a5024b099d6
Duy Duc Tran (Dustin Tran) <125500499+dustintran1324@users.noreply.github.com>
2024-02-13 13:46:37 -0500

    Added name to App second attempt

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Student name Duy Duc (Dustin) Tran
+                automatically reload. Hello student name Duy Duc (Dustin) Tran
             </p>
         </div>
     );
