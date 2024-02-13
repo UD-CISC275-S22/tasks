bd54e3fe8fa03854e06a085909850fb87cf5b08c
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-02-13 11:54:51 -0500

    commiting changes to website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import mark from "./mark.jpg.webp";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -11,6 +16,30 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Jacob Wilber
             </p>
+            <h1>Mark Zuckerberg</h1>
+            <img src={mark} alt="picture of Mark Zuckerberg" />
+            <ul>
+                <li>CEO of Meta</li>
+                <li>Harvard Dropout</li>
+                <li>Founded Facebook at the age of 19</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            Red Box #1
+                            <div className="Red-Box"></div>
+                        </Col>
+                        <Col>
+                            Red Box #2
+                            <div className="Red-Box"></div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
