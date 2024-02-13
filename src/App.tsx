79c0ee9837a5bc556d0d8c6724d26101866e51f7
Miles Gaydos <mdgaydos@udel.edu>
2024-02-12 21:59:37 -0500

    adding the files required for task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
+            <h1>HEADER</h1>
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
@@ -11,6 +13,16 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Miles Gaydos says Hello World
             </p>
+            <Button onClick={() => console.log("Hello World!")}>
+                {" "}
+                Log Hello World
+            </Button>
+            <img src="whistle.jpg" alt="A picture of Josh Hutcherson" />
+            <ol>
+                <li>Computer Science</li>
+                <li>Math</li>
+                <li>History</li>
+            </ol>
         </div>
     );
 }
