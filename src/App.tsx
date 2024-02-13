f7bcecc56be4596074948f8201bfe741dedac9cd
Coleman <cowalsh@udel.edu>
2024-02-13 17:50:33 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Now my name is Coleman Walsh sr
+                Hello World ! Edit <code>src/App.tsx</code> and save. This page
+                will automatically reload. Now my name is Coleman Walsh
             </p>
         </div>
     );
