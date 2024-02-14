2f584872fc8742105caed015ea609f777a582150
Miles Gaydos <mdgaydos@udel.edu>
2024-02-14 17:03:23 -0500

    Adding files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <h1>HEADER</h1>
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
@@ -13,16 +12,33 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Miles Gaydos says Hello World
             </p>
+            <header>Image Below:</header>
+            <img
+                src="src\images\TheWhale.jpg"
+                alt="A picture of Brendan Fraser"
+            />
+            <p></p>
+            Unordered List:
+            <ul>
+                <li>Thing One</li>
+                <li>Thing Two</li>
+                <li>Thing Three</li>
+            </ul>
+            <p></p>
             <Button onClick={() => console.log("Hello World!")}>
-                {" "}
                 Log Hello World
             </Button>
-            <img src="whistle.jpg" alt="A picture of Josh Hutcherson" />
-            <ol>
-                <li>Computer Science</li>
-                <li>Math</li>
-                <li>History</li>
-            </ol>
+            <p></p>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="div1"></div>
+                    </Col>
+                    <Col>
+                        <div className="div1"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
