ec719a869ba9fefe8a6b7cc6d492d4c0f7a2e988
paul156h <136230815+paul156h@users.noreply.github.com>
2024-02-13 19:35:19 -0500

    Revert "Solved html css"

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,16 @@
 import React from "react";
-import logo from "./assets/ardy.png";
 import "./App.css";
-import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header></header>
-            <h1 className="App-heading"> James Paul Healy UD CISC275</h1>
-            <p className="App-logo">Hello World</p>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <Container>
-                <Row>
-                    <Col className="App-text">
-                        <ul>
-                            Do not put the following items in the deep frier:
-                            <li>Ice</li>
-                            <li>Airpods</li>
-                            <li>Rats</li>
-                        </ul>
-                    </Col>
-                    <Col>
-                        <img src={logo} alt="You ardy know" />
-                    </Col>
-                </Row>
-            </Container>
-            <Container>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                height: 200,
-                                width: 50,
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                    </Col>
-                    <Col>
-                        <div
-                            style={{
-                                height: 200,
-                                width: 50,
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                    </Col>
-                </Row>
-            </Container>
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript
+            </header>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload. James Paul Healy Hello World
+            </p>
         </div>
     );
 }
