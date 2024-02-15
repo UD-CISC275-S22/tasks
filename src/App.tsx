4f343c9ae8f1cde243ab71b7e34e096f1590efcf
nathanrowell <nathandrowell@gmail.com>
2024-02-15 11:32:39 -0500

    Added two columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                I am adding my name Nathan Rowell. This page will automatically
-                reload. Hello World. Hello World again
-            </p>
-            <h1>Hello World NUMBER TWO</h1>
-            <img
-                src="https://media.pitchfork.com/photos/64c28ffcc526dd3929136bfd/16:9/w_2656,h_1494,c_limit/Taylor%20Swift.jpg"
-                alt="TAYLOR SWIFT"
-            />
+            <Container>
+                <Row>
+                    <Col>
+                        <header className="App-header">
+                            UD CISC275 with React Hooks and TypeScript
+                        </header>
+                        <p>
+                            I am adding my name Nathan Rowell. This page will
+                            automatically reload. Hello World. Hello World again
+                        </p>
+                    </Col>
+                    <Col>
+                        <h1>Hello World NUMBER TWO</h1>
+                        <img
+                            src="https://media.pitchfork.com/photos/64c28ffcc526dd3929136bfd/16:9/w_2656,h_1494,c_limit/Taylor%20Swift.jpg"
+                            alt="TAYLOR SWIFT"
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
