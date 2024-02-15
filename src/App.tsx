1227afa23e8cb6317e69ad8a39116453be0f0021
nathanrowell <nathandrowell@gmail.com>
2024-02-15 12:14:31 -0500

    Fixed Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,28 @@ function App(): JSX.Element {
                 <Row>
                     <Col>
                         <header className="App-header">
-                            UD CISC275 with React Hooks and TypeScript
+                            UD CISC275 with NATHAN ROWELL, React Hooks, and
+                            TypeScript
                         </header>
                         <Button onClick={() => console.log("Hello World!")}>
                             Log Hello World
                         </Button>
-                        <p>
-                            I am adding my name Nathan Rowell. This page will
-                            automatically reload. Hello World. Hello World again
-                        </p>
-                        <p> my favorite animals:</p>
-                        <ol>
-                            <li>Cat</li>
-                            <li>Penguin</li>
-                            <li>Hippo</li>
-                        </ol>
+                        <p>Hello World again</p>
                     </Col>
                     <Col>
-                        <h1>Hello World NUMBER TWO</h1>
+                        <h1>TAYLOR SWIFT</h1>
                         <img
                             src="https://media.pitchfork.com/photos/64c28ffcc526dd3929136bfd/16:9/w_2656,h_1494,c_limit/Taylor%20Swift.jpg"
                             alt="TAYLOR SWIFT"
-                            width="200"
-                            height="200"
+                            width="400"
+                            height="300"
                         />
+                        <p> My Favorite Taylor Swift Songs:</p>
+                        <ol>
+                            <li>Cardigan</li>
+                            <li>Enchanted</li>
+                            <li>You Belong With Me</li>
+                        </ol>
                     </Col>
                 </Row>
             </Container>
