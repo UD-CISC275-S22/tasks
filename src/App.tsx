9e60e762e4f58354b5464462ce670ecec2f2ba3d
DracoMeteorStorm <adracometeorstorm@gmail.com>
2024-02-15 20:58:49 -0500

    Added additional HTML and CSS functions to the page.

--- src/App.tsx
+++ src/App.tsx
@@ -12,98 +12,90 @@ export function App(): JSX.Element {
                             <>
                                 <>
                                     <>
-                                        <>
-                                            <div>
-                                                <Button>Click Me</Button>
-                                            </div>
-                                            <div>
-                                                <Button
-                                                    onClick={() =>
-                                                        console.log(
-                                                            "I am logged"
-                                                        )
-                                                    }
-                                                >
-                                                    Click Me
-                                                </Button>
-                                            </div>
-                                        </>
-                                        <>
-                                            <h1>This is header text</h1>
+                                        <></>
+                                        <div className="App">
+                                            <header className="App-header">
+                                                UD CISC275 with React Hooks and
+                                                TypeScript - Hello world -
+                                                Created by Tolu Akin
+                                            </header>
                                             <p>
-                                                This is just a paragraph of
-                                                text. It can go onto multiple
-                                                lines, if you want.
+                                                Edit <code>src/App.tsx</code>{" "}
+                                                and save. This page will
+                                                automatically reload.
                                             </p>
-                                        </>
-                                    </>
-                                    <>
+                                        </div>
                                         <div>
-                                            <h1>Hello World</h1>
-                                            <p>How are you doing today?</p>
+                                            <Button
+                                                onClick={() =>
+                                                    console.log("Hello World!")
+                                                }
+                                            >
+                                                Log Hello World
+                                            </Button>
                                         </div>
                                     </>
+                                    <>
+                                        <p>
+                                            This is just a paragraph of text. It
+                                            can go onto multiple lines, if you
+                                            want.
+                                        </p>
+                                    </>
                                 </>
-                                <div>
-                                    Unordered List:
-                                    <ul>
-                                        <li>First thing</li>
-                                        <li>Another thing</li>
-                                        <li>A third item</li>
-                                    </ul>
-                                    Ordered List:
-                                    <ol>
-                                        <li>First thing</li>
-                                        <li>Another thing</li>
-                                        <li>A third item</li>
-                                    </ol>
-                                </div>
+                                <>
+                                    <div>
+                                        <h1>Hello World</h1>
+                                        <p>How are you doing today?</p>
+                                    </div>
+                                </>
                             </>
                             <div>
-                                <h1>Hello World</h1>
-                                <img
-                                    src="Users\skysw\tasks\src\assets\swagicon.png"
-                                    alt="waddle dee"
-                                />
+                                List:
+                                <ul>
+                                    <li>item 1</li>
+                                    <li>item 2</li>
+                                    <li>item 3</li>
+                                </ul>
                             </div>
                         </>
-                        <div
-                            style={{ border: "1px solid blue", padding: "4px" }}
-                        >
-                            this will be surrounded by a border and padding.
+                        <div>
+                            <img src="verse.jpg" alt="cool bg" />
                         </div>
                     </>
-                    <div>
-                        This is{" "}
-                        <span style={{ color: "red" }}>colored text</span>.
+                    <div style={{ border: "1px solid blue", padding: "4px" }}>
+                        this will be surrounded by a border and padding.
                     </div>
                 </>
                 <div>
-                    <Container>
-                        <Row>
-                            <Col>First column.</Col>
-                            <Col>
-                                Second column. You can put whatever you want in
-                                here, and it will be on the right side. Maybe
-                                try adding an image?
-                                <img
-                                    src="Users\skysw\tasks\src\assets\swagicon.png"
-                                    alt="waddle dee 2"
-                                />
-                            </Col>
-                        </Row>
-                    </Container>
+                    This is <span style={{ color: "red" }}>colored text</span>.
                 </div>
             </>
-            <div className="App">
-                <header className="App-header">
-                    UD CISC275 with React Hooks and TypeScript - Hello world -
-                    Created by Tolu Akin
-                </header>
-                <p>
-                    Edit <code>src/App.tsx</code> and save. This page will
-                    automatically reload.
-                </p>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "40px",
+                                    height: "80px",
+                                    backgroundColor: "#FF1818"
+                                }}
+                            ></div>
+                            First column.
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "40px",
+                                    height: "80px",
+                                    backgroundColor: "#FF4848"
+                                }}
+                            ></div>
+                            Second column.
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </>
     );
