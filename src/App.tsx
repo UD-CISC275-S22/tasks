3a6b3ad08d21c205c8f396894ff96d1995a99f49
LordListowel <animlistowel@gmail.com>
2024-02-15 20:53:45 -0500

    Task-3 Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -11,7 +12,30 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <body>Hello World</body>
+            <div>Hello World</div>
+            <h3>Learning HTML</h3>
+            <Container>
+                <Row>
+                    <Col>
+                        <img
+                            src="https://www.udel.edu/content/dam/udelImages/udaily/2021/March/fb-campus-aerial-the-green-800x420.jpg"
+                            alt="UD Campus"
+                        ></img>
+                        <div className="First"></div>
+                    </Col>
+                    <Col>
+                        <ul>
+                            <li>UD campus is beautiful</li>
+                            <li>I love the people here</li>
+                            <li>There is so much to do here</li>
+                        </ul>
+                        <Button onClick={() => console.log("Hello World!")}>
+                            Log Hello World
+                        </Button>
+                        <div className="Second"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
