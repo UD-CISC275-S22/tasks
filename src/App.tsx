11599b91ead61ccbd5770391579a63e3e19a7557
CARTER MCCABE <cfmccabe@udel.edu>
2024-02-15 23:47:34 -0500

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import file from "../public/funny.jpeg";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript by Carter McCabe
+                UD CISC275 with React Hooks and TypeScript by Carter McCabe!
             </header>
 
             <p>
@@ -22,7 +23,7 @@ function App(): JSX.Element {
                 <li>Guitars</li>
             </ol>
 
-            <img src="/funny.jpeg" alt="funny computer science meme" />
+            <img src={file} alt="funny computer science meme" />
 
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
