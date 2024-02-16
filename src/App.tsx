c83dc9aec79925678b5915bdfbf666dbd0d0b619
CARTER MCCABE <cfmccabe@udel.edu>
2024-02-15 19:38:17 -0500

    trying some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,22 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript by Carter McCabe
             </header>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World!
             </p>
+            <header className="App-header2">I love headers</header>
+            <blockquote class="imgur-embed-pub" lang="en" data-id="wvVJg64">
+                <a href="//imgur.com/wvVJg64">
+                    the struggle and confusion is real af
+                </a>
+            </blockquote>
+            <script
+                async
+                src="//s.imgur.com/min/embed.js"
+                charset="utf-8"
+            ></script>
         </div>
     );
 }
