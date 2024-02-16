535199b909f1dcc472ddf254cb65abceb7017d2d
ErenYaegar <hbagga@udel.edu>
2024-02-15 23:16:53 -0500

    column colors and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import ronaldoImage from "/Users/hb/Desktop/CISC275/tasks/src/ronaldo 5.jpeg";
 
 function App(): JSX.Element {
     return (
@@ -7,10 +9,50 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>Welcome to my Website </h1>
+            <ul>
+                <li>First thing</li>
+                <li>Another thing</li>
+                <li>A third item</li>
+            </ul>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100%",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        First column.
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100%",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        Second column. You can put whatever you want in here,
+                        and it will be on the right side. Maybe try adding an
+                        Maybe try adding an image?
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Harman Bagga. Hello World
             </p>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <img
+                src={ronaldoImage}
+                alt="Ronaldo siuuuuuuuu"
+                style={{ height: "200px", width: "auto" }}
+            />
         </div>
     );
 }
