21a26cf062ebfddfa3b375f334ea4eca2b0257fb
Phillip <phillipm@udel.edu>
2024-02-17 21:15:27 -0500

    removed img

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import img from "./Images/Alicia_Meme.jpg";
 
 function App(): JSX.Element {
     return (
