c78fe9e5ec47b4f7c605faf3878ebdbc047dc47d
bocks <bocksteink@gmail.com>
2024-02-19 17:17:02 -0500

    Completed convertYesNo function

--- src/functions.ts
+++ src/functions.ts
@@ -48,5 +48,12 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
+    if (word.toUpperCase() === "YES") {
+        return true;
+    } else if (word.toUpperCase() === "NO") {
+        return false;
+    } else {
+        return null;
+    }
     return true;
 }
