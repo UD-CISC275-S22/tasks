89f3675073a7dcee3f8ef86bf0dcfd628d83cbb4
Ben Bank <benbank@udel.edu>
2024-02-20 11:09:19 -0500

    TEST

--- src/functions.ts
+++ src/functions.ts
@@ -4,32 +4,46 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * (5 / 9);
 }
-
+//DONE
 /**
  * Consumes three numbers and produces their sum. BUT you should only add a number
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let x: number;
+    x = 0;
+    if (first > 0) {
+        x += first;
+    }
+    if (second > 0) {
+        x += second;
+    }
+    if (third > 0) {
+        x += third;
+    }
+    return x;
 }
-
+//DONE
 /**
  * Consumes a string and produces the same string in UPPERCASE and with an exclamation
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    message = message.toLocaleUpperCase();
+    message += "!";
+    return message;
 }
-
+//DONE
 /**
  * Consumes a string (a message) and returns a boolean if the string ends in a question
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message[message.length - 1] === "?";
 }
+//DONE
 
 /**
  * Consumes a word (a string) and returns either `true`, `false`, or `null`. If the string
@@ -37,5 +51,12 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    word = word.toLowerCase();
+    if (word === "yes") {
+        return true;
+    } else if (word === "no") {
+        return false;
+    }
+    return null;
 }
+//DONE
