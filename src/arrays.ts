2b51f32b5d92a7061e1758884440346eee63bea3
Drew Crispino <andrewvcrispino@gmail.com>
2024-02-19 20:13:38 -0500

    Completed tripleNumbers function

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,6 +5,7 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
+    //Not done
     return numbers;
 }
 
@@ -13,7 +14,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    let tripled = numbers.map((trip: number): number => trip * 3);
+    return tripled;
 }
 
 /**
