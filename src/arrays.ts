3ad23b16072edbfc62f4efd5c674c814b84b3931
Matthew <mmontal@udel.edu>
2024-02-20 22:39:19 -0500

    Completed function stringsToIntegers

--- src/arrays.ts
+++ src/arrays.ts
@@ -25,7 +25,7 @@ export function bookEndList(numbers: number[]): number[] {
 export function tripleNumbers(numbers: number[]): number[] {
     let newNumbers: number[] = [];
     newNumbers = numbers.map((number: number): number => number * 3);
-    return numbers;
+    return newNumbers;
 }
 
 /**
@@ -33,7 +33,7 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map((str) => parseInt(str) || 0);
 }
 
 /**
