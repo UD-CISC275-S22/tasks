d1526c89bcb371b631e325e2217c158eb194ca51
Drew Crispino <andrewvcrispino@gmail.com>
2024-02-24 23:21:54 -0500

    Completed publishQuestion function

--- src/objects.ts
+++ src/objects.ts
@@ -96,7 +96,7 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return { ...question, published: !question.published };
 }
 
 /**
