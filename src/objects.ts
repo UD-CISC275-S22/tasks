2c6f7ce756d51555683f29a2c8e705fd2cbfb996
Drew Crispino <andrewvcrispino@gmail.com>
2024-02-24 23:52:51 -0500

    Completed addOption function

--- src/objects.ts
+++ src/objects.ts
@@ -122,7 +122,7 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    return { ...question, options: [...question.options, newOption] };
 }
 
 /**
