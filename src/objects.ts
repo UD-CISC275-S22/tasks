9a2c271402b3604ecd9ef425870c000a15f26bda
Drew Crispino <andrewvcrispino@gmail.com>
2024-02-24 23:32:14 -0500

    Completed duplicateQuestion function

--- src/objects.ts
+++ src/objects.ts
@@ -106,7 +106,12 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    return {
+        ...oldQuestion,
+        name: "Copy of " + oldQuestion.name,
+        published: false,
+        id: id
+    };
 }
 
 /**
