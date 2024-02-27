5405ed9e5ab523ef493f2671ee61511c6932721b
LordListowel <animlistowel@gmail.com>
2024-02-27 02:48:39 -0500

    Fixed 3/4 initila errors

--- src/objects.ts
+++ src/objects.ts
@@ -119,7 +119,12 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return { ...oldQuestion, id: id, name: `Copy of ${oldQuestion.name}` };
+    return {
+        ...oldQuestion,
+        id: id,
+        name: `Copy of ${oldQuestion.name}`,
+        published: false
+    };
 }
 
 /**
@@ -130,7 +135,7 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return { ...question, options: [...question.options, "newOption"] };
+    return { ...question, options: [...question.options, newOption] };
 }
 
 /**
