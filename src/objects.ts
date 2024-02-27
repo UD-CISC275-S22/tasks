c9798e67689e495f3591024ece67db8b46ef2e35
Matthew <mmontal@udel.edu>
2024-02-27 15:14:15 -0500

    Completed the function isValid

--- src/objects.ts
+++ src/objects.ts
@@ -48,6 +48,11 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
+    if (question.type === "short_answer_question") {
+        return true;
+    } else if (question.type === "multiple_choice_question") {
+        return question.options.includes(answer);
+    }
     return false;
 }
 
