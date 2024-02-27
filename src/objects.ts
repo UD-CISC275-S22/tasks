2dd9f78a6729bb31f201624fa63fa0d5ec9029ca
Matthew <mmontal@udel.edu>
2024-02-27 17:31:20 -0500

    Completed function toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -63,7 +63,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return question.id.toString() + ": " + question.name[10];
+    return question.id.toString() + ": " + question.name.substring(0, 10);
 }
 
 /**
@@ -84,7 +84,14 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let result = "# " + question.name + "\n" + question.body + "\n";
+
+    if (question.type === "multiple_choice_question") {
+        question.options.forEach((option) => {
+            result += "- " + option + "\n";
+        });
+    }
+    return result;
 }
 
 /**
