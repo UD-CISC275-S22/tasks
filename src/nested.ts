8d687e1d082baa8c8776a2599385917af81cba9a
Matthew <mmontal@udel.edu>
2024-02-27 23:31:00 -0500

    Completed function findQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -32,7 +32,7 @@ export function findQuestion(
     questions: Question[],
     id: number
 ): Question | null {
-    return null;
+    return questions.find((question) => question.id === id) || null;
 }
 
 /**
