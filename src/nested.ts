29c0e9c330a124d43442324d80309f6651595871
Maliq Adewale <maliqade@udel.edu>
2024-02-27 23:04:23 -0500

    Fixed the linting error

--- src/nested.ts
+++ src/nested.ts
@@ -1,4 +1,4 @@
-import { text } from "stream/consumers";
+// import { text } from "stream/consumers";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
 import { duplicateQuestion, makeBlankQuestion } from "./objects";
