76a4ee9eebd855115c46aadb4d2ff53f16e4c30d
Davyn Savage <dsav@udel.edu>
2024-02-27 23:24:08 -0500

    please

--- src/nested.ts
+++ src/nested.ts
@@ -267,7 +267,3 @@ export function duplicateQuestionInArray(
         ...questions.slice(index + 1)
     ];
 }
-function isEmpty(body: string): boolean {
-    throw new Error("Function not implemented.");
-    return false;
-}
