683678edc159a7846c59b909b9bf7cd956075305
Benjamin Le <benle@udel.edu>
2024-02-28 19:05:46 -0500

    implemented RevealAnswer and ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,20 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    function changeQuestionType(): void {
+        setType(
+            type === "multiple_choice_question"
+                ? "short_answer_question"
+                : "multiple_choice_question"
+        );
+    }
+    return (
+        <div>
+            <Button onClick={changeQuestionType}> Change Type </Button>
+            {type === "short_answer_question"
+                ? "Short Answer"
+                : "Multiple Choice"}
+        </div>
+    );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,14 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(false);
+    function flipVisibility(): void {
+        setVisible(!visible);
+    }
+    return (
+        <span>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>{" "}
+            {visible && 42}
+        </span>
+    );
 }
