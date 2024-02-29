4eb664e8f788294863c87a6e9b9e256741d03041
Olive Odida <oodida@udel.edu>
2024-02-29 17:45:54 -0500

    added secondary header, changed color of main header, added image to the website, added image to src

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,20 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript: Hello World, This is
                 Olive Odida
             </header>
+            <h2>This is a new secondary header</h2>
+            <img
+                src="/Users/oodida/Desktop/tasks/src/dollar_coin.jpg"
+                alt="A picture of a two dollar coin"
+                width="500"
+                height="500"
+            />
+            <ul>
+                <li>Blacklist</li>
+                <li>Love is Blind</li>
+                <li>Avatar the Last Airbender</li>
+                <li>Beyond the Boundary</li>
+            </ul>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
