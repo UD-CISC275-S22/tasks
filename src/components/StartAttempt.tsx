201bb8fd62c99880d4624c3b36ff7eed3be3e613
Benjamin Le <benle@udel.edu>
2024-02-28 20:21:27 -0500

    implemented startattempt and twodice

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,24 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [tries, setTries] = useState<number>(4);
+    const [progress, setProgress] = useState<boolean>(false);
+    function reduceTries(): void {
+        setTries(tries - 1);
+        setProgress(true);
+    }
+    return (
+        <div>
+            <Button onClick={reduceTries} disabled={progress || tries === 0}>
+                Start Quiz
+            </Button>
+            <Button onClick={() => setProgress(false)} disabled={!progress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={() => setTries(tries + 1)} disabled={progress}>
+                Mulligan
+            </Button>
+            <div>Number of Attempts {tries}</div>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,26 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [die1, setDie1] = useState<number>(1);
+    const [die2, setDie2] = useState<number>(2);
+
+    return (
+        <div>
+            <Button onClick={() => setDie1(d6())}>Roll Left</Button>
+            <Button onClick={() => setDie2(d6())}>Roll Right</Button>
+            <div>
+                <span data-testid="left-die">Left Die: {die1} </span>
+                <span data-testid="right-die">Right Die: {die2} </span>
+            </div>
+            <div>
+                {die1 === die2 && die1 !== 1 ? (
+                    <span>Win</span>
+                ) : die1 === 1 && die2 === 1 ? (
+                    <span>Lose</span>
+                ) : (
+                    <span></span>
+                )}
+            </div>
+        </div>
+    );
 }
