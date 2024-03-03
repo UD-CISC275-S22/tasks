7c2fa922c466b1175ca9ad3f5fa590ac4a72c441
KUSHAL <kushal.gurrapu@gmail.com>
2024-03-02 19:50:01 -0500

    finished TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,22 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [die1, setDie1] = useState<number>(1);
+    const [die2, setDie2] = useState<number>(2);
+
+    return (
+        <div>
+            <div>
+                <span data-testid="left-die">Left Die: {die1} </span>
+                <span data-testid="right-die"> Right Die: {die2}</span>
+            </div>
+            <Button onClick={() => setDie1(d6())}>Roll Left</Button>
+            <Button onClick={() => setDie2(d6())}>Roll Right</Button>
+            {die1 === 1 && die2 === 1 ? (
+                <div>You Lose!</div>
+            ) : die1 === die2 ? (
+                <div>You Win!</div>
+            ) : null}
+        </div>
+    );
 }
