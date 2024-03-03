a8b280b38622ed0ee506b6da9d61c4de29f6e8dd
Rahul <rahulp@udel.edu>
2024-03-03 11:09:06 -0500

    lll

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,22 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [die1, setDie1] = useState<number>(0);
+    const [die2, setDie2] = useState<number>(1);
+    const leftRoll = () => {
+        setDie1(d6);
+    };
+    const rightRoll = () => {
+        setDie2(d6);
+    };
+    return (
+        <div>
+            <span data-testid="left-die">{die1}</span>
+            <span data-testid="right-die">{die2}</span>
+            <Button onClick={leftRoll}> Roll Left</Button>
+            <Button onClick={rightRoll}> Roll Right</Button>
+            <Button>{die1 === die2 && die1 !== 1 && <div>Win</div>}</Button>
+            <Button>{die1 === die2 && die1 === 1 && <div>Lose</div>}</Button>
+        </div>
+    );
 }
