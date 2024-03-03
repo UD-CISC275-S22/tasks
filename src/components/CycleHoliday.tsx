88067f3bf22101921bcde87f13118115fa996112
KUSHAL <kushal.gurrapu@gmail.com>
2024-03-02 19:48:30 -0500

    finished CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,36 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+// import { EnumType } from "typescript";
+
+type Holiday = "🦃" | "🎄" | "🐣" | "🎃" | "🪖";
+
+const HolidayTime: Record<Holiday, Holiday> = {
+    "🐣": "🪖",
+    "🪖": "🎃",
+    "🎃": "🦃",
+    "🦃": "🎄",
+    "🎄": "🐣"
+};
+
+const HolidayAlph: Record<Holiday, Holiday> = {
+    "🎄": "🐣",
+    "🐣": "🎃",
+    "🎃": "🪖",
+    "🪖": "🦃",
+    "🦃": "🎄"
+};
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("🐣");
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <Button onClick={() => setHoliday(HolidayAlph[holiday])}>
+                Advance by Alphabet
+            </Button>
+            <Button onClick={() => setHoliday(HolidayTime[holiday])}>
+                Advance by Year
+            </Button>
+        </div>
+    );
 }
