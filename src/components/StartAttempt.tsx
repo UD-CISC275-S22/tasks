9086c874721761dff06b4bd2dd23b1b0baca094e
KUSHAL <kushal.gurrapu@gmail.com>
2024-03-02 19:49:37 -0500

    finished StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,25 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [atts, setAtts] = useState<number>(4);
+    const [prog, setProg] = useState<boolean>(false);
+
+    const startStop = (): void => {
+        prog ? setProg(false) : (setProg(true), setAtts(atts - 1));
+    };
+
+    return (
+        <div>
+            <Button onClick={startStop} disabled={prog || !atts}>
+                Start Quiz
+            </Button>
+            <Button onClick={startStop} disabled={!prog}>
+                Stop Quiz
+            </Button>
+            <Button onClick={() => setAtts(atts + 1)} disabled={prog}>
+                Mulligan
+            </Button>
+            <div>Attemps: {atts}</div>
+        </div>
+    );
 }
