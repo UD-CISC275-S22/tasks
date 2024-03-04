015324a8245d3bcfbc32a42fed7515c01284a9f5
RUSHIL KAUSHIK <rushilkaushik0326@gmail.com>
2024-03-04 18:45:12 -0500

    finished

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,38 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const HolidaysAbc: string[] = [
+        "Christmas: 🎄",
+        "Diwali: 🪔",
+        "Halloween: 🎃",
+        "New Years: 🎉",
+        "Thanksgiving: 🦃"
+    ];
+    const HolidaysTime: string[] = [
+        "Diwali: 🪔",
+        "Halloween: 🎃",
+        "Thanksgiving: 🦃",
+        "Christmas: 🎄",
+        "New Years: 🎉"
+    ];
+
+    const [holiday, setHoliday] = useState<string>(HolidaysAbc[0]);
+
+    function nextAbc(current: string) {
+        const index = HolidaysAbc.indexOf(holiday);
+        setHoliday(HolidaysAbc[(index + 1) % HolidaysAbc.length]);
+    }
+
+    function nextTime(current: string) {
+        const index = HolidaysTime.indexOf(holiday);
+        setHoliday(HolidaysTime[(index + 1) % HolidaysTime.length]);
+    }
+
+    return (
+        <div>
+            <Button onClick={() => nextAbc(holiday)}>Next in Alphabet</Button>
+            <Button onClick={() => nextTime(holiday)}>Next in Year</Button>
+            <div>Holiday: {holiday}</div>
+        </div>
+    );
 }
