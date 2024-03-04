66951d0a6e272a577245c4af8189e10046cf9347
RUSHIL KAUSHIK <rushilkaushik0326@gmail.com>
2024-03-04 18:12:03 -0500

    finished TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,36 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [left, setLeft] = useState<number>(1);
+    const [right, setRight] = useState<number>(2);
+    const [msg, setMsg] = useState<string | null>(null);
+
+    function roll(b: boolean) {
+        b ? setLeft(d6()) : setRight(d6());
+    }
+
+    function getMsg(): string | null {
+        if (left == 1 && right == 1) {
+            return "Lose";
+        } else {
+            if (left == right) {
+                return "Win";
+            } else {
+                return null;
+            }
+        }
+    }
+
+    return (
+        <div>
+            <span data-testid="left-die">Left: {left} </span>
+            <span data-testid="right-die">Right: {right} </span>
+            <div>
+                <Button onClick={() => roll(true)}>Roll Left</Button>
+                <Button onClick={() => roll(false)}>Roll Right</Button>
+            </div>
+            <div>{msg}</div>
+            {getMsg()}
+        </div>
+    );
 }
