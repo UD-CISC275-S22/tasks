6ab7505d851316524633ea54547411d1addcd831
Drew Crispino <andrewvcrispino@gmail.com>
2024-03-04 16:35:07 -0500

    Completed ChangeType component

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,29 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    type QuestionType = "multiple_choice_question" | "short_answer_question";
+
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function flipQuestionType(): void {
+        if (questionType === "short_answer_question") {
+            setQuestionType("multiple_choice_question");
+        } else {
+            setQuestionType("short_answer_question");
+        }
+    }
+
+    return (
+        <div>
+            <Button onClick={flipQuestionType}>Change Type</Button>
+            {(questionType === "multiple_choice_question" && (
+                <div>Multiple Choice</div>
+            )) ||
+                (questionType === "short_answer_question" && (
+                    <div>Short Answer</div>
+                ))}
+        </div>
+    );
 }
