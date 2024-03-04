9f9cfaea1bc68c262b0775cd91bf649fbcd2d7bc
RUSHIL KAUSHIK <rushilkaushik0326@gmail.com>
2024-03-04 17:18:30 -0500

    finished StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,35 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+
+    function start(): void {
+        setInProgress(true);
+        setAttempts(attempts - 1);
+    }
+
+    return (
+        <div>
+            <div>{attempts}</div>
+            <Button
+                onClick={start}
+                disabled={inProgress == true || attempts == 0}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => setInProgress(false)}
+                disabled={inProgress == false}
+            >
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => setAttempts(attempts + 1)}
+                disabled={inProgress == true}
+            >
+                Mulligan
+            </Button>
+        </div>
+    );
 }
