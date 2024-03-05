da1e1080a50be98b76bf5038c9f39e677ae80e0f
Rahul <rahulp@udel.edu>
2024-03-05 14:42:36 -0500

    changes

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+//import { dhValue, setDhValue } from "./DoubleHalfState";
 //const [dhValue, setDhValue] = useState<number>(10);
 interface dhProps {
     dhValue: number;
