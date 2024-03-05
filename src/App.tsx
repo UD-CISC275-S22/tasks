77fd1fd7a049cb2309eb0347211d55af22e6520a
OLIVIA FOLIIARD <FOLLIARD@UDEL.EDU>
2024-03-05 10:42:56 -0500

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Button, Container, Row, Col } from "react-bootstrap";
-import img from "./jelliesBackground.jpg";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): JSX.Element {
     return (
@@ -18,33 +13,6 @@ function App(): JSX.Element {
             <header className="App-header" style={{ backgroundColor: "red" }}>
                 Olivia Folliard UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
-            <Container>
-                <Row>
-                    <Col>
-                        <h1>First Column.</h1>
-                        <Button onClick={() => console.log("Hello World!")}>
-                            Log Hello World
-                        </Button>
-                    </Col>
-                    <Col>
-                        <p>Jellies!</p>
-                        <img src={img} alt="A picture of jellyfish." />
-                        <ul>
-                            <li>jellyfish live in the ocean</li>
-                            <li>some jellies glow in the dark</li>
-                            <li>there is a kind of jelly that is immortal</li>
-                        </ul>
-                    </Col>
-                </Row>
-            </Container>
-            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
-            <p> Hello World! </p>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -57,7 +25,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
