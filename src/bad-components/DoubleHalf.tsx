f9d8b116b36b3f97bbafcb0977bd091286c443ca
RUSHIL KAUSHIK <rushilkaushik0326@gmail.com>
2024-03-05 18:11:41 -0500

    finished ChooseTeam

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
 interface dhProps {
     dhValue: number;
