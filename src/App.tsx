17109dbdcc5ae569bddf215fb20e20f184f81498
William Branam <wbranam1009@gmail.com>
2024-03-05 12:42:09 -0500

    merge changes

--- src/App.tsx
+++ src/App.tsx
@@ -10,45 +10,9 @@ import { Counter } from "./components/Counter";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header" style={{ color: "red" }}>
+            <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <Container>
-                <p>
-                    Edit <code>src/App.tsx</code> and save. This page will
-                    automatically reload. By - William Branam. Hello World.
-                </p>
-            </Container>
-            <div>
-                <Button onClick={() => console.log("I am logged")}>
-                    Click Me
-                </Button>
-            </div>
-            <Container>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                backgroundColor: "red",
-                                width: "30%",
-                                height: "40%"
-                            }}
-                        ></div>
-                        First column.
-                    </Col>
-                    <Col>
-                        <div
-                            style={{
-                                backgroundColor: "red",
-                                width: "30%",
-                                height: "40%"
-                            }}
-                        ></div>
-                        Second column. You can put whatever you want in right
-                        right side. Maybe try adding an adding an image?
-                    </Col>
-                </Row>
-            </Container>
             <hr></hr>
             <Counter></Counter>
             <hr />
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+const [visible, setVisible] = useState<boolean>(false);
+
+function flipVisibility(): void {
+    setVisible(!visible);
+}
+
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    return (
+        <div>
+            <button onClick={flipVisibility}>
+                <div>Reveal Answer</div>
+            </button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
