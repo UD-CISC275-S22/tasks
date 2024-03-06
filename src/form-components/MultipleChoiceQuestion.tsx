80b470e08b851d8bbb26ba55d54faac00446888d
Benjamin Le <benle@udel.edu>
2024-03-06 14:50:31 -0500

    implemented MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/esm/Form";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -15,7 +15,24 @@ export function MultipleChoiceQuestion({
             <h3>Multiple Choice Question</h3>
             <Form.Group>
                 <Form.Label>Choose a answer:</Form.Label>
+                <Form.Select
+                    value={selected}
+                    onChange={(event) => setSelected(event.target.value)}
+                >
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
             </Form.Group>
+            <div>
+                {selected === expectedAnswer ? (
+                    <span>✔️</span>
+                ) : (
+                    <span>❌</span>
+                )}
+            </div>
         </div>
     );
 }
