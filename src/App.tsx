2ced3da747deb9be9109412b1ac08e64be87e651
Matthew <mmontal@udel.edu>
2024-03-06 00:08:57 -0500

    Corrected some code that wasnt working

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
-import image1 from "./images/Hamilton.jpg";
-import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -27,30 +25,8 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Matthew Montalvo Hello World
-            </p>
-            <header className="Latest-header">New Header</header>
-            <img src={image1} alt="Hamilton the musical star" />
-            <ul>
-                <li>Apple</li>
-                <li>Banana</li>
-                <li>Orange</li>
-            </ul>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <Container>
-                <Row>
-                    <Col>
-                        <header className="first-Rectangle"> </header>
-                    </Col>
-                    <Col>
-                        <header className="second-Rectangle"> </header>
-                    </Col>
-                </Row>
-            </Container>
         </div>
     );
 }
+
+export default App;
