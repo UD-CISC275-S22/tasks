f569d87c3dcb655494e06533536e2e0657085d61
KaiW <kaidw@udel.edu>
2024-03-05 23:57:00 -0500

    uploaded after successful merge

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,42 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [progress, setProgress] = useState<boolean>(false);
+    function mulligan(): void {
+        setAttempts(attempts + 1);
+    }
+    function flipProgress(): void {
+        if (progress === false) {
+            setAttempts(attempts - 1);
+        }
+        setProgress(!progress);
+    }
+    return (
+        <div>
+            <div>
+                Test Active: {!progress && <div>No</div>}{" "}
+                {progress && <div>Yes</div>}
+            </div>
+            <div>
+                Number of Attempts: <span>{attempts}</span>
+            </div>
+            <div>
+                <Button
+                    onClick={flipProgress}
+                    disabled={progress || attempts < 1}
+                >
+                    Start Quiz
+                </Button>
+                <Button onClick={flipProgress} disabled={!progress}>
+                    Stop Quiz
+                </Button>
+            </div>
+            <div>
+                <Button onClick={mulligan} disabled={progress}>
+                    Mulligan
+                </Button>
+            </div>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,28 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [diceLeft, setDiceLeft] = useState<number>(1);
+    const [diceRight, setDiceRight] = useState<number>(6);
+    function rollLeft(): void {
+        setDiceLeft(d6());
+    }
+    function rollRight(): void {
+        setDiceRight(d6());
+    }
+    return (
+        <div>
+            <div>
+                Left Die: <span data-testid="left-die">{diceLeft}</span> | Right
+                <span data-testid="right-die">{diceRight}</span>
+            </div>
+            <div>
+                <Button onClick={rollLeft}>Roll Left</Button>
+                <Button onClick={rollRight}>Roll Right</Button>
+            </div>
+            <div>
+                {diceRight === 1 && diceLeft === 1 && <div>Lose</div>}
+                {diceRight === diceLeft && diceRight !== 1 && <div>Win</div>}
+            </div>
+        </div>
+    );
 }
