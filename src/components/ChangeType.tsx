4d290768c1d599d737509cb1c8be299f5c834f52
Olivia <okarney@udel.edu>
2024-03-05 19:21:04 -0500

    trying to push to github ... again

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,26 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function changeType(): void {
+        setType(
+            questionType === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+
+    return (
+        <div>
+            <span>
+                {questionType === "short_answer_question"
+                    ? "Short Answer"
+                    : "Multiple Choice"}
+            </span>
+            <Button onClick={changeType}>Change Type</Button>
+        </div>
+    );
 }
--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,45 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("🌲");
+
+    function alphabeticalChange(): void {
+        if (holiday === "🌲") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🎆");
+        } else if (holiday === "🎆") {
+            setHoliday("🍀");
+        } else if (holiday === "🍀") {
+            setHoliday("❤️");
+        } else if (holiday === "❤️") {
+            setHoliday("🌲");
+        }
+        // Christmas, Halloween, New Year's, St. Patrick's Day, Valentine's Day
+    }
+
+    function byDate(): void {
+        if (holiday === "🎃") {
+            setHoliday("🌲");
+        } else if (holiday === "🌲") {
+            setHoliday("🎆");
+        } else if (holiday === "🎆") {
+            setHoliday("❤️");
+        } else if (holiday === "❤️") {
+            setHoliday("🍀");
+        } else if (holiday === "🍀") {
+            setHoliday("🎃");
+        }
+
+        // Halloween, Christmas,  New Year's, Valentine's Day, St. Patrick's Day
+    }
+
+    return (
+        <div>
+            <span>Holiday: {holiday}</span>
+            <br></br>
+            <Button onClick={alphabeticalChange}>Advance by Alphabet</Button>
+            <Button onClick={byDate}>Advance by Year</Button>
+        </div>
+    );
 }
