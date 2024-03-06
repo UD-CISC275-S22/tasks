d9ca47ab70673a1116638d4ec106a2a62dbe1ea4
Nazmul <nazmul@udel.edu>
2024-03-06 17:21:48 -0500

    1/5

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,31 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [display, setDisplay] = useState(false);
+    const [answer, setAnswer] = useState("");
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAnswer(event.target.value);
+    };
+    const handleCheckAnswer = () => {
+        setDisplay((prevDisplay) => !prevDisplay);
+    };
     return (
         <div>
             <h3>Check Answer</h3>
+            <form>
+                <label>
+                    Enter Answer:
+                    <input type="text" onChange={handleInputChange}></input>
+                    <button type="button" onClick={handleCheckAnswer}>
+                        Check Answer
+                    </button>
+                </label>
+                {!display && (
+                    <div>
+                        {answer === expectedAnswer ? <p>✔️</p> : <p>❌</p>}
+                    </div>
+                )}
+            </form>
         </div>
     );
 }
