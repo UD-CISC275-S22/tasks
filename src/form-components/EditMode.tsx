e2ff5d5a0f5e358294e734d21b099cbcd8eb3bdf
William Branam <wbranam1009@gmail.com>
2024-03-07 15:22:09 -0500

    solved edit mode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    const [userName, setUserName] = useState<string>("Your Name");
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Group>
+                <Form.Label>Switch Editing Mode:</Form.Label>
+                <Form.Check
+                    type="switch"
+                    id="is-happy-check"
+                    label={isEditing ? "Editing" : "Not Editing"}
+                    checked={isEditing}
+                    onChange={() => {
+                        setIsEditing(!isEditing);
+                    }}
+                />
+                <Form.Label>
+                    {userName} {isStudent ? "is" : "is not"} a student
+                </Form.Label>
+                <Form.Control
+                    value={userName}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setUserName(event.target.value)
+                    }
+                />
+                <Form.Check
+                    type="checkbox"
+                    id="is-student-check"
+                    label="Change is a student:"
+                    checked={isStudent}
+                    onChange={() => setIsStudent(!isStudent)}
+                />
+            </Form.Group>
         </div>
     );
 }
