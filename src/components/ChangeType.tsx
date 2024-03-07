ee5459faf661c979bd8a0b3046f8d22129abd6d2
Eren <hbagga@udel.edu>
2024-03-07 15:13:34 -0500

    finished task 8

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,26 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    const flipQuestionTypeHandler = (): void => {
+        if (questionType === "short_answer_question") {
+            setQuestionType("multiple_choice_question");
+        } else {
+            setQuestionType("short_answer_question");
+        }
+    };
+
+    const questionTypeOutput: string =
+        questionType === "short_answer_question"
+            ? "Short Answer"
+            : "Multiple Choice";
+    return (
+        <div>
+            <Button onClick={flipQuestionTypeHandler}>Change Type</Button>
+            <p>{questionTypeOutput}</p>
+        </div>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,41 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [numAttempts, setNumAttempts] = useState<number>(4);
+    const [quizInProgess, setQuizInProgress] = useState<boolean>(false);
+
+    const startQuizHandler = (): void => {
+        setQuizInProgress(true);
+        setNumAttempts(
+            (prevNumAttempts: number): number => prevNumAttempts - 1
+        );
+    };
+
+    const stopQuizHandler = (): void => {
+        setQuizInProgress(false);
+    };
+
+    const mulliganHandler = (): void => {
+        setNumAttempts(
+            (prevNumAttempts: number): number => prevNumAttempts + 1
+        );
+    };
+
+    return (
+        <div>
+            <Button
+                onClick={startQuizHandler}
+                disabled={quizInProgess || !numAttempts}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={stopQuizHandler} disabled={!quizInProgess}>
+                Stop Quiz
+            </Button>
+            <Button onClick={mulliganHandler} disabled={quizInProgess}>
+                Mulligan
+            </Button>
+            <p>Attempt Number: {numAttempts}</p>
+        </div>
+    );
 }
