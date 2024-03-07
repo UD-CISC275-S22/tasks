040bb4196f31330ef71a44a5d4159cb0af50ca1a
Nazmul <nazmul@udel.edu>
2024-03-06 23:16:03 -0500

    2/5 form

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,39 +1,41 @@
 import React, { useState } from "react";
-import { setConstantValue } from "typescript";
 
 export function GiveAttempts(): JSX.Element {
     const [attempts, setAttempts] = useState(3);
     const [reqAttemp, setReqAttempt] = useState(0);
-    const [updatedAttempts, setUpdatedAttempts] = useState(attempts);
 
     function useAttemp() {
-        setAttempts((prev) => prev - 1);
+        if (attempts > 0) {
+            setAttempts((prev) => prev - 1);
+        }
     }
 
     const gainAttempts = (event: React.ChangeEvent<HTMLInputElement>) => {
         const convertToNum = Number(event.target.value);
         setReqAttempt(convertToNum);
     };
-    const changeAttempt = () => {
+    const changeAttempt = (event: React.FormEvent) => {
+        event.preventDefault();
         if (reqAttemp > 0) {
             const newAttempts = attempts + reqAttemp;
-            setUpdatedAttempts(newAttempts);
+            setAttempts(newAttempts);
         }
     };
-
     return (
         <div>
             <h3>Give Attempts</h3>
-            <button onClick={useAttemp}>use</button>
-            <form>
+            <button onClick={useAttemp} disabled={attempts === 0}>
+                use
+            </button>
+            <form onSubmit={changeAttempt}>
                 <input
                     placeholder="Enter a number"
                     type="number"
                     onChange={gainAttempts}
                 ></input>
-                <button onClick={changeAttempt}>gain</button>
+                <button type="submit">gain</button>
             </form>
-            <p>Attemps Left: {updatedAttempts}</p>
+            <p>Attempts Left: {attempts}</p>
         </div>
     );
 }
