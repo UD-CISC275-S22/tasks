3559bf5b8a3dea8b7fda1c62b6d076bcebc577c2
Ian Duffy <ianduffy@udel.edu>
2024-03-07 13:55:31 -0500

    Finished CheckAnswer and Started GiveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 
+/*
+Numeric input box
+*/
 export function GiveAttempts(): JSX.Element {
+    /*
+    const [attemptsLeft, setAttempts] = useState<string>("3");
+    const [attemptsRequest, setAttemptsRequest] = useState<string>("0");
+
+    function updateAttemptsLeft(
+        event: React.ChangeEvent<HTMLInputElement>
+    ): void {
+        setAttempts(event.target.value);
+    }
+    */
     return (
         <div>
             <h3>Give Attempts</h3>
