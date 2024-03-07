c0df5d5cf8d2e375be2e2227e7d5939d24fc0f0f
Olivia <okarney@udel.edu>
2024-03-07 14:10:37 -0500

    wrote CheckAnswer ; think it works and is passing the tests

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsRequested, setAttemptsRequested] = useState<number>(0);
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <span>Number of Attempts Remaining: {attemptsLeft}</span>
+
+            <Form.Group controlId="formAttemptsRequested">
+                <Form.Label>Number of Attempts Requested:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attemptsRequested}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAttemptsRequested(event.target.value)
+                    }
+                />
+            </Form.Group>
+
+            <Button onClick={() => setAttemptsLeft(attemptsLeft - 1)}>
+                use
+            </Button>
+            <Button
+                onClick={() =>
+                    setAttemptsLeft(attemptsLeft + { attemptsRequested })
+                }
+            >
+                gain
+            </Button>
         </div>
     );
 }
