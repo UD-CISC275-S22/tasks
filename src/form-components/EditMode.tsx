b0cb3ac2649013eb2eadf38eb4cea7153b5a5c4d
Maliq Adewale <maliqade@udel.edu>
2024-03-08 15:00:31 -0500

    Three functions complete; ChangeColor, CheckAnswer and GiveAttempts

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 
 export function EditMode(): JSX.Element {
+    const [defaultMessage, setDefaultMessage] =
+        useState<string>("Maliq is a Student");
+    const [alternateMessage, setALternateMessage] = useState<string>("");
     return (
         <div>
             <h3>Edit Mode</h3>
