35f12eb43423876f5854f7bdfd81bfd0735d9309
KaiW <kaidw@udel.edu>
2024-03-08 20:41:37 -0500

    task 10 current progress

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
+    function updateUserAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <div>
+                <Form.Group controlId="formCheckAnswer">
+                    <Form.Label>Answer Below:</Form.Label>
+                    <Form.Control
+                        value={userAnswer}
+                        onChange={updateUserAnswer}
+                    />
+                </Form.Group>
+            </div>
+            <div>
+                <h3>{userAnswer === expectedAnswer ? "✔️" : "❌"}</h3>
+            </div>{" "}
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+
+    function updateAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="choosenAnswer">
+                <Form.Label>What letter am I thinking of?</Form.Label>
+                <Form.Select value={answer} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{answer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
