58694a7be2afd2eacce1a83aaaa3dd59b715360a
Nazmul <nazmul@udel.edu>
2024-03-10 14:36:42 -0400

    5/5

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedOpt, setSelectedOpt] = useState("Choose");
+    function updateOption(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedOpt(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="favoriteColors">
+                <Form.Label>Choose the correct answer</Form.Label>
+                <Form.Select value={selectedOpt} onChange={updateOption}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {selectedOpt === expectedAnswer ? <p>✔️</p> : <p>❌</p>}
         </div>
     );
 }
