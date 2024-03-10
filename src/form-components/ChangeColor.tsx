b0cc68944437b0045904376a4dfc55f5509297af
Maliq Adewale <maliqade@udel.edu>
2024-03-09 21:13:07 -0500

    fixed the linting message in ChangeColor file

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,4 +1,4 @@
-import React, { cloneElement, useState } from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
