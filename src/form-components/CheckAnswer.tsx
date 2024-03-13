79b8c3b45b73eb67ccbf52ea1c65ca5c8cca4808
Mayowa Ayeni <mayeni@udel.edu>
2024-03-12 20:52:01 -0400

    finished first 3 form comp

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>): void {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formCheckAnswer">
+                <Form.Label>What is the answer to everything?</Form.Label>
+                <Form.Control value={answer} onChange={updateAnswer} />
+            </Form.Group>
+            {answer === expectedAnswer && <div>✔️</div>}
+            {answer !== expectedAnswer && <div>❌</div>}
         </div>
     );
 }
