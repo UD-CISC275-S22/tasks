9652ff79ef685bc26300ab8eba4fceaea1ef86bd
Mayowa Ayeni <mayeni@udel.edu>
2024-03-13 22:43:28 -0400

    done task 10

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [currentChoice, setCurrentChoice] = useState<string>(options[0]);
+
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrentChoice(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="answers">
+                <Form.Label>What is the correct answer?</Form.Label>
+                <Form.Select value={currentChoice} onChange={updateChoice}>
+                    {options.map((option) => (
+                        <option value={option} key={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {currentChoice === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
