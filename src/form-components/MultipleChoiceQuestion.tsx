a3904c351fab726ec67f2fba63ca78551928be1d
Ricky Kiamilev <rickyk@udel.edu>
2024-03-13 22:54:35 -0400

    done

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +7,31 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedChoice, setSelectedChoice] = useState<string>(options[0]);
+    const [isCorrect, setIsCorrect] = useState<boolean>(
+        selectedChoice === expectedAnswer
+    );
+
+    useEffect(() => {
+        setIsCorrect(selectedChoice === expectedAnswer);
+    }, [selectedChoice, expectedAnswer]);
+
+    const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedChoice(event.target.value);
+    };
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <select value={selectedChoice} onChange={handleSelect}>
+                {options.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <p></p>
+            {isCorrect ? <span>✔️</span> : <span>❌</span>}
         </div>
     );
 }
